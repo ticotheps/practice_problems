2c6ccbf95aeb534a6196bd12843e6c09da52165b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-11 17:13:20 -0400

    Creates another example of how to handle multiple promises

--- other_practice_probs/brandon_prob/brandon_prob.js
+++ other_practice_probs/brandon_prob/brandon_prob.js
@@ -15,39 +15,89 @@
 //   console.log('This is in the catch: ' + message);
 // });
 
-//--------------------------------------------------------
-
 //---------------------CALLBACK example-------------------
 
-const userLeft = false;
-const userWatchingCatVideo = true;
+// const userLeft = false;
+// const userWatchingCatVideo = false;
 
-function watchTutorialCallback(callback, errorCallBack) {
-  if (userLeft) {
-    errorCallBack({
-      name: 'User Left',
-      message: ':(',
-    });
-  } else if (userWatchingCatVideo) {
-    errorCallBack({
-      name: 'User Watching Cat Video',
-      message: 'WebDevSimplified < Cat',
-    });
-  } else {
-    callback('Thumbs up and Subscribe!');
-  }
-}
+// function watchTutorialCallback(callback, errorCallback) {
+//   if (userLeft) {
+//     errorCallback({
+//       name: 'User Left',
+//       message: ':(',
+//     });
+//   } else if (userWatchingCatVideo) {
+//     errorCallback({
+//       name: 'User Watching Cat Video',
+//       message: 'WebDevSimplified < Cat',
+//     });
+//   } else {
+//     callback('Thumbs up and Subscribe!');
+//   }
+// }
 
-watchTutorialCallback(
-  message => {
-    console.log('Success: ' + message);
-  },
-  error => {
-    console.log(error.name + ' ' + error.message);
+// watchTutorialCallback(
+//   message => {
+//     console.log('Success: ' + message);
+//   },
+//   error => {
+//     console.log(error.name + ' ' + error.message);
+//   }
+// );
+
+//------------CALLBACK => PROMISE EXAMPLE---------------
+// -copy code from 'watchTutorialCallback()' above
+// -remove parameters because Promise will handle that for us
+// -replace all instances of 'callback' with 'resolve'
+// -replace all instances of 'errorCall
+
+// function watchTutorialPromise() {
+//   return new Promise((resolve, reject) => {
+//     if (userLeft) {
+//       reject({
+//         name: 'User Left',
+//         message: ':(',
+//       });
+//     } else if (userWatchingCatVideo) {
+//       reject({
+//         name: 'User Watching Cat Video',
+//         message: 'WebDevSimplified < Cat',
+//       });
+//     } else {
+//       resolve('Thumbs up and Subscribe!');
+//     }
+//   });
+// }
+
+// watchTutorialPromise()
+//   .then(message => {
+//     console.log('Success: ' + message);
+//   })
+//   .catch(error => {
+//     console.log(error.name + ' ' + error.message);
+//   });
+
+//------------MULTIPLE PROMISES EXAMPLE---------------
+
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve('Video 1 Recorded');
+});
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+  resolve('Video 2 Recorded');
+});
+
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve('Video 3 Recorded');
+});
+
+Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+  messages => {
+    console.log(messages);
   }
 );
 
-//--------------------------------------------------------
+//------------Brandon's Code Challenge---------------
 
 // friends = ['Jake', 'Ryan', 'Brandon', 'Jamie', 'Julian', 'Lowell'];
 
