86ab78247cacf55aa795213dae1af4894acef61d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-11 16:00:00 -0400

    Creates a third pass solution for centuryFromYear.py

--- code_signal/journey_begins/centuryFromYear/centuryFromYear.py
+++ code_signal/journey_begins/centuryFromYear/centuryFromYear.py
@@ -1,55 +1,27 @@
 def centuryFromYear(year):
-    # century_dict = dict()
-    # century_dict.update({'1': 1, '2': 2, '3': 3,'4': 4,'5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, '11': 11, '12': 12, '13': 13, '14': 14, '15': 15, '16': 16, '17': 17, '18': 18, '19': 19, '20': 20, '21': 21})
-
     if ((year > 0) and (year <= 2005)):
-        # str_year = str(year)
-        # len_year = len(str_year)
+        str_year = str(year)  # converts integer input to string type with 'str()'
+        len_year = len(str_year)  # finds length of new 'str_year'
+        century_arr = []
         century = 0
-        # for digit in range(len_year):
-        #     print(str_year[digit])
-        if (year > 0) and (year <= 100):
+        # iterates through 'str_year'...
+        for digit in range(len_year):
+            # appends each digit from 'str_year' to century_arr
+            century_arr.append(str_year[digit])
+        if len_year == 1:
             century += 1
-        elif (year > 100) and (year <= 200):
-            century += 2
-        elif (year > 200) and (year <= 300):
-            century += 3
-        elif (year > 300) and (year <= 400):
-            century += 4
-        elif (year > 400) and (year <= 500):
-            century += 5
-        elif (year > 500) and (year <= 600):
-            century += 6
-        elif (year > 600) and (year <= 700):
-            century += 7
-        elif (year > 700) and (year <= 800):
-            century += 8
-        elif (year > 800) and (year <= 900):
-            century += 9
-        elif (year > 900) and (year <= 1000):
-            century += 10
-        elif (year > 1000) and (year <= 1100):
-            century += 11
-        elif (year > 1100) and (year <= 1200):
-            century += 12
-        elif (year > 1200) and (year <= 1300):
-            century += 13
-        elif (year > 1300) and (year <= 1400):
-            century += 14
-        elif (year > 1400) and (year <= 1500):
-            century += 15
-        elif (year > 1500) and (year <= 1600):
-            century += 16
-        elif (year > 1600) and (year <= 1700):
-            century += 17
-        elif (year > 1700) and (year <= 1800):
-            century += 18
-        elif (year > 1800) and (year <= 1900):
-            century += 19
-        elif (year > 1900) and (year <= 2000):
-            century += 20
-        elif (year > 2000) and (year <= 2100):
-            century += 21
+        if len_year == 2:
+            century += 1
+        if len_year == 3:
+            if ((century_arr[1] == '0') and (century_arr[2] == '0')):
+                century = int(century_arr[0])
+            else:
+                century = int(century_arr[0]) + 1
+        if len_year == 4:
+            if ((century_arr[2] == '0') and (century_arr[3] == '0')):
+                century = int(century_arr[0] + century_arr[1])
+            else:
+                century = int(century_arr[0] + century_arr[1]) + 1
         return century
     else:
         return "This is not a valid year. Please try again."
@@ -60,3 +32,5 @@ print(centuryFromYear(12))  # should print "1"
 print(centuryFromYear(195))  # should print "2"
 print(centuryFromYear(2005))  # should print "21"
 print(centuryFromYear(2035))  # should print "This is not a valid year. Please try again."
+print(centuryFromYear(1700))  # should print "17"
+print(centuryFromYear(45))  # should print "1"
