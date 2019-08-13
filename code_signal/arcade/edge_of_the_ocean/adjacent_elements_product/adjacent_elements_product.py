381e29d69f157708fcdfa4e3ab206855decdcd59
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-13 13:57:50 -0400

    Updates solution

--- code_signal/arcade/edge_of_the_ocean/adjacent_elements_product/adjacent_elements_product.py
+++ code_signal/arcade/edge_of_the_ocean/adjacent_elements_product/adjacent_elements_product.py
@@ -1,15 +1,20 @@
-num_arr = [1, 2, 3, 4]
+num_arr = [3, 6, -2, -5, 7, 3]
 
 def adjacentElementsProduct(inputArray):
     arr_length = len(inputArray)
-    largest_product = 0
+    largest_product = []
     if (arr_length >= 2) and (arr_length <= 10):
-        for i in range(arr_length):
-            for j in range(i+1, arr_length):
-                product = inputArray[i] * inputArray[j]
-                # print(product)
-                if product > largest_product:
-                    largest_product = product
-    return largest_product
+        i = 0
+        j = 1
+        while (i < arr_length) and (j < arr_length):
+            product = inputArray[i] * inputArray[j]
+            # print(product)
+            if len(largest_product) > 0:
+                if product > largest_product[0]:
+                    largest_product.pop(0)
+            largest_product.append(product)
+            i += 1
+            j += 1
+    return largest_product[0]
           
 print(adjacentElementsProduct(num_arr))
