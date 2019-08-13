25fb70f476face80950f43fdf2207d1fe4cede1d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-13 01:18:15 -0400

    Creates a first pass solution

--- code_signal/arcade/edge_of_the_ocean/adjacent_elements_product/adjacent_elements_product.py
+++ code_signal/arcade/edge_of_the_ocean/adjacent_elements_product/adjacent_elements_product.py
@@ -2,6 +2,14 @@ num_arr = [1, 2, 3, 4]
 
 def adjacentElementsProduct(inputArray):
     arr_length = len(inputArray)
-    print(arr_length)
-    
-adjacentElementsProduct(num_arr)
+    largest_product = 0
+    if (arr_length >= 2) and (arr_length <= 10):
+        for i in range(arr_length):
+            for j in range(i+1, arr_length):
+                product = inputArray[i] * inputArray[j]
+                # print(product)
+                if product > largest_product:
+                    largest_product = product
+    return largest_product
+          
+print(adjacentElementsProduct(num_arr))
