0e6fc99a0ad4bbeee1833e580e9d1173fa60126e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-12 16:30:38 -0400

    Updates brute force solution to twoNumberSum.py

--- algo_expert/easy/twoNumberSum/twoNumberSum.py
+++ algo_expert/easy/twoNumberSum/twoNumberSum.py
@@ -3,7 +3,9 @@ def twoNumberSum(array, targetSum):
 	for i in range(len(array) - 1):
 		firstNum = array[i]
 		for j in range(i + 1, len(array)):
-			secondNum = array[i + 1]
+			secondNum = array[j]
 			if (firstNum + secondNum == targetSum):
 				return [firstNum, secondNum]
 	return []
+
+print(twoNumberSum([1,2,3,4], 6))
