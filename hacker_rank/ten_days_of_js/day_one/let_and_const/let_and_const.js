ec0428323d3035608326ec8a0569b00e41cac7bd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-29 23:59:52 -0400

    Adds to solution

--- hacker_rank/ten_days_of_js/day_one/let_and_const/let_and_const.js
+++ hacker_rank/ten_days_of_js/day_one/let_and_const/let_and_const.js
@@ -27,10 +27,15 @@ function readLine() {
 
 function main() {
   // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  const PI = Math.PI;
 
   // Print the area of the circle:
+  let area = PI * r ** 2;
+  console.log(area);
 
   // Print the perimeter of the circle:
+  let perimeter = 2 * PI * r;
+  console.log(perimeter);
 
   try {
     // Attempt to redefine the value of constant variable PI
