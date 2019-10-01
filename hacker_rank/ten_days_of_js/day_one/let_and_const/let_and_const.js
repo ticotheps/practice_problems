839874cca15ac430bff7bc6bcfe16d3b196a1cad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-01 14:13:23 -0400

    Creates a new directory for 'frontend masters' problems

--- hacker_rank/ten_days_of_js/day_one/let_and_const/let_and_const.js
+++ hacker_rank/ten_days_of_js/day_one/let_and_const/let_and_const.js
@@ -28,6 +28,7 @@ function readLine() {
 function main() {
   // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
   const PI = Math.PI;
+  let r = readLine();
 
   // Print the area of the circle:
   let area = PI * r ** 2;
