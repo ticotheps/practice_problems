e178a5415fbf4f14389652cd24359a0762ae52e6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-01 18:32:56 -0400

    Creates a 'breadcrumbs.js' file to demonstrate how to make an algorithm with nested 'for' loops FASTER using caching

---
+++ frontend_masters/guide_to_algorithms_with_js/breadcrumbs_method.js
@@ -0,0 +1,11 @@
+/* 
+Expected Input: 
+  - an array of numbers
+
+Expected Output:  
+  - if the array contains NO duplicate numbers, return true
+  - if the array contains duplicate numbers, return false
+  
+Constraints:
+  - none
+*/
