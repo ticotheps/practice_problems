64033965b02acb767329b28b332a6053d39d8148
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-01 18:27:27 -0400

    Comments out console.log() statements; adds comments above code to provide context for the problem

--- frontend_masters/guide_to_algorithms_with_js/nested_for_loop.js
+++ frontend_masters/guide_to_algorithms_with_js/nested_for_loop.js
@@ -1,19 +1,37 @@
+/* 
+Expected Input: 
+  - an array of numbers
+
+Expected Output:  
+  - if the array contains NO duplicate numbers, return true
+  - if the array contains duplicate numbers, return false
+  
+Constraints:
+  - none
+*/
+
 const isUnique = arr => {
-  let unique = true;
+  let uniqueNumsInArr = true;
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`~~~~ ** OUTER LOOP ** ~~~~ * i * === ${i}`);
+    // console.log(`~~~~ ** OUTER LOOP ** ~~~~ * i * === ${i}`);
 
     for (let j = 0; j < arr.length; j++) {
-      console.log(`~~~~ INNER LOOP ~~~~ j === ${j}`);
+      // console.log(`~~~~ INNER LOOP ~~~~ j === ${j}`);
       if (i !== j && arr[i] === arr[j]) {
-        unique = false;
+        uniqueNumsInArr = false;
       }
     }
   }
 
-  return unique;
+  return uniqueNumsInArr;
 };
 
-console.log(isUnique([1, 2, 3]) === true);
-console.log(isUnique([1, 1, 3]) === false);
+// console.log(isUnique([1, 2, 3]) === true);
+
+// console.log(isUnique([1, 1, 3]) === false);
+
+console.log(isUnique([11, 35, 51, 91, 82, 51])); // should return false
+console.log(isUnique([1, 1, 3])); // should return false
+console.log(isUnique([1, 2, 3])); // should return true
+console.log(isUnique([-1, 2, 3])); // should return true
