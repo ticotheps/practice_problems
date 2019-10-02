ab1274312e944e751a8ec1596f3ff2552913f481
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-01 22:36:15 -0400

    Adds starter code for 'uniqSort.js' problem

--- frontend_masters/guide_to_algorithms_with_js/uniqSort.js
+++ frontend_masters/guide_to_algorithms_with_js/uniqSort.js
@@ -6,3 +6,11 @@ It should not return any duplicate values in the sorted array.
 
 // input: [1, 5, 2, 1] => output: [1, 2, 5]
 // input: [4, 2, 2, 3, 2, 2] => output: [2, 3, 4]
+
+const uniqSort = arr => {
+  const breadcrumbs = {};
+
+  return arr.sort((a, b) => a - b);
+};
+
+uniqSort([4, 2, 2, 3, 2, 2, 2]); // => [2, 3, 4]
