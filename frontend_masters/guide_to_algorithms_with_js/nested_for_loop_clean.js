ad87f640d3f1c0ef2d221e27171e7e4a868f3dba
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-03 19:40:49 -0400

    Cleans up the comments in the 'cleaner' brute force solution

--- frontend_masters/guide_to_algorithms_with_js/nested_for_loop_clean.js
+++ frontend_masters/guide_to_algorithms_with_js/nested_for_loop_clean.js
@@ -16,19 +16,18 @@ const isUnique = arr => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (i !== j && arr[i] === arr[j]) {
-          uniqueIntegers = false;
-          return uniqueIntegers;
-        }
+        uniqueIntegers = false;
+        return uniqueIntegers;
       }
     }
   }
   return uniqueIntegers;
 };
 
-console.log(isUnique([1, 1, 3])); // should return false
-console.log(isUnique([1, 2, 3])); // should return true
-console.log(isUnique([-1, 2, 3])); // should return true
-console.log(isUnique([11, 35, 51, 91, 82, 51])); // should return false
+console.log(isUnique([1, 1, 3])); // false
+console.log(isUnique([1, 2, 3])); // true
+console.log(isUnique([-1, 2, 3])); // true
+console.log(isUnique([11, 35, 51, 91, 82, 51])); // false
 
 // console.log(isUnique([1, 2, 3]) === true);
 // console.log(isUnique([1, 1, 3]) === false);
