781f87a9baf1816d661f3f7e52122544a6a597ae
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-03 19:53:30 -0400

    Creates a new 'breadcrumbs_method_clean.js' file

---
+++ frontend_masters/guide_to_algorithms_with_js/breadcrumbs_method_clean.js
@@ -0,0 +1,29 @@
+/* 
+Expected Input: 
+  - an array of numbers
+Expected Output:  
+  - if the array contains NO duplicate numbers, return true
+  - if the array contains duplicate numbers, return false
+Constraints:
+  - integers = whole numbers
+  - integers can be negative
+*/
+// Runtime Complexity = O(n)
+// Space Complexity = O(n)
+const isUnique = arr => {
+  const breadcrumbs = {};
+  let uniqueNumsInArr = true;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (breadcrumbs[arr[i]]) {
+      uniqueNumsInArr = false;
+      return uniqueNumsInArr;
+    } else {
+      breadcrumbs[arr[i]] = true;
+    }
+  }
+  return uniqueNumsInArr;
+};
+
+console.log(isUnique([1, 1, 3])); // should return false
+// console.log(isUnique([1, 2, 3])); // should return true
