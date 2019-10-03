15550afe3e8a6593f16484ae3099fd69ce9fe50a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-03 16:19:07 -0400

    Adds more console.log() statements for more clarity

--- frontend_masters/guide_to_algorithms_with_js/breadcrumbs_method.js
+++ frontend_masters/guide_to_algorithms_with_js/breadcrumbs_method.js
@@ -15,11 +15,19 @@ const isUnique = arr => {
   let uniqueNumsInArr = true;
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`~~~LOOP~~~ i === ${i}`);
+    console.log(`\n~~~LOOP~~~ i === ${i}; arr[${i}] = ${arr[i]}`);
+    console.log('breadcrumbs =', breadcrumbs);
 
     if (breadcrumbs[arr[i]]) {
+      console.log(
+        `The value, ${arr[i]}, ALREADY DOES exist as a key in breadcrumbs`
+      );
       uniqueNumsInArr = false;
+      return uniqueNumsInArr;
     } else {
+      console.log(
+        `The value, ${arr[i]}, DOES NOT yet exist as a key in breadcrumbs`
+      );
       breadcrumbs[arr[i]] = true;
     }
   }
