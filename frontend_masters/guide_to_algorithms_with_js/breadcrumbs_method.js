fdd352a0437cbc0ec9368995a831ecd28998acd1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-03 15:58:18 -0400

    Updates 'nested_for_loop.js' and 'breadcrumbs_method.js' with space complexity analysis

--- frontend_masters/guide_to_algorithms_with_js/breadcrumbs_method.js
+++ frontend_masters/guide_to_algorithms_with_js/breadcrumbs_method.js
@@ -1,21 +1,21 @@
 /* 
 Expected Input: 
   - an array of numbers
-
 Expected Output:  
   - if the array contains NO duplicate numbers, return true
   - if the array contains duplicate numbers, return false
-  
 Constraints:
-  - none
+  - integers = whole numbers
+  - integers can be negative
 */
-
+// Runtime Complexity = O(n)
+// Space Complexity = O(n)
 const isUnique = arr => {
   const breadcrumbs = {};
   let uniqueNumsInArr = true;
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`~~~~ Loop ~~~~ i === ${i}`);
+    console.log(`~~LOOP~~ i === ${i}`);
 
     if (breadcrumbs[arr[i]]) {
       uniqueNumsInArr = false;
@@ -23,11 +23,8 @@ const isUnique = arr => {
       breadcrumbs[arr[i]] = true;
     }
   }
-
   return uniqueNumsInArr;
 };
 
 console.log(isUnique([1, 2, 3])); // should return true
 console.log(isUnique([1, 1, 3])); // should return false
-
-// Runtime Complexity of Breadcrumbs (caching) Method = O(n) + O(n log n)
--- frontend_masters/guide_to_algorithms_with_js/nested_for_loop.js
+++ frontend_masters/guide_to_algorithms_with_js/nested_for_loop.js
@@ -1,39 +1,36 @@
 /* 
 Expected Input: 
   - an array of numbers
-
 Expected Output:  
   - if the array contains NO duplicate numbers, return true
   - if the array contains duplicate numbers, return false
-  
 Constraints:
-  - none
+  - integers = whole numbers
+  - integers can be negative
 */
-
+// Runtime Complexity = O(n^2)
+// Space Complexity = O(1)
 const isUnique = arr => {
   let uniqueNumsInArr = true;
 
   for (let i = 0; i < arr.length; i++) {
-    // console.log(`~~~~ ** OUTER LOOP ** ~~~~ * i * === ${i}`);
+    console.log(`~~OUTER LOOP~~ i === ${i}`);
 
     for (let j = 0; j < arr.length; j++) {
-      // console.log(`~~~~ INNER LOOP ~~~~ j === ${j}`);
+      console.log(`**INNER LOOP** j === ${j}`);
+
       if (i !== j && arr[i] === arr[j]) {
         uniqueNumsInArr = false;
       }
     }
   }
-
   return uniqueNumsInArr;
 };
-
-// console.log(isUnique([1, 2, 3]) === true);
-
-// console.log(isUnique([1, 1, 3]) === false);
 
 console.log(isUnique([11, 35, 51, 91, 82, 51])); // should return false
 console.log(isUnique([1, 1, 3])); // should return false
 console.log(isUnique([1, 2, 3])); // should return true
 console.log(isUnique([-1, 2, 3])); // should return true
 
-// Runtime Complexity of nested 'for' loops = O(n^2); Quadratic time
+// console.log(isUnique([1, 2, 3]) === true);
+// console.log(isUnique([1, 1, 3]) === false);
