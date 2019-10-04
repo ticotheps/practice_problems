9738e7e714a0ce92a74db17f21eb99eeeae8c301
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-04 03:42:09 -0400

    Defines a function 'times10'

--- frontend_masters/guide_to_algorithms_with_js/memoization.js
+++ frontend_masters/guide_to_algorithms_with_js/memoization.js
@@ -1,6 +1,9 @@
 // Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
 // a simple multiplication fn
-const times10 = () => {};
+const times10 = n => {
+  let product = n * 10;
+  return product;
+};
 
 console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
 console.log('times10 returns:', times10(9));
@@ -9,10 +12,10 @@ console.log('times10 returns:', times10(9));
 // protip 1: Create a function that checks if the value for n has been calculated before.
 // protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
 
-const cache = {};
+// const cache = {};
 
-const memoTimes10 = n => {};
+// const memoTimes10 = n => {};
 
-console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
-console.log('Task 2 calculated value:', memoTimes10(9)); // calculated
-console.log('Task 2 cached value:', memoTimes10(9)); // cached
+// console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
+// console.log('Task 2 calculated value:', memoTimes10(9)); // calculated
+// console.log('Task 2 cached value:', memoTimes10(9)); // cached
