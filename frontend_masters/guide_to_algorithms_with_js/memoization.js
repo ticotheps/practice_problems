cbeff82bce9e1db3a7519c10fccf51a585db1d45
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-07 23:25:11 -0400

    Updates the instructions for 'Task 2'

--- frontend_masters/guide_to_algorithms_with_js/memoization.js
+++ frontend_masters/guide_to_algorithms_with_js/memoization.js
@@ -8,9 +8,11 @@ const times10 = n => {
 console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
 console.log('times10 returns:', times10(9));
 
-// Task 2: Use an object to cache the results of your times10 function.
-// protip 1: Create a function that checks if the value for n has been calculated before.
-// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+// Task 2: Use an object to cache the results of your 'times10' function.
+// *Protip 1*: Create a function that checks if the value for n has been
+//             calculated before.
+// *Protip 2*: If the value for 'n' has not been calculated, calculate
+//             it and then save that result inside the cache object.
 
 // const cache = {};
 
