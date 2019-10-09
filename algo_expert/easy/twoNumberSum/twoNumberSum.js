ab1f370f3c497b88e081077881aafdf894486e36
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-09 00:11:31 -0400

    Writes down step 1 of Polya's P.S.T.

---
+++ algo_expert/easy/twoNumberSum/twoNumberSum.js
@@ -0,0 +1,18 @@
+/* 
+Expected Inputs:
+- 2 inputs
+  - (1) type: array
+    - non-empty
+    - contains DISTINCT integer
+  - (2) type: integer
+    - represents a target sum
+
+Expected Outputs:
+- 1 output
+  - type: array
+    - contains one of the following:
+      - (a) two integers, from the input array, IF those two integers sum
+            up to the second provided input (target sum integer)
+      - (b) no contents (empty), IF no two integers from teh input array
+            sum up to the second provided input (target sum integer)
+*/
