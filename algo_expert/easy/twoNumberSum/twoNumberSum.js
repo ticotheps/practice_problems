545a826d4201a766a55156a0a72b6c47ed7ac189
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-09 00:19:22 -0400

    Creates example inputs/outputs to discover any constraints with problem

--- algo_expert/easy/twoNumberSum/twoNumberSum.js
+++ algo_expert/easy/twoNumberSum/twoNumberSum.js
@@ -15,4 +15,33 @@ Expected Outputs:
             up to the second provided input (target sum integer)
       - (b) no contents (empty), IF no two integers from teh input array
             sum up to the second provided input (target sum integer)
+
+Any Constraints?
+  - Can the integers be negative?
+    -Yes
+  - Integers: whole numbers only; no fractions
+*/
+
+/*
+Example Inputs:
+  - Input #1a: [1, 2, 3, 4, 5]
+  - Input #1b: 8
+
+  - Input #2a: [2, 4, 6, 8, 10]
+  - Input #2b: 15
+
+  - Input #3a: [-1, 1, 3, 5, 7]
+  - Input #3b: 4
+
+  - Input #4a: [-8, 22, 4, -5, 3]
+  - Input #4b: 17
+
+Example Output: 
+  - Output #1: [3, 5]
+
+  - Output #2: []
+
+  - Output #3: [-1, 5]
+
+  - Output #4: [-5, 22]
 */
