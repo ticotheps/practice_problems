ebb333fdc9f4f462e3a5d58daa1028801a0ed27c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-13 22:45:52 -0400

    Write out the expected inputs/outputs and any constraints for step 1, Understanding the Problem.

--- proyecto_eulero/problemo_dos/problemo_dos.js
+++ proyecto_eulero/problemo_dos/problemo_dos.js
@@ -1,11 +1,31 @@
 /*---------------UNDERSTANDING THE PROBLEM---------------
 - Expected Inputs:
+  - 1 input
+    - type: number
+      - positive, whole numbers only
+      - cannot be a float
+      - can be named 'n'
+    - this number, n, represents an upper-limit that an individual value 
+      in the 'fib_array' variable can have.
 
 - Expected Outputs:
+  - Only 1 output
+    - type: number
+      - can be named "evenSum"
+    - this number represents the sum of all the even numbers from the 'fib_array' variable that are less than 'n'.
 
 - Any Contstraints?
+  - The first and second values in the 'fib_array' variable are 1 & 2,
+    respectively.
 
 - Examples:
+  - Input #1: 10
+    - fib_array = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    - even_nums_fib_array = [2, 8, 34]
+  - Output #1: 44
+    - even_nums_fib_array = [2, 8, 34]
+    - evenSum = 2 + 8 + 34 = 44 
+    - evenSum = 44
 
 */
 
