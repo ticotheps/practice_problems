cb37f767c14e79bb7c384b7262174333dbb889ad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-13 22:27:45 -0400

    Writes out comments for 4 steps of Polya's P.S.T.

---
+++ proyecto_eulero/problemo_dos/problemo_dos.js
@@ -0,0 +1,33 @@
+/*---------------UNDERSTANDING THE PROBLEM---------------
+- Expected Inputs:
+
+- Expected Outputs:
+
+- Any Contstraints?
+
+- Examples:
+
+*/
+
+/*---------------DEVISING A PLAN---------------
+- BRUTE FORCE SOLUTION:
+  - Strategy:
+
+  - Pseudocode:
+
+  - Runtime Complexity: O()
+  - Space Complexity: O()
+*/
+
+//---------------IMPLEMENTING A PLAN---------------
+
+/*---------------REFLECTING/ITERATING---------------
+- Brute Force Solution Runtime Complexity: O()
+- Brute Force Solution Space Complexity: O()
+
+- Can I make my brute force solution FASTER?
+
+- How much faster & space-efficient will your new solution be?
+
+- How will you do it?
+*/
