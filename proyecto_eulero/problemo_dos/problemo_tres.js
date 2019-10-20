5357551d8437c3349734011321b29cb5f2c8f244
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-20 19:02:28 -0400

    Updates the 'Understanding the Problem' step of Polya's Problem Solving Technique (P.S.T.)

---
+++ proyecto_eulero/problemo_dos/problemo_tres.js
@@ -0,0 +1,21 @@
+/* UNDERSTANDING THE PROBLEM */
+
+/*
+- Expected Input(s): 
+  - Only ONE expected input.
+  - Type: number.
+  - Can be named 'n' to represent a given number, in which you'd like to
+    find the largest prime factor of.
+
+- Expected Output(s):
+  - Only ONE expected output.
+  - Type: number.
+  - Can be named 'largestPrimeFactor' to represent the largest prime
+    factor for the given input, n.
+*/
+
+/* DEVISING A PLAN */
+
+/* IMPLEMENTING THE PLAN */
+
+/* REFLECTING/ITERATING */
