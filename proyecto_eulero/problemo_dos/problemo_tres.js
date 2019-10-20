cd336ae19545594ca70d757ff8f627be0ba6106d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-20 19:10:18 -0400

    Updates the 'constraints' section of the 'Understanding the Problem' step of Polya's P.S.T.

--- proyecto_eulero/problemo_dos/problemo_tres.js
+++ proyecto_eulero/problemo_dos/problemo_tres.js
@@ -12,6 +12,23 @@
   - Type: number.
   - Can be named 'largestPrimeFactor' to represent the largest prime
     factor for the given input, n.
+
+- Constraints:
+  - Define a 'prime factor'?
+    - Prime Number: a number that is only divisible by 1 and itself.
+      - i.e. - 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31
+  - What is 'prime factorization'?
+    - Prime Factorization: breaking apart a number down to its lowest
+      prime factors.
+      - i.e.
+                75
+               /  \
+              3    25
+                  /  \
+              3  5    5 => 3 x 5 x 5 = 75
+  - Can it be a negative number? 
+    -
+  - Can it be a floating number?
 */
 
 /* DEVISING A PLAN */
