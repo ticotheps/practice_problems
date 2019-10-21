77bd3ef632edfab6d5a03e117d2ea8a5c74bb71b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-21 11:20:08 -0400

    Updates instructions for the problem and lays out steps for Polya's P.S.T.

---
+++ hacker_rank/practice_test/findTheNumber/findTheNumber.js
@@ -0,0 +1,15 @@
+/*----------UNDERSTANDING THE PROBLEM---------- 
+
+*/
+
+/*----------DEVISING A PLAN---------- 
+
+*/
+
+/*----------IMPLEMENTING THE PLAN---------- 
+
+*/
+
+/*----------REFLECT/ITERATE ON THE PLAN---------- 
+
+*/
