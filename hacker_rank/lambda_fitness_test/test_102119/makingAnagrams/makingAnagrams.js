253d37b1938440792a758d6123388ec1ce118ba7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-21 14:21:48 -0400

    Fills out step 1 of Polya's P.S.T.

--- hacker_rank/lambda_fitness_test/test_102119/makingAnagrams/makingAnagrams.js
+++ hacker_rank/lambda_fitness_test/test_102119/makingAnagrams/makingAnagrams.js
@@ -1,12 +1,27 @@
 /*----------UNDERSTANDING THE PROBLEM----------
+- What is an 'anagram'?
+  - A coupling of two strings that contain the exact same letters, in
+    the exact same frequencies, which allows for the first string's
+    letters to be rearranged to form the second string and vice versa.
+    
 - Expected Input(s):
+  - Number of Inputs: 2
+  - Types (respectively): String, String
+  - Names/Values (respectively): 'a', 'b'
 
 - Expected Output(s):
+  - Number of Outputs: 1
+  - Type: Number (integer)
+  - Name/Value: 'numOfDeletions'
 
 - Constraints:
+  - Any number of characters can be deleted from each string.
+  - Characters can only be deleted from either side of the string.
+  - The variable 'numOfDeletions' can't be negative or a floating number.
 
 - Examples:
-
+  - Inputs #1: 'cde', 'abc'
+  - Inputs #2: 4 (# of deletions required for both strings to reach 'c')
 
  */
 
