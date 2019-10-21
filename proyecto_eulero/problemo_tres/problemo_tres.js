45b894df5f2f9bbb72780b0379f609dd99baebe7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-21 00:46:41 -0400

    Completes first-pass solution

--- proyecto_eulero/problemo_tres/problemo_tres.js
+++ proyecto_eulero/problemo_tres/problemo_tres.js
@@ -36,7 +36,7 @@
 (1) Create function that takes in a given input, n, which is a positive
     integer.
 
-(2) Create a variable, largestPFactor, that is set equal to n. This
+(2) Create a variable, largestPFactor, that is set equal to 0. This
     number will represent the current largest prime factor while the
     second 'for' loop is evaluating all of the items in the cache. This
     variable will be returned at the end of the function.
@@ -63,28 +63,98 @@
 
 /*----------IMPLEMENTING THE PLAN----------*/
 
-const findLargestPFactor = n => {
-  let largestPFactor = n;
-  // console.log(largestPFactor);
+// const findLargestPFactor = n => {
+//   let largestPFactor = 0;
+//   // console.log(largestPFactor);
 
-  let factorsOfN = {};
-  // console.log(factorsOfN);
+//   let factorsOfN = {};
+//   // console.log(factorsOfN);
 
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0) {
-      console.log(`${i} is a factor of n! Add it to the cache!`);
-      factorsOfN[i] = false;
-      console.log(`factorsOfN[${i}] = ${factorsOfN[i]}\n`);
+//   for (let i = 2; i <= n; i++) {
+//     if (n % i === 0) {
+//       // console.log(`${i} is a factor of n! Add it to the cache!`);
+//       factorsOfN[i] = false;
+//       // console.log(`factorsOfN[${i}] = ${factorsOfN[i]}\n`);
+//     }
+//   }
+
+//   for (key in factorsOfN) {
+//     let factorCounter = 0;
+
+//     for (let j = 2; j < key; j++) {
+//       let otherFactors = [];
+//       console.log(`\nkey = ${key}`);
+//       console.log(`j = ${j}`);
+
+//       if (key % j == 0) {
+//         factorsOfN[key] = true;
+//         otherFactors.push(key);
+//         factorCounter += 1;
+//         console.log(`${key} can be divided by ${j} evenly!`);
+//       } else {
+//         console.log(`${key} CANNOT be divided by ${j} evenly!`);
+//       }
+//       console.log(`----------\nfactorCounter = ${factorCounter}\n`);
+
+//       if (factorCounter == 0) {
+//         largestPFactor = key;
+//         console.log(`${key} is a prime factor of ${n}!\n`);
+//       }
+
+//       console.log(`Other factors = ${otherFactors}`);
+//     }
+//   }
+
+//   // return largestPFactor;
+// };
+
+// console.log(findLargestPFactor(3));
+// console.log(findLargestPFactor(9));
+// console.log(findLargestPFactor(10));
+// console.log(findLargestPFactor(29));
+
+// ----NEW SOLUTION----
+const findLargestPF = n => {
+  let primeNums = {};
+  const primeArray = [];
+  let largestPrimeFactor = 0;
+
+  for (let i = n; i > 1; i--) {
+    primeNums[i] = true;
+  }
+
+  for (key in primeNums) {
+    for (let j = 2; j <= key; j++) {
+      if (key != j && key % j == 0) {
+        // console.log(`${key} is divisible by ${j}!`);
+        primeNums[key] = false;
+      }
     }
   }
 
-  // return largestPFactor;
+  for (key in primeNums) {
+    if (primeNums[key] == true) {
+      primeArray.unshift(key);
+    }
+  }
+  // console.log(`primeArray = ${primeArray}`);
+
+  for (let k = 0; k < primeArray.length; k++) {
+    if (n % primeArray[k] == 0 && primeArray[k] > largestPrimeFactor) {
+      // console.log(`${primeArray[k]} is a prime factor of ${n}!`);
+      largestPrimeFactor = primeArray[k];
+    }
+  }
+
+  // console.log(primeNums);
+  return largestPrimeFactor;
 };
 
-// console.log(findLargestPFactor(3));
-console.log(findLargestPFactor(9));
-// console.log(findLargestPFactor(10));
-// console.log(findLargestPFactor(29));
+console.log(findLargestPF(3)); // should be 3
+console.log(findLargestPF(11)); // should be 11
+console.log(findLargestPF(20)); // should be 5
+
+// console.log(findLargestPF(600851475143));
 
 /*----------REFLECTING/ITERATING----------
  */
