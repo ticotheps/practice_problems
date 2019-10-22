994fae627bbd055387d8de9d4e524b967062dbf2
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-22 00:56:08 -0400

    Provides a solution for problem 2

--- hacker_rank/lambda_fitness_test/test_102219/note_construction/note_construction.js
+++ hacker_rank/lambda_fitness_test/test_102219/note_construction/note_construction.js
@@ -1,25 +1,25 @@
 const constructNote = (magazine, note) => {
-  console.log('Magazine =', magazine);
-  console.log('Note =', note);
+  // console.log('Magazine =', magazine);
+  // console.log('Note =', note);
   let matches = 0;
 
   for (let i = 0; i < note.length; i++) {
-    console.log(`note[${i}] = ${note[i]}`);
+    // console.log(`note[${i}] = ${note[i]}`);
     for (let j = 0; j < magazine.length; j++) {
-      console.log(`magazine[${j}] = ${magazine[j]}`);
+      // console.log(`magazine[${j}] = ${magazine[j]}`);
 
       if (note[i] == magazine[j]) {
-        console.log(`We have a match!`);
+        // console.log(`We have a match!`);
         matches++;
-        console.log('Matches:', matches);
+        // console.log('Matches:', matches);
       }
     }
   }
-  console.log(matches);
+  // console.log(matches);
   if (matches == note.length) {
-    return 'Yes';
+    return true;
   } else {
-    return 'No';
+    return false;
   }
 };
 
@@ -29,5 +29,5 @@ const note_example_1 = ['give', 'one', 'grand', 'today'];
 const magazine_example_2 = ['dude', 'me', 'where', 'grand', 'today', 'night'];
 const note_example_2 = ['dude', 'car', 'where', 'my', 'is'];
 
-console.log(constructNote(magazine_example_1, note_example_1));
-console.log(constructNote(magazine_example_2, note_example_2));
+console.log(constructNote(magazine_example_1, note_example_1)); // should return true
+console.log(constructNote(magazine_example_2, note_example_2)); // should return false
