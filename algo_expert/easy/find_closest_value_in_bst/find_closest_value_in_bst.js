c8d2361ae711d666b6a449d3e8a8b6cddb8d1f1b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-30 09:36:27 -0400

    Lays out Poly's 4 steps and fills out first step in commented code

---
+++ algo_expert/easy/find_closest_value_in_bst/find_closest_value_in_bst.js
@@ -0,0 +1,50 @@
+/*----------UNDERSTANDING THE PROBLEM----------
+  - Define "BST".
+    - "BST" = Binary Search Tree
+  - Define BST "node".
+    - "BST node":
+      - Each node has 3 properties associated with it:
+        (1) "value": integer value
+        (2) "left": another BST node that must have a value that is LESS
+            THAN the value of the current BST node.
+        (3) "right": another BST node that must have a value that is
+            GREATER THAN OR EQUAL TO the value of the current BST node.
+      - Each BST node must have either: 2, 1, or 0 children nodes that
+        are BST nodes.
+        - Examples:
+          - BST node #1:
+            - "value": 12
+            - "left": None (null)
+            - "right": None (null)
+          - BST node #2:
+            - "value": 12
+            - "left": 4
+            - "right": None (null)
+          - BST node #3:
+            - "value": 12
+            - "left": None (null)
+            - "right": 15
+    
+  - Expected Input(s):
+    - Number of Inputs: 2
+    - Types of Inputs (respectively): 
+      (1) BST data structure (object of objects?).
+      (2) a Number (integer), representing the target value we're 
+          searching with.
+  
+  - Expected Output(s):
+    - Numer of Outputs: 1
+    - Type of Output: a Number (integer)
+
+  - Constraints:
+    - Assume that there will only be ONE closest value.
+
+
+/*---------------DEVISING A PLAN---------------
+ */
+
+/*------------IMPLEMENTING THE PLAN------------
+ */
+
+/*-------------REFLECTING/ITERATING------------
+ */
