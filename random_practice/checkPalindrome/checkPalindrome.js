f584ed1d898c3a5d400f4268a0c08f27ed27c5c8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-06 13:46:28 -0500

    Wrote 7 tests for the 'checkPalindrome' solution; got all 7 tests to pass

--- random_practice/checkPalindrome/checkPalindrome.js
+++ random_practice/checkPalindrome/checkPalindrome.js
@@ -1,19 +1,91 @@
+const assert = require('assert');
+
 const exampleString1 = 'Dad';
 const exampleString2 = 'aba';
 const exampleString3 = 'Dad:Tic!';
 const exampleString4 = 'rotor';
 const exampleString5 = '1009001';
 const exampleString6 = '%@#@%';
+const exampleString7 = 'a%@#@%b';
 
 function checkPalindrome(string) {
-  console.time('checkPalindrome');
-  console.log(string);
-  console.timeEnd('checkPalindrome');
+  // console.time('checkPalindrome');
+  // console.log('string = ', string);
+
+  let regEx = /[\W_]/g;
+
+  let newString = string.toLowerCase().replace(regEx, '');
+  // console.log('newString = ', newString);
+
+  let reversedString = newString
+    .split('')
+    .reverse()
+    .join('');
+  // console.log('reversedString = ', reversedString);
+
+  if (newString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-checkPalindrome(exampleString1);
-checkPalindrome(exampleString2);
-checkPalindrome(exampleString3);
-checkPalindrome(exampleString4);
-checkPalindrome(exampleString5);
-checkPalindrome(exampleString6);
+//----------TESTS----------
+
+assert.deepStrictEqual(
+  true,
+  checkPalindrome(exampleString1),
+  "'Dad' is a palindrome"
+);
+// console.log(checkPalindrome(exampleString1)); // true
+// console.timeEnd('checkPalindrome');
+
+assert.deepStrictEqual(
+  true,
+  checkPalindrome(exampleString2),
+  "'aba' is a palindrome"
+);
+// console.log(checkPalindrome(exampleString2)); // true
+// console.timeEnd('checkPalindrome');
+
+assert.deepStrictEqual(
+  false,
+  checkPalindrome(exampleString3),
+  "'Dad:Tic!' is NOT a palindrome"
+);
+// console.log(checkPalindrome(exampleString3)); // false
+// console.timeEnd('checkPalindrome');
+
+assert.deepStrictEqual(
+  true,
+  checkPalindrome(exampleString4),
+  "'rotor' is a palindrome"
+);
+// console.log(checkPalindrome(exampleString4)); // true
+// console.timeEnd('checkPalindrome');
+
+assert.deepStrictEqual(
+  true,
+  checkPalindrome(exampleString5),
+  "'1009001' is a palindrome"
+);
+// console.log(checkPalindrome(exampleString5)); // true
+// console.timeEnd('checkPalindrome');
+
+assert.deepStrictEqual(
+  true,
+  checkPalindrome(exampleString6),
+  "'%@#@%' is a palindrome"
+);
+// console.log(checkPalindrome(exampleString6)); // true
+// console.timeEnd('checkPalindrome');
+
+assert.deepStrictEqual(
+  false,
+  checkPalindrome(exampleString7),
+  "'a%@#@%b' is a palindrome"
+);
+// console.log(checkPalindrome(exampleString7)); // false
+// console.timeEnd('checkPalindrome');
+
+console.log('7 of 7 TESTS HAVE PASSED!');
