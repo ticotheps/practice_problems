645e0618bf388aafa5f20c46cc2073954d70f722
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-14 21:41:09 -0500

    Creates a solution for the 'contains()' method in the BST class

--- algo_expert/medium/bst_construction/bst_construction.js
+++ algo_expert/medium/bst_construction/bst_construction.js
@@ -34,8 +34,21 @@ class BST {
     return this;
   }
 
+  // ITERATIVE SOLUTION
+  // Average: O(log(n)) time | O(1) space
+  // Worst:  O(n) time | O(1) space
   contains(value) {
-    // Write your code here.
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
   }
 
   remove(value) {
