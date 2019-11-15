b4f3beaaf39c0a48fddce0c863a6bf8a944d4845
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-14 22:04:05 -0500

    Creates a solution for the 'remove()' method

--- algo_expert/medium/bst_construction/bst_construction.js
+++ algo_expert/medium/bst_construction/bst_construction.js
@@ -51,10 +51,55 @@ class BST {
     return false;
   }
 
-  remove(value) {
-    // Write your code here.
-    // Do not edit the return statement of this method.
+  // ITERATIVE SOLUTION
+  // Average: O(log(n)) time | O(1) space
+  // Worst:  O(n) time | O(1) space
+  remove(value, parentNode = null) {
+    let currentNode = this;
+
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left !== null && currentNode.right !== null) {
+          currentNode.value = currentNode.right.getMinValue();
+          // currentNode.value = smallest value of right subtree
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (parentNode === null) {
+          if (currentNode.left !== null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          } else {
+            currentNode.value = null;
+          }
+        } else if (parentNode.left === currentNode) {
+          parentNode.left =
+            currentNode.left !== null ? currentNode.left : currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right =
+            currentNode.right !== null ? currentNode.left : currentNode.right;
+        }
+        break;
+      }
+    }
     return this;
+  }
+
+  getMinValue() {
+    let currentNode = this;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
   }
 }
 
