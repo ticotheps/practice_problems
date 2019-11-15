b69a1c175ff47d3a9a52759cfd470b827a825ba4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-14 21:36:08 -0500

    Creates solution for insertion into BST

--- algo_expert/medium/bst_construction/bst_construction.js
+++ algo_expert/medium/bst_construction/bst_construction.js
@@ -9,9 +9,28 @@ class BST {
     this.right = null;
   }
 
+  // ITERATIVE SOLUTION
+  // Average: O(log(n)) time | O(1) space
+  // Worst:  O(n) time | O(1) space
   insert(value) {
-    // Write your code here.
-    // Do not edit the return statement of this method.
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
     return this;
   }
 
