cd8ff34da6dd1a1ae35ac1f8ba50c74ee28df432
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-20 12:26:57 -0500

    Lays out step 1 for PPolya's PST

--- euler_project/challenge_4/challenge_4.js
+++ euler_project/challenge_4/challenge_4.js
@@ -1,4 +1,23 @@
-/* UNDERSTANDING THE PROBLEM */
+/* ----------UNDERSTANDING THE PROBLEM----------
+-What is a 'palindromic number'?
+  -An integer that reads the same both ways (forwards & backwards).
+
+-Expected Input(s): 
+  -Number of Expected Inputs: 1
+    -"numDigits" = the number of digits in each integer that are 
+      multiplied together to get a product.
+
+-Expected Output(s):
+  -Number of Expected Outputs: 1
+    -"largestPal" = largest palindrome made from the product of two
+      3-digit numbers.
+
+-Any Constraints:
+  -Can the numbers be floating point numbers?
+    -No.
+  -Can the numbers be negative?
+    -No.
+*/
 
 /* DEVISING A PLAN */
 
