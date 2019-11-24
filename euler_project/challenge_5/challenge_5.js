a6a0ae5b3c7ef30d0ba8dba64a90f7d7574f7bd9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-24 17:34:23 -0500

    Creates layout for Polya's PST for challenge 5; completes step 1 of Polya's PST

---
+++ euler_project/challenge_5/challenge_5.js
@@ -0,0 +1,41 @@
+/* ----------UNDERSTANDING THE PROBLEM----------
+- Objective: Find the smallest positive number that is evenly divisible
+  by all of the numbers from 1 to 20.
+
+- Expected Input(s): 
+  - Number of Expected Inputs: 2
+  - Names of Expected Inputs: 'startNum', 'endNum' (both represent
+    a range of numbers that the output can be divide by evenly)
+  - Types of the Expected Inputs:
+    - 'startNum' => number
+    - 'endNum' => number
+
+- Expected Output(s):
+  - Number of Expected Outputs: 1
+  - Name of Expected Output: 'smallestMultiple'
+  - Type of the Expected Output: 'smallestMultiple' => number
+
+- Any Constraints?
+  - Can the output be negative?
+    - No. It must be positive.
+  - Can the numbers that define the range be negative?
+    - No. They must be positive.
+  - Can the numbers that define the range be floating point numbers?
+    - No. They must be integers.
+
+- Example #1:
+  - Example Inputs: 
+    - 'startNum' = 1
+    - 'endNum' = 10
+  - Example Output:
+    - 'smallestMultiple' = 2520
+ */
+
+/* ---------------DEVISING A PLAN---------------
+ */
+
+/* ------------IMPLEMENTING THE PLAN------------
+ */
+
+/* ------------REFLECTING/ITERATING-------------
+ */
