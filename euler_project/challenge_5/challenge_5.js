f8d550aa1508cfc68745960a0eb57d65dfb67e04
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-24 17:59:44 -0500

    Lays out the plan for step 2 of Polya's PST

--- euler_project/challenge_5/challenge_5.js
+++ euler_project/challenge_5/challenge_5.js
@@ -12,8 +12,8 @@
 
 - Expected Output(s):
   - Number of Expected Outputs: 1
-  - Name of Expected Output: 'smallestMultiple'
-  - Type of the Expected Output: 'smallestMultiple' => number
+  - Name of Expected Output: 'smallestCommonMultiple'
+  - Type of the Expected Output: 'smallestCommonMultiple' => number
 
 - Any Constraints?
   - Can the output be negative?
@@ -28,10 +28,32 @@
     - 'startNum' = 1
     - 'endNum' = 10
   - Example Output:
-    - 'smallestMultiple' = 2520
+    - 'smallestCommonMultiple' = 2520
  */
 
 /* ---------------DEVISING A PLAN---------------
+- BRUTE FORCE SOLUTION:
+  (1) Create a variable, 'smallestCommonMultiple', using a 'let'
+      statement. This variable will hold the current smallest multiple
+      that can be evenly divided by each of the numbers in the range
+      from 'startNum' to 'endNum' (inclusively).
+  (2) Use a 'for' loop to calculate multiples of the 'endNum' (the
+      largest number in the range of possible inclusive divisors).
+  (3) Create a variable, 'currentDivisor', using a 'let' statement to
+      hold the value of the current divisor being tested. Set the value
+      of 'currentDivisor' to 'endNum - 1'.
+  (4) Beginning with 'currentDivisor', evaluate each multiple to see if
+      it can be evenly divided by 'currentDivisor'.
+      (a) Use a 'while' loop that continues as long as 'multiple % 
+          currentDivisor === 0'. 
+      (b) If the multiple can be evenly divided by 'currentDivisor',
+          subtract 1 from 'currentDivisor' and evaluate whether or not
+          that 'currentDivisor' can evenly divide into the multiple.
+      (c) If that multiple cannot be evenly divided by 'currentDivisor',
+          move to the next multiple and set the value of 'currentDivisor'
+          back to 'endNum'.
+  (5) Return 'smallestCommonMultiple'.
+
  */
 
 /* ------------IMPLEMENTING THE PLAN------------
