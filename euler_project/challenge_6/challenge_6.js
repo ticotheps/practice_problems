a2225690fb6a65f9a75a0268ff9ed7bd0ad33762
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-25 09:56:26 -0500

    Lays out Polya's PST for challenge_6; completes step 1 of Polya's PST

---
+++ euler_project/challenge_6/challenge_6.js
@@ -0,0 +1,52 @@
+/*----------UNDERSTANDING THE PROBLEM----------
+- What is a 'natural number'?
+  - a positive integer that is used for counting or ordering in math.
+
+- Objective
+  - Find the difference between the SUM of the squares of the first 100
+    natural numbers and the SQUARE of the sum of the first 100 natural
+    numbers.
+  
+- Expected Inputs(s):
+  - Number of Inputs: 
+    - 2 inputs
+  - Types of Each Input (respectively): 
+    - number
+    - number
+  - Names of Inputs:
+    - 'rangeStartNum' = defines the start of the range for our natural 
+      numbers.
+    - 'rangeEndNum' = defines the end of the range for our natural 
+      numbers.
+
+- Expected Output(s):
+  - Number of Outputs:
+    - 1 output
+  - Name of Output:
+    - 'sumSquareDifference' = defines the difference between the sum of 
+      the squares of the first 100 natural numbers and the square of the
+      sum of the first 100 natural numbers.
+  - Type of Output: 
+    - number
+
+- Any Constraints?
+  - Can the numbers be negative?
+    - No. 'Natural numbers' are positive integers.
+  - Can the numbers be floating point numbers?
+    - No. 'Natural numbers' must be integers (whole numbers).
+  - How high can we expect the input 'rangeEndNum' to reach?
+    - Only expect the input 'rangeEndNum' to reach 100.
+
+- Example
+  - Example Input(s): 1, 10 (inclusive)
+  - Example Output: 2640 (3025 - 385 = 2640)
+ */
+
+/*---------------DEVISING A PLAN---------------
+ */
+
+/*------------IMPLEMENTING THE PLAN------------
+ */
+
+/*------------REFLECTING/ITERATING-------------
+ */
