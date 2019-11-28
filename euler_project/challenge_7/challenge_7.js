ea13eee933c274a3375702061f94d703724a7c02
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-28 12:19:35 -0500

    Completes sstep 1 of Polya's PST for challenge_7

--- euler_project/challenge_7/challenge_7.js
+++ euler_project/challenge_7/challenge_7.js
@@ -1,4 +1,29 @@
-/*----------UNDERSTANDING THE PROBLEM----------*/
+/*----------UNDERSTANDING THE PROBLEM----------
+- Objective: to find the 10001st prime number.
+
+- Define "prime number".
+  - a positive integer that can only be evenly divided by 1 and itself.
+
+- Expected Input(s):
+  - Number of Expected Inputs: 1
+  - Type of Expected Input: number
+  - Name of Expected Input: 'primeIndex'
+
+- Expected Output(s):
+  - Number of Expected Output: 1
+  - Type of Expected Output: number
+  - Name of Expected Output: 'primeNumAtIndex'
+
+- Any Constraints?
+  - 'primeIndex' cannot be negative & cannot be a floating point number.
+  - The input cannot be 'null' or 'undefined'. We can always expect to 
+    receive an input.
+
+- Example:
+  - Example Input: 6 (referring to the 6th prime number; assuming the 
+    first prime number is 2)
+  - Example Output: 13
+*/
 
 /*---------------DEVISING A PLAN---------------*/
 
