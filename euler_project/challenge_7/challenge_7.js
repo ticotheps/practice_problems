b62773b7d51480258f6aabf4704bf62a35b87804
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-11-29 14:05:38 -0500

    Refactored 'checkPrime()' helper function

--- euler_project/challenge_7/challenge_7.js
+++ euler_project/challenge_7/challenge_7.js
@@ -34,12 +34,12 @@
   (2) Create a function, 'findPrimeNumAtIndex()', that will take in one
       parameter, 'primeIndex', and will return one output, 
       'primeNumAtIndex'.
-      (3) Create a new variable using the 'let' keyword, 
-          'primeNumsCounter', to store a running count of all the prime 
-          numbers encountered thus far.
-      (4) Create another variable using the 'const' keyword, 
+      (3) Create a new variable using the 'const' keyword, 
           'primeNumAtIndex', that will store the value of the prime 
           number at the given 'primeIndex'.
+      (4)) Create another variable using the 'let' keyword, 
+          'primeNumsCounter', to store a running count of all the prime 
+          numbers encountered thus far.
       (5) Create a 'while' loop that will continue to run as long as
           'primeNumsCounter' is NOT equal to 'primeIndex'.
           (6) Use a 'for' loop to iterate through a range of numbers 
@@ -67,11 +67,11 @@ function checkPrime(num) {
     // console.log(`${num} is not a prime number`);
     return false;
   } else {
+    // console.log("num = ", num);
     let divisorCounter = 0;
     // console.log("divisorCounter (start) = ", divisorCounter);
 
     while (divisorCounter <= 2) {
-      // console.log("num = ", num);
       for (let i = 1; i <= num; i++) {
         // console.log("\ni = ", i);
         let remainder = num % i;
@@ -81,9 +81,14 @@ function checkPrime(num) {
           // console.log("divisorCounter (changed) = ", divisorCounter);
         }
       }
-      // 'num' has only 2 divisors (1 & itself) = it IS prime
-      return true;
+
+      if (divisorCounter === 2) {
+        // console.log(`\n${num} IS a prime number`);
+        // 'num' has only 2 divisors (1 & itself) = it IS prime
+        return true;
+      }
     }
+    // console.log(`\n${num} is NOT a prime number`);
     // 'num' has MORE THAN 2 divisors = it is NOT prime
     return false;
   }
@@ -95,15 +100,47 @@ assert.deepStrictEqual(
   false,
   "'undefined' is not a valid input"
 );
-assert.deepStrictEqual(checkPrime(0), false, "0 is NOT a prime number");
 assert.deepStrictEqual(checkPrime(-1), false, "-1 is NOT a prime number");
+assert.deepStrictEqual(checkPrime(0), false, "0 is NOT a prime number");
 assert.deepStrictEqual(checkPrime(1), false, "1 is NOT a prime number");
+assert.deepStrictEqual(checkPrime(2), true, "2 is a prime number");
 assert.deepStrictEqual(checkPrime(3), true, "3 is a prime number");
+assert.deepStrictEqual(checkPrime(4), false, "4 is NOT a prime number");
 assert.deepStrictEqual(checkPrime(5), true, "5 is a prime number");
+assert.deepStrictEqual(checkPrime(6), false, "6 is NOT a prime number");
 assert.deepStrictEqual(checkPrime(7), true, "7 is a prime number");
+assert.deepStrictEqual(checkPrime(8), false, "8 is NOT a prime number");
+assert.deepStrictEqual(checkPrime(9), false, "9 is NOT a prime number");
+assert.deepStrictEqual(checkPrime(10), false, "10 is NOT a prime number");
 assert.deepStrictEqual(checkPrime(11), true, "11 is a prime number");
+assert.deepStrictEqual(checkPrime(12), false, "12 is NOT a prime number");
 assert.deepStrictEqual(checkPrime(13), true, "13 is a prime number");
 
 console.log("***** ALL TESTS FOR 'checkPrime()' HAVE PASSED");
+
+// function findPrimeNumAtIndex(primeIndex) {
+//   let primeNumAtIndex = 0;
+//   let primeNumsCounter = 0;
+
+//   while (primeNumsCounter <= primeIndex) {
+//     console.log("primeNumsCounter = ", primeNumsCounter);
+
+//     for (let i = 1; i <= primeIndex; i++) {
+//       console.log("i = ", i);
+
+//       if (checkPrime(i) === true) {
+//         console.log(`${i} is a prime number!`);
+//         primeNumsCounter++;
+//         primeNumAtIndex = i;
+//       } else {
+//         console.log(`${i} is NOT a prime number!`);
+//       }
+//     }
+//   }
+
+//   return primeNumAtIndex;
+// }
+
+// console.log(findPrimeNumAtIndex(6));
 
 /*-------------REFLECTING/ITERATING------------*/
