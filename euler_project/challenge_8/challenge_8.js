f86711a9204754961e6586f89ab1da900c28b44d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-01 18:28:14 -0500

    Refactors BRUTE FORCE SOLUTION and wadds test cases

--- euler_project/challenge_8/challenge_8.js
+++ euler_project/challenge_8/challenge_8.js
@@ -73,14 +73,14 @@ function findGreatestAdjacentDigitsProduct(longNumString, numOfAdjacentDigits) {
     typeof longNumString !== "string" ||
     typeof numOfAdjacentDigits !== "number"
   ) {
-    console.log(
-      "Please provide two valid inputs (a 'string' & a 'number', respectively)."
-    );
+    // console.log(
+    //   "Please provide two valid inputs (a 'string' & a 'number', respectively)."
+    // );
     return undefined;
   } else if (BigInt(longNumString) < 0 || numOfAdjacentDigits < 0) {
-    console.log(
-      "Please provide two valid inputs (positive integers greater than 0)."
-    );
+    // console.log(
+    //   "Please provide two valid inputs (positive integers greater than 0)."
+    // );
     return undefined;
   } else {
     let greatestAdjacentDigitsProduct = 0;
@@ -122,17 +122,81 @@ function findGreatestAdjacentDigitsProduct(longNumString, numOfAdjacentDigits) {
 const hugeNum =
   "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 
-console.log(findGreatestAdjacentDigitsProduct(undefined, undefined)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct(undefined, 2)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct("123456", undefined)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct(null, null)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct(null, 2)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct("123456", null)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct("-123456", 2)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct("123456", -2)); // should be undefined
-console.log(findGreatestAdjacentDigitsProduct("123456", 2)); // should be 30
-console.log(findGreatestAdjacentDigitsProduct("123456789", 3)); // should be 504
-console.log(findGreatestAdjacentDigitsProduct(hugeNum, 4)); // should be 5832
-console.log(findGreatestAdjacentDigitsProduct(hugeNum, 13)); // should be 5832
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct(undefined, undefined),
+  undefined,
+  "The two valid input types are 'string' & 'number', respectively."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct(undefined, 2),
+  undefined,
+  "The two valid input types are 'string' & 'number', respectively."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct("123456", undefined),
+  undefined,
+  "The two valid input types are 'string' & 'number', respectively."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct(null, null),
+  undefined,
+  "The two valid input types are 'string' & 'number', respectively."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct(null, 2),
+  undefined,
+  "The two valid input types are 'string' & 'number', respectively."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct("123456", null),
+  undefined,
+  "The two valid input types are 'string' & 'number', respectively."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct("-123456", 2),
+  undefined,
+  "The non-strict value of both inputs must be positive."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct("123456", -2),
+  undefined,
+  "The non-strict value of both inputs must be positive."
+);
+
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct("123456", 2),
+  30,
+  "The greatest product of 2 adjacent integers in the provided 1000-digit number series is 30."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct("123456789", 3),
+  504,
+  "The greatest product of 3 adjacent integers in the provided 1000-digit number series is 504."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct(hugeNum, 4),
+  5832,
+  "The greatest product of 4 adjacent integers in the provided 1000-digit number series is 5832."
+);
+assert.deepStrictEqual(
+  findGreatestAdjacentDigitsProduct(hugeNum, 13),
+  23514624000,
+  "The greatest product of 13 adjacent integers in the provided 1000-digit number series is 23514624000."
+);
+
+console.log("***** ALL TESTS HAVE PASSED *****");
+
+// console.log(findGreatestAdjacentDigitsProduct(undefined, undefined)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct(undefined, 2)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct("123456", undefined)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct(null, null)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct(null, 2)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct("123456", null)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct("-123456", 2)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct("123456", -2)); // should be undefined
+// console.log(findGreatestAdjacentDigitsProduct("123456", 2)); // should be 30
+// console.log(findGreatestAdjacentDigitsProduct("123456789", 3)); // should be 504
+// console.log(findGreatestAdjacentDigitsProduct(hugeNum, 4)); // should be 5832
+// console.log(findGreatestAdjacentDigitsProduct(hugeNum, 13)); // should be 23514624000
 
 /*------------ REFLECTING/ITERATING -------------*/
