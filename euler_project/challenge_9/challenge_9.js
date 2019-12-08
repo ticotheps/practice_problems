18f9200b82b485b4b743982627a744dfd4dd95c4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-07 23:58:24 -0500

    Lays out the objective and defines any unclear terms

--- euler_project/challenge_9/challenge_9.js
+++ euler_project/challenge_9/challenge_9.js
@@ -1,4 +1,18 @@
-/*----------UNDERSTANDING THE PROBLEM----------*/
+/*----------UNDERSTANDING THE PROBLEM----------
+- Objective: 
+  - Find the "Pythagorean Triplet" for which a + b + c = 1000. 
+
+- Definitions:
+  - Pythagorean Triplet: a set of 3 "natural numbers" that have the
+    following characteristics:
+      (a) a < b < c
+        - three uniquely distinct positive integers
+      (b) a^2 + b^2 = c^2
+        - For the lowest 2 numbers in the set, the squares of each of
+          those numbers will add up to the square of the laregest number
+          in the set.
+
+*/
 
 /*---------------DEVISING A PLAN---------------*/
 
