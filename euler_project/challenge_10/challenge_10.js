d854120031de28ef7a909c5c29ec5fdf31a7517f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-09 00:57:01 -0500

    Creates a new direcotory for challenge_10

---
+++ euler_project/challenge_10/challenge_10.js
@@ -0,0 +1,55 @@
+/*----------UNDERSTANDING THE PROBLEM----------
+- Objective: Find the sum of all the prime numbers below 2,000,000
+
+- Definitions
+  - "prime number": a positive integer that can only be divided by 1 and
+    itself.
+
+- Expected Input(s)
+  - Number of Expected Parameters: 1
+  - Names of Expected Parameters: "limitSummationOfPrimes"
+  - Data Types of Expected Parameters: Number
+
+- Expected Output(s)
+  - Number of Expected Outputs: 1
+  - Names of Expected Output: "sumOfPrimes"
+  - Data Type of Expected Output: Number
+
+- Constraints
+  - What will make the algorithm fail?
+    - Negative numbers?
+      - Yes.
+    - Number data types?
+      - No.
+    - Floating point numbers?
+      - Yes.
+    - Non-alphanumeric characters in the inputs?
+      - Yes.
+
+- Example #1
+  - Example #1 Input: 10
+  - Example #1 Output: 17 (2 + 3 + 5 + 7 = 17)
+*/
+
+/*---------------DEVISING A PLAN---------------
+- BRUTE FORCE SOLUTION (Pseudocoded Steps)
+  (1) <Step 1 here>
+  (2) <Step 2 here>
+  (3) <Step 3 here>
+  ...
+*/
+
+/*------------IMPLEMENTING THE PLAN------------*/
+
+/*--------REFLECTING/ITERATING THE PLAN--------
+- BRUTE FORCE SOLUTION ANALYSIS
+  - Were you able to arrive at the correct answer with your solution?
+    - Yes/No
+  - Analyze the Time & Space Complexity of your solution.
+    - Time Complexity: 
+    - Space Complexity: 
+  - Could either, Time or Space Complexity, be improved in your solution?
+    - Yes/No
+  - If so, how would you go about improving it?
+  - What is the new Time & Space Complexity of your improved solution?
+*/
