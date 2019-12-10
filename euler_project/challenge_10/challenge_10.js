8c7374e6d23727b9942aef637e889abc6bba3548
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-10 10:15:38 -0500

    Completes the 'checkPrime()' helper function and adds test cases for it

--- euler_project/challenge_10/challenge_10.js
+++ euler_project/challenge_10/challenge_10.js
@@ -1,4 +1,4 @@
-/*----------UNDERSTANDING THE PROBLEM----------
+/* ----------UNDERSTANDING THE PROBLEM----------
 - Objective: Find the sum of all the prime numbers below 2,000,000
 
 - Definitions
@@ -31,63 +31,85 @@
   - Example #1 Output: 17 (2 + 3 + 5 + 7 = 17)
 */
 
-/*---------------DEVISING A PLAN---------------
+/* ---------------DEVISING A PLAN---------------
 - BRUTE FORCE SOLUTION (Pseudocoded Steps)
-  (1) Write a helper function, "checkPrime()", that will determine 
+  (1) Write a helper function, "checkPrime()", that will determine
       whether or not a number is prime.
-  (2) Create a "sumOfPrimes" variable that will be returned as the 
-      output.
-  (3) Create a function called, 'findSumOfPrimes()', that takes in 
+  (2) Create a "sumOfPrimes" variable that will be returned as the
+  output. This variable will also act as a container for the running total sum
+  of all the prime numbers below the input, "limitForSumOfPrimes".
+  (3) Create a function called, 'findSumOfPrimes()', that takes in
       one parameter, "limitForSumOfPrimes", and returns one output,
       "sumOfPrimes".
   (4) Using a 'for' loop, iterate through each number between 2 and the
       "limitForSumOfPrimes" input (inclusive).
-      (a) Call the helper function, "checkPrime()", to determine if the 
-          number is prime. 
+      (a) Call the helper function, "checkPrime()", to determine if the
+          number is prime.
           (i)   If the number is prime, add the value to the current value
                 of "sumOfPrimes".
           (ii)  If the number is NOT prime, don't do anything.
   (5) Return "sumOfPrimes".
 */
 
-/*------------IMPLEMENTING THE PLAN------------*/
-const assert = require("assert");
+/* ------------IMPLEMENTING THE PLAN------------*/
+const assert = require('assert');
 
-("use strict");
+('use strict');
 
 // Returns 'True' or 'False' to determine if a number is prime
 function checkPrime(num) {
-  console.log("num = ", num);
+  // console.log('\nnum(input) = ', num);
 
-  if (num <= 1) {
-    return false;
+  if (num === null || num === undefined || num <= 0 || typeof num !== "number" || !Number.isInteger(num)) {
+    // console.log(`Sorry, ${num} is not a valid input. Please provide a positive integer to this function.)`);
+    return undefined;
   } else {
-    for (let i = num; i > 0; i--) {
+    if (num === 1) {
+      // console.log('1 is not a prime number');
+      return false;
+    }
+    
+    if (num > 1) {
       let factorCounter = 0;
-
-      if (num % i == 0) {
-        factorCounter += 1;
-        console.log("Found another factor!");
-        console.log("factorCounter = ", factorCounter);
+      for (let i = num; i > 0; i--) {
+        if (num % i == 0) {
+          // console.log(`\n${i} IS a factor of ${num}!`);
+          factorCounter += 1;
+          // console.log('factorCounter = ', factorCounter, '\n');
+        } else {
+          // console.log(`\n${i} is NOT a factor of ${num}!`);
+          // console.log('factorCounter = ', factorCounter, '\n');
+        }
+      }
+      if (factorCounter > 2) {
+        return false;
       } else {
-        console.log(`${i} is not a factor of ${num}!`);
+        return true;
       }
     }
-    return true;
   }
-  return false;
 }
 
-console.log(checkPrime(3)); // should print True
-console.log(checkPrime(4)); // should print False
 
-/*--------REFLECTING/ITERATING THE PLAN--------
+assert.deepStrictEqual(checkPrime(3.24324), undefined, "3.24324 is not a valid input type");
+assert.deepStrictEqual(checkPrime(-3), undefined, "-3 is not a valid input type");
+assert.deepStrictEqual(checkPrime(-3.24324), undefined, "-3.24324 is not a valid input type");
+assert.deepStrictEqual(checkPrime(1), false, "1 is not a prime number");
+assert.deepStrictEqual(checkPrime(null), undefined, "'null' is not a valid input type");
+assert.deepStrictEqual(checkPrime(undefined), undefined, "'undefined' is not a valid input type");
+assert.deepStrictEqual(checkPrime(), undefined, "No input was provided");
+assert.deepStrictEqual(checkPrime(3), true, "3 is a prime number");
+assert.deepStrictEqual(checkPrime(300), false, "300 is a prime number");
+
+console.log('\n*-----ALL TESTS FOR "checkPrime()" ARE PASSING-----*\n');
+
+/* --------REFLECTING/ITERATING THE PLAN--------
 - BRUTE FORCE SOLUTION ANALYSIS
   - Were you able to arrive at the correct answer with your solution?
     - Yes/No
   - Analyze the Time & Space Complexity of your solution.
-    - Time Complexity: 
-    - Space Complexity: 
+    - Time Complexity:
+    - Space Complexity:
   - Could either, Time or Space Complexity, be improved in your solution?
     - Yes/No
   - If so, how would you go about improving it?
