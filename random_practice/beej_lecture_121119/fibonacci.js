712bf9a7e34ce6e781c8f20180e0c83611ae9313
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-11 14:51:57 -0500

    Adds a fibonacci.py file

--- random_practice/beej_lecture_121119/fibonacci.js
+++ random_practice/beej_lecture_121119/fibonacci.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-console */
 /**
  * Write a recursive algorithm for the Fibonacci sequence
  *
  * Fibonacci Sequence
- * fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+ * fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
  *
  * Each number is the sum of the previous two numbers.
  *
@@ -18,24 +19,34 @@
  *  - How performant is it?
  *    - Delivers the 1000000th (millionth) number in < 1 second.
  */
+const fibArr = []; // memoization
+function findNthFibNum(n) {
+	console.log(`n = ${n}`);
 
-function findNthFibNum(nIndex) {
-  console.log(`nIndex = ${nIndex}`);
-  const firstFibNum = 0;
-  const secondFibNum = 1;
-
-  const cache = {}; // memoization
-  console.log(`cache = ${cache}`);
-
-  if (nIndex === firstFibNum || nIndex === secondFibNum) {
-    return nIndex;
-  }
-  if (cache[nIndex] === false) {
-    cache[nIndex] = findNthFibNum(nIndex - 1) + findNthFibNum(nIndex - 2);
-  }
-  return cache;
+	if (n === 0) {
+		return 1;
+	} else if (n === 1) {
+		return 2;
+	} else {
+		return findNthFibNum(n - 1) + findNthFibNum(n - 2);
+	}
 }
 
-console.log(findNthFibNum(0));
-console.log(findNthFibNum(1));
-console.log(findNthFibNum(10));
+function printFibNum(n) {
+	for (let i = 1; i <= 11; i++) {
+		console.log(`The ${n} Fibonacci Number is: ${findNthFibNum(n)}`);
+	}
+}
+
+console.log(printFibNum(0));
+console.log(printFibNum(1));
+console.log(printFibNum(2));
+console.log(printFibNum(3));
+console.log(printFibNum(4));
+console.log(printFibNum(5));
+console.log(printFibNum(6));
+console.log(printFibNum(7));
+console.log(printFibNum(8));
+console.log(printFibNum(9));
+console.log(printFibNum(10));
+console.log(printFibNum(100));
---
+++ random_practice/beej_lecture_121119/fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+for n in range(1, 11):
+    print(n, ':', fibonacci(n))
