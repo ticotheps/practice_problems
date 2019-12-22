4ee2dffdd9adacb87e8ae4c74db1b174a8aaa5aa
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-21 22:09:58 -0500

    Creates a new directory for challenge_11

---
+++ euler_project/challenge_11/challenge_11.js
@@ -0,0 +1,67 @@
+/*----------UNDERSTANDING THE PROBLEM----------
+- Objective 
+  - We will be writing an algorithm that attempts to perform this desired
+    action to solve the presented problem.
+
+- Definitions
+  - "New Term": <some definition here>
+
+- Expected Input(s)
+  - Number of Expected Parameters
+    - <number of expected parameters here> 
+  - Names of Expected Parameters
+    - <name of parameter A here>
+    - <name of parameter B here>
+  - Data Types of Expected Parameters
+    - <data type of parameter A here>
+    - <data type of parameter B here>
+
+- Expected Output(s)
+  - Number of Expected Outputs
+    - <number of expected outputs here> 
+  - Names of Expected Output
+    - <name of output here>
+  - Data Type of Expected Output
+    - <data type of output here>
+
+- Constraints
+  - What will make the algorithm fail?
+    - Negative numbers?
+      -
+    - Number data types?
+      -
+    - Floating point numbers?
+      -
+    - Non-alphanumeric characters in the inputs?
+      -
+
+- Example #1
+  - Example #1 Input(s)
+    - <Example #1 input A here>
+    - <Example #1 input B here>
+  - Example #1 Output(s)
+    - <Example #1 output here>
+*/
+
+/*---------------DEVISING A PLAN---------------
+- BRUTE FORCE SOLUTION (Pseudocoded Steps)
+  (1) <Step 1 here>
+  (2) <Step 2 here>
+  (3) <Step 3 here>
+  ...
+*/
+
+/*------------IMPLEMENTING THE PLAN------------*/
+
+/*--------REFLECTING/ITERATING THE PLAN--------
+- BRUTE FORCE SOLUTION ANALYSIS
+  - Were you able to arrive at the correct answer with your solution?
+    - Yes/No
+  - Analyze the Time & Space Complexity of your solution.
+    - Time Complexity: 
+    - Space Complexity: 
+  - Could either, Time or Space Complexity, be improved in your solution?
+    - Yes/No
+  - If so, how would you go about improving it?
+  - What is the new Time & Space Complexity of your improved solution?
+*/
