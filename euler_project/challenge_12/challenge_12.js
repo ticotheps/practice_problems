5f1287d45e42668239cb884359b2c05ad5eb087b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-24 01:36:16 -0500

    Creates a helper function for the BRUTE FORCE SOLUTION

--- euler_project/challenge_12/challenge_12.js
+++ euler_project/challenge_12/challenge_12.js
@@ -4,43 +4,42 @@
     triangle number.
 
 - Definitions
-  - "New Term": <some definition here>
+  - "triangle number": the nth number in a sequence of numbers that is
+    determined by summing up each previous natural number (beginning with 1) all
+    the way up to the nth number.
+    - Take a look at these examples below (natural number:triangle number)
+      - 1: 1 (1)
+      - 2: 3 (1 + 2)
+      - 3: 6 (1 + 2 + 3)
+      - 4: 10 (1 + 2 + 3 + 4)
 
 - Expected Input(s)
-  - Number of Expected Parameters
-    - <number of expected parameters here> 
-  - Names of Expected Parameters
-    - <name of parameter A here>
-    - <name of parameter B here>
-  - Data Types of Expected Parameters
-    - <data type of parameter A here>
-    - <data type of parameter B here>
+  - Number of Expected Parameters: 1
+  - Name of Expected Parameter: "triNum", represents a 'nth' triangle number in
+    the sequence
+  - Data Types of Expected Parameters: Number
 
 - Expected Output(s)
-  - Number of Expected Outputs
-    - <number of expected outputs here> 
-  - Names of Expected Output
-    - <name of output here>
-  - Data Type of Expected Output
-    - <data type of output here>
+  - Number of Expected Outputs: 1
+  - Names of Expected Output: "totalDivisors"
+  - Data Type of Expected Output: Number
 
 - Constraints
   - What will make the algorithm fail?
     - Negative numbers?
-      -
+      - Yes.
     - Number data types?
-      -
+      - No.
     - Floating point numbers?
-      -
+      - Yes.
     - Non-alphanumeric characters in the inputs?
-      -
+      - Yes.
 
 - Example #1
-  - Example #1 Input(s)
-    - <Example #1 input A here>
-    - <Example #1 input B here>
-  - Example #1 Output(s)
-    - <Example #1 output here>
+  - Example #1 Input: 4
+  - Example #1 Output: 4 
+    - The 4th triangle number is 10 (1 + 2 + 3 + 4) and 10 happens to have 4
+      divisors (1, 2, 5, 10).
 */
 
 /*---------------DEVISING A PLAN---------------
@@ -52,6 +51,34 @@
 */
 
 /*------------IMPLEMENTING THE PLAN------------*/
+
+const assert = require("assert");
+
+('use strict');
+
+// helper function that finds the value of the nth triangle number from the input
+function findNthTriNum(n) {
+  if (n > 1 && Number.isInteger(n)) {
+    // console.log("n = ", n);
+    let triNum = 0;
+    for (let i = 1; i <= n; i++) {
+      triNum += i;
+      // console.log(triNum);
+    }
+
+    return triNum;
+  }
+  return "Please enter a valid value for n";
+}
+
+
+
+
+assert.deepStrictEqual(findNthTriNum(4), 10, "The 4th triangle number is 10");
+assert.deepStrictEqual(findNthTriNum(5), 15, "The 5th triangle number is 15");
+assert.deepStrictEqual(findNthTriNum(-1), "Please enter a valid value for n", "-1 is not a valid n because it is negative");
+
+console.log("*---* ALL TESTS FOR 'findNthTriNum()' PASSED *---*")
 
 /*--------REFLECTING/ITERATING THE PLAN--------
 - BRUTE FORCE SOLUTION ANALYSIS
