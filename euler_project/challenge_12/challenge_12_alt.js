1f5042576d510eb1ed548cfba30d66322d43ead1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-25 14:56:22 -0500

    Defines a new function for alternate solution

--- euler_project/challenge_12/challenge_12_alt.js
+++ euler_project/challenge_12/challenge_12_alt.js
@@ -1,7 +1,18 @@
+const triNumFactorsCache = {};
+
 function findTriNum(minNumDivisors) {
-  console.log(minNumDivisors);
+  console.log(triNumFactorsCache);
+  let currentTriNum = 0;
+  const reallyBigNum = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 1; i < reallyBigNum; i++) {
+    currentTriNum += i; 
+    console.log("currentTriNum = ", currentTriNum);
+
+    return currentTriNum;
+  }
 
   return minNumDivisors;
 }
 
-findTriNum(4);
+console.log(findTriNum(4));
