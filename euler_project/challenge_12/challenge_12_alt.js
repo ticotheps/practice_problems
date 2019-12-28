a3953c762e15a9b9d140640c620c564d3152d415
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-28 02:57:14 -0500

    Finds the answer with the BRUTE FORCE SOLUTION

--- euler_project/challenge_12/challenge_12_alt.js
+++ euler_project/challenge_12/challenge_12_alt.js
@@ -30,6 +30,10 @@ const assert = require("assert");
 
 console.time("findTriNumWithNDivsors()");
 
+// Holds all the triangle numbers that will be calculated
+const triNumsArray = [];
+let counter = 0;
+
 // Creates an array of all triangle numbers up to the designated input
 function findAllTriNums(maxNumOfTriNums) {
   // Checks to make sure input is a natural number
@@ -38,28 +42,50 @@ function findAllTriNums(maxNumOfTriNums) {
     return false;
   }
 
-  // Holds all the triangle numbers that will be calculated
-  const triNumsArray = [];
   let currentTriNum = 0;
   
-  // Calculates all triangle numbers (from '1' to the 'maxNumOfTriNums') and 
-  // adds them to the 'triNumsArray' array.
-  for (let i = 1; i <= maxNumOfTriNums; i++) {
-    currentTriNum += i; 
-    triNumsArray.push(currentTriNum);
-  }
+  // console.log("length = ", triNumsArray.length);
+  if (triNumsArray.length !== 0) {
+    // lastItem = triNumsArray.length - 1;
+    currentTriNum = triNumsArray[triNumsArray.length - 1];
+    // console.log("currentTriNum = ", currentTriNum);
 
+    // for (let j = counter + 1; j <= maxNumOfTriNums; j++) {
+    //   console.log("j = ", j);
+    //   // if (triNumsArray[j] !== currentTriNum) {
+    //     // currentTriNum = triNumsArray[triNumsArray.length - 1] + j; 
+    //     currentTriNum += j; 
+    //     triNumsArray.push(currentTriNum);
+    //     counter++;
+    //     console.log(`ADD ${currentTriNum} to the array; counter = ${counter}`);
+    //   // }
+    // }
+  }
+    // currentTriNum = 0;
+    // Calculates all triangle numbers (from '1' to the 'maxNumOfTriNums') and 
+    // adds them to the 'triNumsArray' array.
+    for (let i = counter + 1; i <= maxNumOfTriNums; i++) {
+      currentTriNum += i; 
+      triNumsArray.push(currentTriNum);
+      counter++;
+      // console.log(`Add ${currentTriNum} to the array; counter = ${counter}`);
+    }
+  // console.log("counter = ", counter);
   return triNumsArray;
 }
 
 // const triNums10Array = findAllTriNums(10);
 // console.log(triNums10Array);
+// const triNums50Array = findAllTriNums(50);
+// console.log(triNums50Array);
 // const triNums100Array = findAllTriNums(100);
 // console.log(triNums100Array);
 // const triNums1000Array = findAllTriNums(1000);
 // console.log(triNums1000Array);
 // const triNums10000Array = findAllTriNums(10000);
 // console.log(triNums10000Array);
+// const triNums20000Array = findAllTriNums(20000);
+// console.log(triNums20000Array);
 
 
 const divisorsCache = {};
@@ -85,9 +111,7 @@ function findAllDivisors(triNumsArray) {
     // console.log("divisorsCounter = ", divisorsCounter, "\n");
 
     if (!divisorsCache[divisorsCounter]) {
-      divisorsCache[divisorsCounter] = [triNumFromArray];
-    } else {
-      divisorsCache[divisorsCounter].push(triNumFromArray);
+      divisorsCache[divisorsCounter] = triNumFromArray;
     }
   }
   return divisorsCache;
@@ -110,9 +134,9 @@ function findTriNumWithNDivsors(divisorsCache, threshold) {
 
   for (key in divisorsCache) {
     if (key > threshold) {
-      console.log(`\nAt ${key} divisors, "${Number(divisorsCache[key][0])}" is the 1st triNum to have >${threshold} divisors.`);
+      console.log(`\nAt ${key} divisors, "${Number(divisorsCache[key])}" is the 1st triNum to have >${threshold} divisors.`);
 
-      triNum = Number(divisorsCache[key][0]);
+      triNum = Number(divisorsCache[key]);
     
       return triNum;
     }
@@ -134,7 +158,7 @@ function bruteForceSolution(maxNumOfTriNums, threshold) {
 
 console.log(bruteForceSolution(100, 10)); // 276 => 12 divisors
 console.log(bruteForceSolution(100, 25)); // 3570 => 32 divisors
-// console.log(bruteForceSolution(100, 50)); // false; runtime = 18.141ms
+console.log(bruteForceSolution(100, 50)); // false; runtime = 18.141ms
 
 console.log(bruteForceSolution(1000, 50)); // 64980 => 54 divisors
 console.log(bruteForceSolution(1000, 100)); // 97029 => 108 divisors
@@ -154,6 +178,10 @@ console.log(bruteForceSolution(3000, 300)); // 2162160 => 320 divisors
 
 // console.log(bruteForceSolution(6000, 500)); // false; runtime = 2:28.853 (m:ss.mmm)
 
-console.log(bruteForceSolution(10000, 500)); //
+// console.log(bruteForceSolution(10000, 500)); // false; runtime = 9:39.659
+// (m:ss.mmm)
 
+console.log(bruteForceSolution(15000, 500)); // 76576500 => 576 divisors
 console.timeEnd("findTriNumWithNDivsors()");
+
+// console.timeEnd("findTriNumWithNDivsors()");
