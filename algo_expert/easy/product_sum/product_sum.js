4630f5fd55c42ad36a0c1469c34eebb3e1e61781
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-04 12:28:39 -0500

    Partially completes step 2 of Polya's for 'product_sum'

--- algo_expert/easy/product_sum/product_sum.js
+++ algo_expert/easy/product_sum/product_sum.js
@@ -4,48 +4,62 @@
 
 - Definitions
   - Characteristics of a "special array":
-    - Non-empty
-    - Can only contain either:
-      - integers
-      - other "special arrays"
+    - The array will always be non-empty
+    - It can only contain either integers or OTHER special arrays.
 
-  - "Product Sum":
+  - "Product Sum": the value that results when all the items in the array are
+    added together and then multiplied by the level of their depth. 
+    - For example, an array containing only integers will have its sum
+      multiplied by 1 because it doesn't contain any nested OTHER special arrays
+      within it.
+    - However, if an array contains integers AND other nested special arrays,
+      the 'multiplier' for those nested special arrays will differ from the
+      multiplier used with the integers of the outermost special array.
 
 - Expected Input(s)
-  - Number of Expected Parameters: <number of expected parameters here> 
+  - Number of Expected Parameters: 1
   - Names/Data Types of Expected Parameters
-    - <name of parameter A here> (<data type of parameter A here>)
-    - <name of parameter B here> (<data type of parameter B here>)
+    - "specialArr" (an array; must only contain Number or Array data types)
 
 - Expected Output(s)
-  - Number of Expected Outputs: <number of expected outputs here> 
+  - Number of Expected Outputs: 1
   - Names/Data Types of Expected Output
-    - <name of output here> (<data type of output here>)
+    - "productSum" (a number)
 
 - Constraints
-  - What will make the algorithm fail?
+  - Will the following make the algorithm fail?
     - Negative numbers?
-      -
+      - No.
     - Number data types?
-      -
+      - No.
     - Floating point numbers?
-      -
+      - Yes.
+      - Must be integers only.
     - Non-alphanumeric characters in the inputs?
-      -
+      - Yes.
+      - Must be integers (negative or positive) only.
 
 - Example #1
-  - Example #1 Input(s)
-    - <Example #1 input A here>
-    - <Example #1 input B here>
-  - Example #1 Output(s)
-    - <Example #1 output here>
+  - Example #1 Input(s): [1, 2, 3, 4, 5]
+  - Example #1 Output(s): 15 (=> 1 * (1 + 2 + 3 + 4 + 5))
+
+- Example #2
+  - Example #2 Input(s): [1, [2, 3], 4, 5]
+  - Example #2 Output(s): 20 (=> 1 * (1 + (2 * (2 + 3)) + 4 + 5))
+
+- Example #3
+  - Example #3 Input(s): [1, [2, [3, 4], 5]]
+  - Example #3 Output(s): 57 (=> 1 * (1 + (2 * (2 + (3 * (3 + 4)) + 5)))
 */
 
 /*---------------DEVISING A PLAN---------------
-- BRUTE FORCE SOLUTION (Pseudocoded Steps)
-  (1) <Step 1 here>
-  (2) <Step 2 here>
-  (3) <Step 3 here>
+- BRUTE FORCE SOLUTION (Pseudocoded Steps) 
+  (1) Create a helper function ("findItemProductSum()") that will take in an
+      item from the special array and return its product sum.
+  (2) Create a variable ("multiplier") that will hold the value of the number
+      to be multiplied to the sum at each depth.
+  (3) Find the length of the given special array.
+  (4) Iterate through the given special array.
   ...
 */
 
