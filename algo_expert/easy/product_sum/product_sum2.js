353743cb78928e19bf9cbf458a42fffe11af61f0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-09 12:22:42 -0500

    Creates a helper function called 'findSum()'

--- algo_expert/easy/product_sum/product_sum2.js
+++ algo_expert/easy/product_sum/product_sum2.js
@@ -1,3 +1,13 @@
 /**
  * ALTERNATE SOLUTION TO THE "PRODUCT SUM" PROBLEM
  */
+
+// Helper function that calculates the sum of all items in an array
+function findSum(array) {
+	let productSum = 0;
+	console.log('array = ', array);
+
+	return productSum;
+}
+
+console.log(findSum([1, 3, 5]));
