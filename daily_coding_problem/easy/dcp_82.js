81b30b252f757c5847ad4a948f3f0118a071ed66
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-22 15:45:31 -0500

    Completes the 'UNDERSTAND' phase of Polya's problem-solving framework

--- daily_coding_problem/easy/dcp_82.js
+++ daily_coding_problem/easy/dcp_82.js
@@ -10,3 +10,22 @@
  * returns will print: "Hello w", "orld", and "".
  *
  */
+
+/**
+ * ----------UNDERSTAND----------
+ * Objective:
+ *  - Create a method that will return the first 7 characters of a passed in
+ *    string argument ('str').
+ *
+ * Expected:
+ *  - Expected Inputs: 1 input ('str'; string data type)
+ *  - Expected Output: 1 output ('chars'; string data type)
+ *
+ * Constraints:
+ *  - The 'str' argument MUST be of a string data type ONLY.
+ *  - The 'str' argument can be an empty string.
+ *
+ * -------------PLAN-------------
+ * -----------EXECUTE------------
+ * -----------REFLECT------------
+ */
