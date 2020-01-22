ac36a30900cd61abfc472c397a9188be1c768598
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-22 15:58:49 -0500

    Completes the 'PLAN' phase of Polya's problem-solving framework; begins the 'EXECUTE' phase

--- daily_coding_problem/easy/dcp_82.js
+++ daily_coding_problem/easy/dcp_82.js
@@ -24,8 +24,34 @@
  * Constraints:
  *  - The 'str' argument MUST be of a string data type ONLY.
  *  - The 'str' argument can be an empty string.
+ *  - Spaces should be treated as alphanumeric characters.
  *
  * -------------PLAN-------------
+ * BRUTE FORCE SOLUTION:
+ *  (1) Create a function called, 'read7()' that takes in 1 argument, 'str', and
+ *      returns one output, 'sevenChars'.
+ *  (2) Check the data type of the argument passed into the 'read7()' function.
+ *      (a) If it is of 'string' data type, execute the following block of code.
+ *      (b) Otherwise, return string that says "Please provide a valid input."
+ *  (3) Iterate through the given string and do the following:
+ *      (a) If 'str.length' <= 7, return the entire given string.
+ *      (b) If 'str.length' > 7, return the first 7 characters of the given string.
+ *
  * -----------EXECUTE------------
+ */
+const assert = require('assert');
+
+('use strict');
+
+function read7(str) {
+	console.log(`str = ${str}`);
+	let sevenChars;
+
+	return sevenChars;
+}
+
+console.log(read7('Tico Thepsourinthone'));
+
+/**
  * -----------REFLECT------------
  */
