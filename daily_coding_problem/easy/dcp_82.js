f3975c5b6170e41e7c55d989caab5c3057a452d1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-22 15:37:42 -0500

    Adds a new daily coding pproblem (#82)

---
+++ daily_coding_problem/easy/dcp_82.js
@@ -0,0 +1,12 @@
+/**
+ * DAILY CODING PROBLEM #82 (EASY)
+ *
+ * This problem was asked by Microsoft.
+ *
+ * Using a 'read7()' method that returns 7 characters from a file, implement
+ * 'readN(n)' which reads n characters.
+ *
+ * For example, given a file with the content "Hello world", three 'read7()'
+ * returns will print: "Hello w", "orld", and "".
+ *
+ */
