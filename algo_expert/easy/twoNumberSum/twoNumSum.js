0cc77e40fc05421abcf02e47a4d5e79c25e2fb97
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-04 14:39:11 -0500

    Creates a new 'twoNumSum.js' file

---
+++ algo_expert/easy/twoNumberSum/twoNumSum.js
@@ -0,0 +1,18 @@
+const myArr = [3, 5, -4, 8, 11, 1, -1, 6];
+
+function twoNumSum(array, targetSum) {
+	const numsCache = {};
+
+	for (let i = 0; i < array.length; i++) {
+		let potentialMatch = targetSum - array[i];
+
+		if (potentialMatch in numsCache) {
+			return [potentialMatch, array[i]];
+		} else {
+			numsCache[potentialMatch] = true;
+		}
+	}
+	return [];
+}
+
+console.log(twoNumSum(myArr, 10));
