a765e0c18883cd623161b537c8d33067042491dc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-04 15:55:58 -0500

    Refactors the solution

--- algo_expert/easy/twoNumberSum/official_twoNumberSum.js
+++ algo_expert/easy/twoNumberSum/official_twoNumberSum.js
@@ -16,8 +16,8 @@
 // 	return [];
 // }
 
-// MOST OPTIMAL SOLUTION - MEMOIZATION
-// Time Complexity = O(n)
+// SLIGHTLY OPTIMIZED SOLUTION - Sorting & Pointers
+// Time Complexity = O(n log n)
 // Space Complexity = O(n)
 function twoNumberSum(array, targetSum) {
 	const cache = {};
@@ -37,3 +37,25 @@ function twoNumberSum(array, targetSum) {
 const myArray = [3, 5, -4, 8, 11, 1, -1, 6];
 
 console.log(twoNumberSum(myArray, 10));
+
+// MOST OPTIMAL SOLUTION - MEMOIZATION
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+// function twoNumberSum(array, targetSum) {
+// 	const cache = {};
+
+// 	for (let i = 0; i < array.length; i++) {
+// 		let potentialMatch = targetSum - array[i];
+
+// 		if (potentialMatch in cache) {
+// 			return [potentialMatch, array[i]];
+// 		} else {
+// 			cache[array[i]] = true;
+// 		}
+// 	}
+// 	return [];
+// }
+
+// const myArray = [3, 5, -4, 8, 11, 1, -1, 6];
+
+// console.log(twoNumberSum(myArray, 10));
