8a387792676f3cfd37d7583ab9130509d790f3ec
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-04 20:29:18 -0500

    Updates instructions for the 'western_showdown.py' file

--- edabit/easy/western_showdown.py
+++ edabit/easy/western_showdown.py
@@ -2,5 +2,30 @@
 Wild Roger is participating in a Western Showdown, meaning he has to draw (pull
 out and shoot) his gun faster than his opponent in a gun standoff.
 
+Given two strings,"p1" and "p2", return _which_ person drew their gun the
+fastest. If both are drawn at the same time, return "tie".
 
+Examples:
+(a) 
+showdown(
+  "   Bang!        ",
+  "        Bang!   "
+) ➞ "p1"
+
+ # p1 draws his gun sooner than p2
+ 
+(b) 
+showdown(
+  "               Bang! ",
+  "             Bang!   "
+) ➞ "p2"
+
+(c)
+showdown(
+  "     Bang!   ",
+  "     Bang!   "
+) ➞ "tie"
+
+NOTES:
+Both strings are the same length.
 """
