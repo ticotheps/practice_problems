5865dad9e87e759b52cb7cc5e1b4ee962f0911e6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-04 20:47:15 -0500

    Adds instructions for the 'how_many_vowels.py' problem

---
+++ edabit/easy/how_many_vowels.py
@@ -0,0 +1,17 @@
+"""
+How Many Vowels?
+
+Create a function that takes a string and returns the number (count) of vowels
+contained within it.
+
+Examples:
+print(count_vowels("Celebration")) ➞ 5
+
+print(count_vowels("Palm")) ➞ 1
+
+print(count_vowels("Prediction")) ➞ 4
+
+NOTES:
+- The following characters are considered "vowels": a, e, i, o, u (not y).
+- All test cases are ONE word and only contain letters.
+"""
