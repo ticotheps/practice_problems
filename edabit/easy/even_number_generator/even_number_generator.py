5315ae091499a162ff3e617930dfae27aab4c1e6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-09 13:35:29 -0400

    Refactors code in 'even_number_generator.py' and in 'test_even_number_generator.py' for tests to run properly

--- edabit/easy/even_number_generator/even_number_generator.py
+++ edabit/easy/even_number_generator/even_number_generator.py
@@ -3,14 +3,14 @@ Using list comprehensions, create a function that finds all even numbers from 1
 to the given number.
 
 Examples:
-- find_even_numbers(8) -> [2, 4, 6, 8]
-- find_even_numbers(4) -> [2, 4]
-- find_even_numbers(2) -> [2]
+- find_even_nums(8) -> [2, 4, 6, 8]
+- find_even_nums(4) -> [2, 4]
+- find_even_nums(2) -> [2]
 """
 
 def find_even_nums(n):
     pass
 
-print(find_even_numbers(8))  # Should return [2, 4, 6, 8]
-print(find_even_numbers(4))  # Should return [2, 4]
-print(find_even_numbers(2))  # Should return [2]
+print(find_even_nums(8))  # Should return [2, 4, 6, 8]
+print(find_even_nums(4))  # Should return [2, 4]
+print(find_even_nums(2))  # Should return [2]
--- edabit/easy/even_number_generator/test_even_number_generator.py
+++ edabit/easy/even_number_generator/test_even_number_generator.py
@@ -4,9 +4,9 @@ from even_number_generator import find_even_nums
 class Test(unittest.TestCase):
     
     def test_even_number_generator(self):
-        self.assertEqual(find_even_sum(8), [2, 4, 6, 8])
-        self.assertEqual(find_even_sum(4), [2, 4])
-        self.assertEqual(find_even_sum(2), [2])
+        self.assertEqual(find_even_nums(8), [2, 4, 6, 8])
+        self.assertEqual(find_even_nums(4), [2, 4])
+        self.assertEqual(find_even_nums(2), [2])
         
 if __name__ == '__main__':
     unittest.main()
