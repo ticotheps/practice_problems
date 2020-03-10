82d1018d4252e323eda8a8411653e8006e3bacad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-10 13:29:14 -0400

    Defines a 'Test()' class in the 'test_nth_fibonacci.py' file

--- algo_expert/easy/nth_fibonacci/test_nth_fibonacci.py
+++ algo_expert/easy/nth_fibonacci/test_nth_fibonacci.py
@@ -1,2 +1,12 @@
 import unittest
-import 
+from nth_fibonacci import getNthFib
+
+class Test(unittest.TestCase):
+    def test_nth_fibonacci(self):
+        self.assertEqual(getNthFib(2), 1)
+        self.assertEqual(getNthFib(6), 5)
+        self.assertEqual(getNthFib(0), "Sorry. Please pass an input value greater than 0.")
+        
+if __name__ == '__main__':
+    unittest.main()
+        
