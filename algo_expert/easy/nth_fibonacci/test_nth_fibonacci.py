fcb6955941aa334dcf20c11b88c60256c181cd4c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-10 14:12:53 -0400

    Gets all 'test_nth_fibonacci.py' tests to pass

--- algo_expert/easy/nth_fibonacci/test_nth_fibonacci.py
+++ algo_expert/easy/nth_fibonacci/test_nth_fibonacci.py
@@ -4,6 +4,7 @@ from nth_fibonacci import getNthFib
 class Test(unittest.TestCase):
     def test_nth_fibonacci(self):
         self.assertEqual(getNthFib(2), 1)
+        self.assertEqual(getNthFib(4), 2)
         self.assertEqual(getNthFib(6), 5)
         self.assertEqual(getNthFib(0), f"Sorry. Please pass an input value greater than 0.")
         
