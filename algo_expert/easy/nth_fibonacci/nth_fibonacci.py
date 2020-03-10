2fdf9d449e6f86387e58876dcdc774a887edf998
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-10 14:11:14 -0400

    Refactors first-pass solution for 'nth_fibonacci.py' file

--- algo_expert/easy/nth_fibonacci/nth_fibonacci.py
+++ algo_expert/easy/nth_fibonacci/nth_fibonacci.py
@@ -11,10 +11,10 @@ def getNthFib(n):
         return f"Sorry. Please pass an input value greater than 0."
     # Check to see if 'n' == 1 (1st Fibonacci Number is 0)
     elif n == 1:
-        return f"{fibList[0]}\n"
+        return 0
     # Check to see if 'n' == 2 (2nd Fibonacci Number is 1)
     elif n == 2:
-        return f"{fibList[1]}\n"
+        return 1
     # Check to see if 'n' >= 3
     else:
         # 3rd FibNum = 2nd Index of 'fibList'
@@ -22,21 +22,13 @@ def getNthFib(n):
         startFibIndex = 2
         for i in range(startFibIndex, n):
             prev2ndNum = fibList[i - 2]
-            print(f"prev2ndNum = {prev2ndNum}")
-            
             prev1stNum = fibList[i - 1]
-            print(f"prev1stNum = {prev1stNum}")
-            
             nextFibNum = prev2ndNum + prev1stNum
-            print(f"nextFibNum = {nextFibNum}")
-            
-            print("fibList_before =", fibList)
             fibList.append(nextFibNum)
-            print("fibList_after =", fibList)
             
         # Return the last index of the 'fibList' (nth Fibonacci number)
         nthFibNum = fibList[-1]
-        return f"nthFibNum = {nthFibNum}\n"
+        return nthFibNum
 
 print(getNthFib(2))  # should print 1
 print(getNthFib(4))  # should print 2
