8baea098f240b0505276cf66549132597068692b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-10 16:44:00 -0400

    Adds instructions for 'fizzbuzz_interview_question.py'

---
+++ edabit/medium/fizzbuzz_interview_question.py/fizzbuzz_interview_question.py
@@ -0,0 +1,24 @@
+"""
+FizzBuzz Interview Question
+
+Create a function that takes a number as an argument and returns "Fizz", "Buzz"
+or "FizzBuzz".
+
+Constraints:
+    - If the number is a multiple of 3 the output should be "Fizz".
+    - If the number given is a multiple of 5, the output should be "Buzz".
+    - If the number given is a multiple of both 3 and 5, the output should be
+    "FizzBuzz".
+    - If the number is not a multiple of either 3 or 5, the number should be
+    output on its own as shown in the examples below.
+    
+Examples:
+    - fizz_buzz(3) => "Fizz"
+    - fizz_buzz(5) => "Buzz"
+    - fizz_buzz(15) => "FizzBuzz"
+    - fizz_buzz(4) => "4"
+    
+Hints:
+    - Try to be precise with how you spell things and where you put the capital
+    letters.
+"""
