b33f4451dc7524306f9e5d2578d12c2a948f0379
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-11 11:28:59 -0400

    Completes the UNDERSTANDING THE PROBLEM phase of Polya's problem-solving framework

--- edabit/medium/list_of_multiples/list_of_multiples.py
+++ edabit/medium/list_of_multiples/list_of_multiples.py
@@ -11,7 +11,32 @@ List of Multiples
     - Hints:
         - Note that our argument 'num' is also included in the returned list.
 """
+"""
+UNDERSTANDING THE PROBLEM:
+    - Objective: Create an algorithm that takes in 2 inputs ("num", "length")
+    and returns 1 output, "multiples_list", containing "length" number of
+    multiples in the list (one of which includes the "num" itself).
+    
+    - Expected Inputs/Outputs:
+        - Inputs: 2
+            - "num" (data type: integer)
+            - "length" (data type: integer)
+        - Outputs: 1
+            - "multiples_list" (data type: python list)
+            
+    - Constraints:
+        - "num" must be an positive, whole integer.
+        - "length" must be an positive, whole integer.
+        - "num" & "length" cannot be negative numbers.
+        - "num" & "length" cannot be floating point numbers.
+        
+    - Hints:
+        - Note that the "num" argument will always be the first item in the returned list.
+"""
+"""
+DEVISING A PLAN:
 
+"""
 def get_list_of_multiples (num, length):
     return num
 
