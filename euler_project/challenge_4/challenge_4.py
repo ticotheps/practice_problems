44165c3f96cc3dc844f7363d7f1193e6919fb260
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-11 22:31:38 -0400

    Creates a python solution for the 'Largest Palindrome Product' problem; also creates a test file for that solution

---
+++ euler_project/challenge_4/challenge_4.py
@@ -0,0 +1,9 @@
+def check_palindrome(num):
+    return num
+
+def find_largest_palindrome_product(num_of_digits):
+    return num_of_digits
+
+print(find_largest_palindrome_product(1))
+print(find_largest_palindrome_product(2))
+print(find_largest_palindrome_product(3))
---
+++ euler_project/challenge_4/test_challenge_4.py
@@ -0,0 +1,9 @@
+import unittest
+from challenge_4 import find_largest_palindrome_product
+
+class Test(unittest.TestCase):
+    def test_find_largest_palindrome_product(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
