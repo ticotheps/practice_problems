e8e9aeb9757e50bbf3ba8ced9e8355f1a099b6e8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-11 23:30:20 -0400

    Comes up with a partial solution for 'challenge_4.py' python implementation

--- euler_project/challenge_4/challenge_4.py
+++ euler_project/challenge_4/challenge_4.py
@@ -1,34 +1,34 @@
 def check_palindrome(num):
     # Convert 'num' into a string & store in a var called 'numStr'.
     numStr = str(num)
-    print(f"numStr = {numStr}")
+    # print(f"numStr = {numStr}")
     
     # Split 'numStr' into a list of chars & store in a var called
     # 'numStrCharsList'.
     numStrCharsList = list(numStr)
-    print(f"numStrCharsList = {numStrCharsList}")
+    # print(f"numStrCharsList = {numStrCharsList}")
     
     # Reverse the order of the chars & store in a var called
     # 'revNumStrCharsList' using a 'backward step' in Python.
     revNumStrCharsList = numStrCharsList[::-1]
-    print(f"revNumStrCharsList = {revNumStrCharsList}")
+    # print(f"revNumStrCharsList = {revNumStrCharsList}")
     
     # Join all chars in 'revNumStrList' & store in a var called 'reversedNum'
     reversedNumStr = ''.join(revNumStrCharsList)
-    print(f"reversedNumStr = {reversedNumStr}")
+    # print(f"reversedNumStr = {reversedNumStr}")
     
     # Convert string back into an integer
     reversedNumStrInt = int(reversedNumStr)
-    print(f"reversedNumStrInt = {reversedNumStrInt}")
+    # print(f"reversedNumStrInt = {reversedNumStrInt}")
     
     # Determine if 'reversedNum' is equal to 'num'
     if num == reversedNumStrInt:
         # If it is, return True.
-        print("'num' IS a palindrome of 'reversedNumStrInt'")
+        # print("'num' IS a palindrome of 'reversedNumStrInt'")
         return True
     else:
         # If not, return False.
-        print("'num' is NOT a palindrome of 'reversedNumStrInt'")
+        # print("'num' is NOT a palindrome of 'reversedNumStrInt'")
         return False     
 
 print(check_palindrome(12))  # should print False
@@ -36,9 +36,28 @@ print(check_palindrome(22))  # should print True
 print(check_palindrome(22022))  # should print True
 
 
-def find_largest_palindrome_product(num_of_digits):
-    return num_of_digits
-
-# print(find_largest_palindrome_product(1))
-# print(find_largest_palindrome_product(2))
-# print(find_largest_palindrome_product(3))
+def find_largest_palindrome_product(n_digit_numbers):
+    # Declare a var that will store the largest palindrome product to be returned
+    largest_palindrome_product = 0
+    # Declare a var that will store the single largest value in a digit to 9
+    single_largest_digit_value = 9
+    # Multiply the input by the single largest value in a digit to get the
+    # maximum of your 'for' loop's range.
+    startNumInt = int(str(single_largest_digit_value) * n_digit_numbers)
+    print(f"startNumInt = {startNumInt}")
+    
+    # Iterate through all n-digit numbers
+    for i in range(startNumInt, 0, -1):
+        # Iterate through another list of n-digit numbers
+        for j in range(startNumInt, 0, -1):
+            product = i * j
+            print(f"product = {product}")
+            if check_palindrome(product) == True:
+                if product > largest_palindrome_product:
+                    largest_palindrome_product = product
+                    print("We found the largest palindrome product!")
+                    return largest_palindrome_product
+    return "Sorry, there were no palindrome products found."
+print(find_largest_palindrome_product(1))
+print(find_largest_palindrome_product(2))
+print(find_largest_palindrome_product(3))
--- euler_project/challenge_4/test_challenge_4.py
+++ euler_project/challenge_4/test_challenge_4.py
@@ -10,7 +10,9 @@ class Test(unittest.TestCase):
         self.assertEqual(check_palindrome(22022), True)
         
     def test_find_largest_palindrome_product(self):
-        pass
+        self.assertEqual(find_largest_palindrome_product(1), 9)
+        self.assertEqual(find_largest_palindrome_product(2), 9009)
+        self.assertEqual(find_largest_palindrome_product(3), 90909)
     
 if __name__ == '__main__':
     unittest.main()
