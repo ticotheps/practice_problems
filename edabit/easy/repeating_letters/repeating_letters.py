b95feb5eb217f1a7b80998ebaf1e6b37ae6b6808
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-13 00:02:28 -0400

    Adds constraints for the 'repeating letters' problem

--- edabit/easy/repeating_letters/repeating_letters.py
+++ edabit/easy/repeating_letters/repeating_letters.py
@@ -7,4 +7,9 @@ Repeating Letters
         - double_char("String") ➞ "SSttrriinngg"
         - double_char("Hello World!") ➞ "HHeelllloo  WWoorrlldd!!"
         - double_char("1234!_ ") ➞ "11223344!!__  "
+        
+    - Constraints:
+        - All test cases contain valid strings. 
+        - Don't worry about spaces, special characters or numbers. 
+            - They're all considered valid characters.
 """
