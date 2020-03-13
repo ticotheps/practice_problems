15a8c3aa2a88589efed3a5f00bce9caacc310e9a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-13 00:00:34 -0400

    Adds example cases to the instructions for the 'repeating_letters.py' problem

---
+++ edabit/easy/repeating_letters/repeating_letters.py
@@ -0,0 +1,10 @@
+"""
+Repeating Letters
+    - Objective: Create a function that takes in a string (that may have
+    duplicates) and returns a string that does NOT have any duplicates.
+    
+    - Examples:
+        - double_char("String") ➞ "SSttrriinngg"
+        - double_char("Hello World!") ➞ "HHeelllloo  WWoorrlldd!!"
+        - double_char("1234!_ ") ➞ "11223344!!__  "
+"""
