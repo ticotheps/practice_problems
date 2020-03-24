abc148b9dec4dc5ebec940343255d05531a00497
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-23 22:15:51 -0400

    Updates the pseudocode for Projectuler problem #4

--- euler_project/challenge_4/challenge_4.js
+++ euler_project/challenge_4/challenge_4.js
@@ -10,7 +10,7 @@
 -Expected Output(s):
   -Number of Expected Outputs: 1
     -"largestPalindrome" = largest palindrome made from the product of two
-      3-digit numbers.
+      n-digit numbers.
 
 -Any Constraints:
   -Can the numbers be floating point numbers?
@@ -47,80 +47,83 @@
 */
 
 /* IMPLEMENTING THE PLAN */
-const assert = require("assert");
-("use strict");
+const assert = require('assert');
+('use strict');
 
 function checkPalindrome(num) {
-  if (num == undefined || num == null) {
-    return false;
-  }
+	if (num == undefined || num == null) {
+		return false;
+	}
 
-  let numString = Math.abs(num).toString();
-  let lenNumString = numString.length;
+	let numString = Math.abs(num).toString();
+	let lenNumString = numString.length;
 
-  if (lenNumString === 1) {
-    return true;
-  } else if (lenNumString > 1 || lenNumString < 1) {
-    let leftPointer = 0;
-    let rightPointer = lenNumString - 1;
+	if (lenNumString === 1) {
+		return true;
+	} else if (lenNumString > 1 || lenNumString < 1) {
+		let leftPointer = 0;
+		let rightPointer = lenNumString - 1;
 
-    while (leftPointer < rightPointer) {
-      if (numString[leftPointer] === numString[rightPointer]) {
-        leftPointer += 1;
-        rightPointer -= 1;
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
+		while (leftPointer < rightPointer) {
+			if (numString[leftPointer] === numString[rightPointer]) {
+				leftPointer += 1;
+				rightPointer -= 1;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 console.log("*-----* ALL TESTS FOR 'checkPalindrome()' PASSED! *-----*");
-assert.deepEqual(checkPalindrome(), false, "No input was provided");
-assert.deepEqual(checkPalindrome(0), true, "0 is palindromic");
-assert.deepEqual(checkPalindrome(1), true, "1 is palindromic");
-assert.deepEqual(checkPalindrome(2), true, "2 is palindromic");
-assert.deepEqual(checkPalindrome(23), false, "23 is NOT palindromic");
-assert.deepEqual(checkPalindrome(120), false, "120 is NOT palindromic");
-assert.deepEqual(checkPalindrome(121), true, "121 is palindromic");
-assert.deepEqual(checkPalindrome(9283), false, "9283 is NOT palindromic");
-assert.deepEqual(checkPalindrome(92829), true, "92829 is palindromic");
-assert.deepEqual(checkPalindrome(-9), true, "-9 is palindromic");
+assert.deepEqual(checkPalindrome(), false, 'No input was provided');
+assert.deepEqual(checkPalindrome(0), true, '0 is palindromic');
+assert.deepEqual(checkPalindrome(1), true, '1 is palindromic');
+assert.deepEqual(checkPalindrome(2), true, '2 is palindromic');
+assert.deepEqual(checkPalindrome(23), false, '23 is NOT palindromic');
+assert.deepEqual(checkPalindrome(120), false, '120 is NOT palindromic');
+assert.deepEqual(checkPalindrome(121), true, '121 is palindromic');
+assert.deepEqual(checkPalindrome(9283), false, '9283 is NOT palindromic');
+assert.deepEqual(checkPalindrome(92829), true, '92829 is palindromic');
+assert.deepEqual(checkPalindrome(-9), true, '-9 is palindromic');
 
 // Time Complexity: O(n^2) = quadratic
 // Space Complexity: O(1) = constant
 function findLargestPalindrome(numDigits) {
-  let largestPalindrome = 0;
-  let minRange = 10 ** (numDigits - 1);
-  let maxRange = 10 ** numDigits;
+	let largestPalindrome = 0;
+	let minRange = 10 ** (numDigits - 1);
+	let maxRange = 10 ** numDigits;
 
-  for (let i = minRange; i < maxRange; i++) {
-    for (let j = minRange; j < maxRange; j++) {
-      let product = i * j;
-      if (checkPalindrome(product) === true && product > largestPalindrome) {
-        largestPalindrome = product;
-      }
-    }
-  }
-  return largestPalindrome;
+	for (let i = minRange; i < maxRange; i++) {
+		for (let j = minRange; j < maxRange; j++) {
+			let product = i * j;
+			if (
+				checkPalindrome(product) === true &&
+				product > largestPalindrome
+			) {
+				largestPalindrome = product;
+			}
+		}
+	}
+	return largestPalindrome;
 }
 
 console.log("*-----* ALL TESTS FOR 'findLargestPalindrome()' PASSED! *-----*");
 assert.deepEqual(
-  findLargestPalindrome(1),
-  9,
-  "9 is the largest palindromic product of two 1-digit numbers"
+	findLargestPalindrome(1),
+	9,
+	'9 is the largest palindromic product of two 1-digit numbers'
 );
 assert.deepEqual(
-  findLargestPalindrome(2),
-  9009,
-  "9009 is the largest palindromic product of two 2-digit numbers"
+	findLargestPalindrome(2),
+	9009,
+	'9009 is the largest palindromic product of two 2-digit numbers'
 );
 assert.deepEqual(
-  findLargestPalindrome(3),
-  906609,
-  "906609 is the largest palindromic product of two 3-digit numbers"
+	findLargestPalindrome(3),
+	906609,
+	'906609 is the largest palindromic product of two 3-digit numbers'
 );
 
 console.log(findLargestPalindrome(3));
