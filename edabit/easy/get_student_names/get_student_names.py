c235a1ebd684fd075f3a3c7db5b55af30953edb6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-25 14:48:57 -0400

    Adds commented code to help with understanding th e problem

---
+++ edabit/easy/get_student_names/get_student_names.py
@@ -0,0 +1,35 @@
+"""
+'Get Student Names'
+
+- Objective: Create a function that takes in a dictionary of students and returns a list of
+student names in alphabetical order.
+
+- Expected Inputs & Outputs:
+    - Inputs: 
+        - Number: 1 input
+        - Data Type: dictionary
+    - Outputs:
+        - Number: 1 output
+        - Data Type: list
+        
+- Constraints:
+    - Returned list must be in alphabetical order.
+
+- Examples:
+    - Input: get_student_names({
+        "Student 1" : "Steve",
+        "Student 2" : "Becky",
+        "Student 3" : "John"
+    }) -> ["Steve", "Mike", "John"]
+"""
+
+my_dict = {
+    "Student 1" : "Tico",
+    "Student 2" : "Kayle",
+    "Student 3" : "Lucy"
+}
+
+def get_student_names(students):
+    pass
+
+print(get_student_names())
