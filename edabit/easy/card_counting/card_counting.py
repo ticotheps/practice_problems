7223348152aec74bb552c17eeb7d6ad81b58663d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-27 12:02:36 -0400

    Starts on the PLAN phase of the U.P.E.R. analysis for the 'card_counting' problem

--- edabit/easy/card_counting/card_counting.py
+++ edabit/easy/card_counting/card_counting.py
@@ -13,7 +13,7 @@ Objective:
     - Create a function/algorithm that counts the number and returns it
     from the list of cards provided.
     
-Understand:
+UNDERSTAND PHASE:
     - Define Unknown Terms:
         - None.
     - Expected Inputs & Outputs:
@@ -38,6 +38,14 @@ Understand:
         - Example 1: count([5, 9, 10, 3, "J", "A", 4, 8, 5]) -> 1
         - Example 2: count(["A", "A", "K", "Q", "Q", "J"]) -> -6
         - Example 3: count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]) -> 5
+"""
+"""
+PLAN PHASE:
+    BRUTE FORCE SOLUTION:
+    (1) Create a function, 'count()', that takes in a single argument,
+    'cards_list', and returns a single output, 'cards_count'.
+    
+    (2) Create a variable, 'cards_count', and intialize it with the value of 0. This will represent the running total count of cards in the list.
 """
 
 def count(cards_list):
