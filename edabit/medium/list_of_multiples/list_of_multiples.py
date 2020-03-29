276ea324ed1267d6a0ebac979e64ae810b5435f7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-28 23:02:11 -0400

    Refactors 'list_of_multiples()' function and test file

--- edabit/medium/list_of_multiples/list_of_multiples.py
+++ edabit/medium/list_of_multiples/list_of_multiples.py
@@ -4,9 +4,9 @@ List of Multiples
     a list of multiples of num up to length.
     
     - Examples:
-        - get_list_of_multiples(7, 5) -> [7, 14, 21, 28, 35]
-        - get_list_of_multiples(12, 10) -> [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
-        - get_list_of_multiples(17, 6) -> [17, 34, 51, 68, 85, 102]
+        - list_of_multiples(7, 5) -> [7, 14, 21, 28, 35]
+        - list_of_multiples(12, 10) -> [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+        - list_of_multiples(17, 6) -> [17, 34, 51, 68, 85, 102]
         
     - Hints:
         - Note that our argument 'num' is also included in the returned list.
@@ -52,7 +52,7 @@ DEVISING A PLAN:
 """
 # EXECUTE:
 
-def get_list_of_multiples (num, length):
+def list_of_multiples (num, length):
     multiples_list = []
     
     for i in range(1, length + 1):
--- edabit/medium/list_of_multiples/test_list_of_multiples.py
+++ edabit/medium/list_of_multiples/test_list_of_multiples.py
@@ -1,11 +1,11 @@
 import unittest
-from list_of_multiples import get_list_of_multiples
+from list_of_multiples import list_of_multiples
 
 class Test(unittest.TestCase):
-    def test_get_list_of_multiples(self):
-        self.assertEqual(get_list_of_multiples(7, 5), [7, 14, 21, 28, 35])
-        self.assertEqual(get_list_of_multiples(12, 10), [12, 24, 36, 48, 60, 72, 84, 96, 108, 120])
-        self.assertEqual(get_list_of_multiples(17, 6), [17, 34, 51, 68, 85, 102])
+    def test_list_of_multiples(self):
+        self.assertEqual(list_of_multiples(7, 5), [7, 14, 21, 28, 35])
+        self.assertEqual(list_of_multiples(12, 10), [12, 24, 36, 48, 60, 72, 84, 96, 108, 120])
+        self.assertEqual(list_of_multiples(17, 6), [17, 34, 51, 68, 85, 102])
         
 if __name__ == '__main__':
     unittest.main()
