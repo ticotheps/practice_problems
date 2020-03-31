1f5b45f2511c8e73cc23ab5e0c5427453388eea3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-31 00:07:28 -0400

    Map out objective and expected inputs & expected outputs of problem

---
+++ edabit/easy/secret_society/secret_society.py
@@ -0,0 +1,30 @@
+"""
+SECRET SOCIETY
+
+A group of friends have decided to start a secret society. The name of the
+secret society will be the fist letter of each of their names, sorted in
+alphabetical order.
+
+Objective:
+    - Create a function that takes in a list of friends' names and returns the
+    name of their secret society.
+
+Expected Inputs & Outputs:
+    - Inputs: 
+        - Number: 1
+        - Data Type: list
+        - Name: "friends_list"
+        
+    - Outputs: 
+        - Number: 1
+        - Data Type: string
+        - Name: "society_name"
+        
+
+        
+"""
+
+def create_society_name(friends_list):
+    pass
+
+print(society_name())
--- edabit/easy/secret_society/test_secret_society.py
+++ edabit/easy/secret_society/test_secret_society.py
@@ -1,9 +1,9 @@
 import unittest
-from secret_society import secret_society
+from secret_society import society_name
 
 class Test(unittest.TestCase):
     
-    def test_secret_society(self):
+    def test_society_name(self):
         self.assertEqual()
         self.assertEqual()
         self.assertEqual()
