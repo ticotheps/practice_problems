39b5151dd371de144a855a258bad78dece9e7a88
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-31 00:01:34 -0400

    Begins writing test cases for 'secret_society' problem

---
+++ edabit/easy/secret_society/test_secret_society.py
@@ -0,0 +1,12 @@
+import unittest
+from secret_society import secret_society
+
+class Test(unittest.TestCase):
+    
+    def test_secret_society(self):
+        self.assertEqual()
+        self.assertEqual()
+        self.assertEqual()
+        
+if __name__ == '__main__':
+    unittest.main()
