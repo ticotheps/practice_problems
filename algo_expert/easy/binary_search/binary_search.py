18eaa2f443ba94a086eec9c05dbdd2633855b38a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-08 04:27:55 -0400

    Completes the PLAN PHASE for the 'Binary Search' problem

--- algo_expert/easy/binary_search/binary_search.py
+++ algo_expert/easy/binary_search/binary_search.py
@@ -20,6 +20,28 @@ Defining Terms:
         target value with an O(log n) run time complexity.
 
 PLAN PHASE:
+
+    (1) Define a function that takes in two inputs ("sortedList" & "target") and returns a single output ("matchingItemIndex" or -1). 
+    
+    (2) Create a variable, "matchingItemIndex", and initialize it with a value of -1.
+    
+    (3) Create another variable, "lenOfList", and set it equal to the length of the "sortedList" input's length.
+    
+    (4) Create a new variable, "minIndex", and set it equal to the zeroith index of the list.
+    
+    (5) Create a new variable, "maxIndex", and set it equal to the last index of the list.
+    
+    (6) Create a new variable, "middleIndex", and set it equal an integer that is the average of the "minIndex" and the "maxIndex" indices (rounded to the neareast whole number).
+    
+    (7) Use an "if/else" block to compare the "target" input value to the value of "sortedList" at the "middleIndex".
+    
+        (8) If the value of "target" is equal to the value of the "sortedList" item at the "middleIndex", then set the value of "matchingItemIndex" equal to the value of "middleIndex".
+    
+        (9) Else if the value of "target" is greater than the value of the "sortedList" item at "middleIndex", set "minIndex" equal to the value of "middleIndex" and find a new "middleIndex" value using the new "minIndex" value and the previous "maxIndex" value.
+        
+        (10) Else if the value of "target" is less than the value of the "sortedList" item at "middleIndex", set "maxIndex" equal to the value of "middleIndex" and find a new "middleIndex" value using the new "maxIndex" value and the previous "minIndex" value. 
+        
+    (11) Return the value of the "matchingItemIndex".
 """
 
 # EXECUTE PHASE:
