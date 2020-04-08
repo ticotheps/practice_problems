a050ddd757bd4535d387e8c3f7f88b1c33439fb3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-07 21:03:18 -0400

    Adds the instructions for the algoexpert problem

---
+++ algo_expert/easy/binary_search/binary_search.py
@@ -0,0 +1,8 @@
+"""
+BINARY SEARCH
+
+Write a function that takes in a sorted array of integers, as well as a target
+integer. The function should use the Binary Search Algorithm to determine if the
+target integer is contained in the array and should return its index if it is,
+otherwise -1.
+"""
