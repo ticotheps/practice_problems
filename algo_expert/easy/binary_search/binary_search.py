f2b8e599f1182da0c69a4ea9ba0178c7e6154580
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-07 21:11:36 -0400

    Completes the 'objective' and the 'defining terms' sections of the UNDERSTAND PHASE of the U.P.E.R. problem-solving framework

--- algo_expert/easy/binary_search/binary_search.py
+++ algo_expert/easy/binary_search/binary_search.py
@@ -5,4 +5,28 @@ Write a function that takes in a sorted array of integers, as well as a target
 integer. The function should use the Binary Search Algorithm to determine if the
 target integer is contained in the array and should return its index if it is,
 otherwise -1.
+
+UNDERSTAND PHASE:
+
+Objective: 
+    - Create an algorithm that takes in two inputs (an array of sorted
+    integers and an integer representing a "target" value) and returns a single
+    output (an integer; either the index of the target value in the array or
+    -1).
+    
+Defining Terms:
+    - "Binary Search":
+        - An algorithm that can search through a sorted list of items to find a
+        target value with an O(log n) run time complexity.
+
+PLAN PHASE:
+"""
+
+# EXECUTE PHASE:
+
+
+
+"""
+REFLECT/REFACTOR PHASE:
+
 """
