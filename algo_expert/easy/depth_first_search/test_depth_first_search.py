9f3782acca0ae525c2f5bc34a656011d162a507f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-09 03:13:50 -0400

    Writes generic test cases in test file for 'depth_first_search' problem

---
+++ algo_expert/easy/depth_first_search/test_depth_first_search.py
@@ -0,0 +1,15 @@
+import unittest
+from depth_first_search import depthFirstSearch
+
+class Test(unittest.TestCase):
+    def test_depthFirstSearch(self):
+        self.assertEqual(depthFirstSearch(), )
+        self.assertEqual(depthFirstSearch(), )
+        self.assertEqual(depthFirstSearch(), )
+        self.assertEqual(depthFirstSearch(), )
+        self.assertEqual(depthFirstSearch(), )
+        self.assertEqual(depthFirstSearch(), )
+
+if __name__ == '__main__':
+    unittest.main()
+
