fd2979a4053e0b28ef7fcbaa1f17efb9a085e91b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-15 23:32:01 -0400

    Updates test cases for 'depth_first_search' problem

--- algo_expert/easy/depth_first_search/test_depth_first_search.py
+++ algo_expert/easy/depth_first_search/test_depth_first_search.py
@@ -3,12 +3,12 @@ from depth_first_search import depthFirstSearch
 
 class Test(unittest.TestCase):
     def test_depthFirstSearch(self):
-        self.assertEqual(depthFirstSearch(), )
-        self.assertEqual(depthFirstSearch(), )
-        self.assertEqual(depthFirstSearch(), )
-        self.assertEqual(depthFirstSearch(), )
-        self.assertEqual(depthFirstSearch(), )
-        self.assertEqual(depthFirstSearch(), )
+        self.assertEqual(depthFirstSearch([]), )
+        self.assertEqual(depthFirstSearch([]), )
+        self.assertEqual(depthFirstSearch([]), )
+        self.assertEqual(depthFirstSearch([]), )
+        self.assertEqual(depthFirstSearch([]), )
+        self.assertEqual(depthFirstSearch([]), )
 
 if __name__ == '__main__':
     unittest.main()
