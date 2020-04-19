97ad186f9ac0c1f4712c501d5ff131182bc331c5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-19 00:48:07 -0400

    Write test cases for test file of the 'string_length_sorter' problem

--- edabit/easy/string_length_sorter/test_string_length_sorter.py
+++ edabit/easy/string_length_sorter/test_string_length_sorter.py
@@ -3,11 +3,9 @@ from string_length_sorter import sort_by_length
 
 class Test(unittest.TestCase):
     def test_sort_by_length(self):
-        self.assertEqual(sort_by_length(), )
-        self.assertEqual(sort_by_length(), )
-        self.assertEqual(sort_by_length(), )
-        self.assertEqual(sort_by_length(), )
-        self.assertEqual(sort_by_length(), )
-        
+        self.assertEqual(sort_by_length(["Google", "Apple", "Microsoft"]), ['Apple', 'Google', 'Microsoft'])
+        self.assertEqual(sort_by_length(["Leonardo", "Michelangelo", "Raphael", "Donatello"]), ['Raphael', 'Leonardo', 'Donatello', 'Michelangelo'])
+        self.assertEqual(sort_by_length(["Turing", "Einstein", "Jung"]), ['Jung', 'Turing', 'Einstein'])
+
 if __name__ == '__main__':
     unittest.main()
