9bd1dee7266592580ca95cb274bc5c0f2b1f1b1a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-18 23:42:35 -0400

    Creates a new directory for the 'sort a list by string length' problem

---
+++ algo_expert/easy/string_length_sorter/test_string_length_sorter.py
@@ -0,0 +1,13 @@
+import unittest
+from string_length_sorter import string_length_sorter
+
+class Test(unittest.TestCase):
+    def test_string_length_sorter(self):
+        self.assertEqual(string_length_sorter(), )
+        self.assertEqual(string_length_sorter(), )
+        self.assertEqual(string_length_sorter(), )
+        self.assertEqual(string_length_sorter(), )
+        self.assertEqual(string_length_sorter(), )
+        
+if __name__ == '__main__':
+    unittest.main()
