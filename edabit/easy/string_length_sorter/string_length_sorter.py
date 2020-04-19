81b9a297a87eb6146248fae42b0a4e134d8e4242
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-19 00:46:31 -0400

    Completes a first-pass solution for the 'string_length_sorter' problem

--- edabit/easy/string_length_sorter/string_length_sorter.py
+++ edabit/easy/string_length_sorter/string_length_sorter.py
@@ -39,17 +39,83 @@ Examples:
 """
 PLAN PHASE:
     (1) Create a function, 'sort_by_length()', that takes in 1 parameter,
-    'strings_list', and returns a single value, 'sorted_strings_list'.
-    (2) Create a variable, 'shortest_len', that will store the integer value
-    of the shortest string's length. Initialize this variable with a value of 0.
-    (3) Use a 'for' loop to iterate through the 'strings_list' input list.
-        (4) Create a variable, 'string_len', that will store the integer value of
-            the currently iterated on item from the list.
-        (5) Evaluate whether or not the 'string_len' is greater than or less than
-            the current value of 'shortest_len'.
-            (6a)
+        'strings_list', and returns a single value, 'sorted_strings_list'.
+    
+    (2) Create a variable, 'string_len_dict', which will store a key:value 
+        pair of string:string's length. Initialize this variable with an 
+        empty dictionary.
+    
+    (3) Create a variable, 'string_len_list', that will store a new list of 
+        string lengths that have been sorted from shortest to longest. Initialize this 
+        variable with an empty Python list.
+    
+    (4) Use a 'for' loop to iterate through the 'strings_list' input list.
+    
+        (4a) Create a variable, 'string_len', that will store the integer value of
+             the currently iterated on item from the list.
+            
+        (4b) Use the '.append()' method to add 'string_len' to the 'string_len_list'.
+
+        (4c) Create a new dictionary entry for 'string_len_dict', where each string's 
+             length is the key and each string's sequence of characters is the value in
+             the 'key:value' pair.
+            
+    
+    (5) Create a variable, 'sorted_string_len_list', that will store a sorted version
+        of the 'string_len_list' using the 'sorted()' built-in Python method.
+        
+    (6) Create a variable, 'sorted_strings_list', that will store a new sorted list 
+        of actual strings from the input list.
+        
+    (7) Use a 'for' loop that to iterate through each item in the 'sorted_string_len_list'.
+        
+        (7a) Create a new variable, 'sorted_string_value', and set it equal to the 
+             retrieved string value of each iterated on 'string' item from the
+             'sorted_string_len_list'.
+             
+        (7b) Use the '.append()' method to add the 'sorted_string_value' to the 
+             'sorted_strings_list'.
+             
+    (8) Return the value of 'sorted_string_list'.
+        
+    
 """
 # EXECUTE PHASE:
+
+def sort_by_length(strings_list):
+    print(f"\nOriginal List: {strings_list}")
+    string_len_dict = dict()
+    # print(string_len_dict)
+    
+    string_len_list = []
+    # print(string_len_list)
+    
+    for string in strings_list:
+        string_len = len(string)
+        # print(string_len)
+        
+        string_len_list.append(string_len)
+        
+        string_len_dict[string_len] = string
+        # print(string_len_dict[string_len])
+        
+    sorted_string_len_list = sorted(string_len_list)
+    # print(sorted_string_len_list)
+    
+    sorted_strings_list = []
+    
+    for item in sorted_string_len_list:
+        sorted_string_value = string_len_dict[item]
+        # print(sorted_string_value)
+        
+        sorted_strings_list.append(sorted_string_value)
+        
+    print(f"New Sorted List: {sorted_strings_list}")
+    return sorted_strings_list
+
+sort_by_length(["Google", "Apple", "Microsoft"])
+sort_by_length(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+sort_by_length(["Turing", "Einstein", "Jung"])
 
 """
 REFLECT/REFACTOR PHASE:
