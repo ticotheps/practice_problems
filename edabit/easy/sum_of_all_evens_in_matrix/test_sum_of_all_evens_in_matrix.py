ab323565d7b120b83e7a291518a7ad568ac4acd8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-21 00:19:47 -0400

    Lays out test file for 'sum_of_all_evens_in_matrix'

---
+++ edabit/easy/sum_of_all_evens_in_matrix/test_sum_of_all_evens_in_matrix.py
@@ -0,0 +1,13 @@
+import unittest
+from sum_of_all_evens_in_matrix import sum_of_evens
+
+class Test(unittest.TestCase):
+    def test_sum_of_evens(self):
+        self.assertEqual(sum_of_evens(), )
+        self.assertEqual(sum_of_evens(), )
+        self.assertEqual(sum_of_evens(), )
+        self.assertEqual(sum_of_evens(), )
+        self.assertEqual(sum_of_evens(), )
+        
+if __name__ == '__main__':
+    unittest.main()
