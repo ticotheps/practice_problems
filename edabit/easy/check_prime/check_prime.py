d99295f31e6578b74f69be8c420ad68faea69b7c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-28 02:11:34 -0400

    feat(algorithm): finish first-pass solution

--- edabit/easy/check_prime/check_prime.py
+++ edabit/easy/check_prime/check_prime.py
@@ -46,6 +46,66 @@ Constraints:
 
 # PLAN & EXECUTE PHASES:
 
+def check_prime(num):
+    # Declare a variable that designates the 'prime' status of 'num' input.
+    # Initialize it with a value of True.
+    is_prime = True
+    
+    # If input is valid (!= 1, positive, & whole integer), proceed with eval.
+    if num > 1 and type(num) is int:
+        print(f"\n{num} IS a valid input")
+        
+        # Declare a variable that keeps count of the number of divisors 
+        #   that 'num' has. Initialize it with a value of 2, representing 
+        #   the following divisors: 1, itself.
+        num_of_divisors = 2
+        print(f"(START) num_of_divisors for {num} = {num_of_divisors}")
+        
+        # Use a 'for' loop to iterate through a range of numbers that begin
+        #   with '2' and end with 'num + 1' because '1' and 'num' have already
+        #   been accounted for as divisors in the 'num_of_divisors' variable.
+        for possible_divisor in range(2, num):
+            # print(f"possible_divisor = {possible_divisor}")
+            
+            # Only continue evaluating if 'num_of_divisors' is not > 2.
+            if num_of_divisors > 2:
+                # If 'num_of_divisors' >2, change value of 'is_prime' to False.
+                is_prime = False 
+                
+                # Return the value of 'is_prime' because no prime number has >2
+                #   divisors.
+                return is_prime
+            # Otherwise, continue evaluating for other possible divisors for 'num'.
+            else:
+                # Check to see if 'num ' is evenly divisible by 'possible_divisor'.
+                # If 'num' is evenly divisible by 'possible_divisor'...
+                if num % possible_divisor == 0:
+                    print(f"{num} IS evenly divisible by {possible_divisor}!")
+                    
+                    # ...add 1 to the value of 'num_of_divisors'.
+                    num_of_divisors += 1   
+                    print(f"(*STOP EVAL*) num_of_divisors for {num} = {num_of_divisors}") 
+        
+        print(f"(END) num_of_divisors = {num_of_divisors}") 
+                      
+    # If input is invalid, print a string saying input is invalid.
+    else:
+        print(f"\n{num} is NOT a valid input")
+        # Change value of 'is_prime' to False.
+        is_prime = False
+        
+    # Return the value of 'is_prime'
+    return is_prime
+
+
+print(check_prime(1)) # should print False  
+print(check_prime(-31)) # should print False    
+print(check_prime(-3.1)) # should print False
+print(check_prime(3.1)) # should print False
+
+print(check_prime(31)) # should print True
+print(check_prime(18)) # should print False
+print(check_prime(11)) # should print True
 
 """
 REFLECT/REFACTOR PHASE:
