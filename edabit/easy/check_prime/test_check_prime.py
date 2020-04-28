b9ce6121a8041c73729c69e975eadd274b1fb110
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-28 02:14:55 -0400

    test(brute force solution): add missing test case for num = 1

--- edabit/easy/check_prime/test_check_prime.py
+++ edabit/easy/check_prime/test_check_prime.py
@@ -3,6 +3,7 @@ from check_prime import check_prime
 
 class Test(unittest.TestCase):
     def test_check_prime(self):
+        self.assertEqual(check_prime(1), False)
         self.assertEqual(check_prime(31), True)
         self.assertEqual(check_prime(-31), False)
         self.assertEqual(check_prime(-3.1), False)
