e5ffb480ba9b98f13db4746a7d596d652a49dc60
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-28 00:46:54 -0400

    Writes out test cases for 'check if a number is prime' problem

---
+++ edabit/easy/check_prime/test_check_prime.py
@@ -0,0 +1,15 @@
+import unittest
+from check_prime import check_prime
+
+class Test(unittest.TestCase):
+    def test_check_prime(self):
+        self.assertEqual(check_prime(31), True)
+        self.assertEqual(check_prime(-31), False)
+        self.assertEqual(check_prime(-3.1), False)
+        self.assertEqual(check_prime(3.1), False)
+        self.assertEqual(check_prime(18), False)
+        self.assertEqual(check_prime(11), True)
+        
+if __name__ == "__main__":
+    unittest.main()
+        
