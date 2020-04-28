e91fada759a078fad3b6d1f0059b0972e4ee7c16
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-28 00:51:15 -0400

    Lays out the problem, the objective, and some examples

---
+++ edabit/easy/check_prime/check_prime.py
@@ -0,0 +1,30 @@
+"""
+CHECK IF A NUMBER IS PRIME
+
+Objective:
+    - Create a function that outputs True if a number is prime and False
+        otherwise.
+        
+Examples:
+    - check_prime(31) -> True
+    - check_prime(18) -> False
+    - check_prime(11) -> False
+    
+Notes:
+    - A prime number has no other factors except 1 and itself.
+    - 1 is NOT considered a prime number.
+"""
+
+"""
+UNDERSTAND PHASE:
+
+"""
+
+# PLAN & EXECUTE PHASES:
+
+
+"""
+REFLECT/REFACTOR PHASE:
+
+
+"""
