89fabc6f07313349177bddbb953397107dd957ab
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-30 00:11:01 -0400

    test(edabit): write test cases

--- edabit/easy/filter_out_strings/filter_out_strings.py
+++ edabit/easy/filter_out_strings/filter_out_strings.py
@@ -13,3 +13,23 @@ Examples:
 Notes:
     - Zero is a non-negative integer.
 """
+
+"""
+UNDERSTAND PHASE
+
+
+"""
+
+# PLAN + EXECUTE PHASES
+
+def filter_out_strings(input_list):
+    return input_list
+
+print(filter_out_strings([1, 2, "a", "b"])) # should print [1, 2]
+print(filter_out_strings([1, "a", "b", 0, 15])) # should print [1, 0, 15]
+print(filter_out_strings([1, 2, "aasf", "1", "123", 123])) # should print [1, 2, 123]
+
+"""
+REFLECT/REFACTOR PHASE
+
+"""
---
+++ edabit/easy/filter_out_strings/test_filter_out_strings.py
@@ -0,0 +1,11 @@
+import unittest
+from filter_out_strings import filter_out_strings
+
+class Test(unittest.TestCase):
+    def test_filter_out_strings(self):
+        self.assertEqual(filter_out_strings([1, 2, "a", "b"]), [1, 2])
+        self.assertEqual(filter_out_strings([1, "a", "b", 0, 15]), [1, 0, 15])
+        self.assertEqual(filter_out_strings([1, 2, "aasf", "1", "123", 123]), [1, 2, 123])
+        
+if __name__ == '__main__':
+    unittest.main()
