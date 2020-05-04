e101ffce95a3c3697067770d2c7a62fe1e1a0d78
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-04 05:39:29 -0400

    feat(edabit): add code challenge prompt and details

---
+++ edabit/medium/a_circle_two_squares/a_circle_two_squares.py
@@ -0,0 +1,18 @@
+"""
+A CIRCLE AND TWO SQUARES
+
+Imagine a circle and two squares: a smaller and a bigger one. For the smaller
+one, the circle is a circumcircle and for the bigger one, an incircle.
+
+Objective: 
+    - Create a function that takes in an integer (radius of the circle) and returns
+    the square area of the square inside the circle.add()
+
+Examples:
+    - square_areas_difference(5) -> 50
+    - square_areas_difference(6) -> 72
+    - square_areas_difference(7) -> 98
+    
+Notes:
+    - Use only positive integer parameters.
+"""
