84a1b85c7c34ed0417288eb5bc5be149aaa55e05
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-05 01:55:39 -0400

    feat(edabit): Add the code challenge prompt, the objective, the examples, and the notes

---
+++ edabit/medium/calculate_the_profit/calculate_the_profit.py
@@ -0,0 +1,36 @@
+"""
+CALCULATE THE PROFIT
+
+You work for a manufacturer, and have been asked to calculate the total profit
+made on the sales of product. You are given a dictionary containing the cost
+price per unit (in dollars), the sell price per unit (in dollars), and the
+starting inventory. Return the total profit made, rounded to the nearest dollar.
+Assume all inventory has been sold.
+
+Objective:
+    - Write an algorithm that will take in a single Python dictionary and
+        return a single integer, representing the number of dollars made in
+        profit.
+        
+Examples:
+    - calculate_the_profit({
+        "cost_price": 32.67,
+        "sell_price": 45.00,
+        "inventory": 1200,
+    }) -> 14796
+    
+    - calculate_the_profit({
+        "cost_price": 225.89,
+        "sell_price": 550.00,
+        "inventory": 100,
+    }) -> 32411
+    
+    - calculate_the_profit({
+        "cost_price": 2.77,
+        "sell_price": 7.95,
+        "inventory": 8500,
+    }) -> 44030
+    
+Notes:
+    - Profit = Total Sales - Total Cost
+"""
