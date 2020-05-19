608f1cb5d07ad28d706bbfbaff7d59b5bae68d28
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-19 10:35:20 -0400

    feat(challenge prompt): add description of 'Drunken Python' challenge prompt

---
+++ edabit/medium/drunken_python/drunken_python.py
@@ -0,0 +1,31 @@
+"""
+Drunken Python
+
+Python got drunk and built-in functions 'str()' and 'int()' are acting odd:
+
+str(4) -> 4
+str("4") -> 4
+int("4") -> "4"
+int(4) -> "4"
+
+You need to create two functions to substitute 'str()' and 'int()'. A function
+called 'int_to_str()' that converts integers into strings and a function called
+'str_to_in()' that converts strings into integers.
+
+Examples:
+    - int_to_str(4) -> "4"
+    - str_to_int("4") -> 4
+    - int_to_str(29348) -> "29348"
+    
+Notes:
+    - This is meant to illustrate the dangers of using already-existing function
+        names.
+    - Extra points if you can de-drunk Python.
+"""
+
+"""
+PHASE I: UNDERSTAND THE PROBLEM
+
+Objective:
+    -
+"""
