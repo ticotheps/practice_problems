ddd55f27e2693c10198cbd9e083934ca40e0efdd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-19 11:25:36 -0400

    feat: add planned out steps for creating a first-pass solution

--- edabit/medium/drunken_python/drunken_python.py
+++ edabit/medium/drunken_python/drunken_python.py
@@ -24,11 +24,11 @@ Notes:
 """
 
 """
-PHASE I: UNDERSTAND THE PROBLEM
+PHASE I (UNDERSTAND THE PROBLEM)
 
 Objectives:
-    - Create two new algorithms that will substitute for the built-in Python functions 'str()'
-    and 'int()'. 
+    - Create two new algorithms that will substitute for the already built-in
+    Python functions 'str()' and 'int()'. 
     - The 'int_to_str()' function will take in a single input, 'num_int',
     and will return a single output, 'num_str'.
     - The 'str_to_int()' function will take in a single input, 'num_str', and
@@ -79,4 +79,47 @@ Constraints:
             - empty?
                 - No. 
                 - Input must exist.
+"""
+
+# PHASE II (DEVISE A PLAN)
+# PHASE III (IMPLEMENT THE PLAN)
+
+def int_to_str(num_int):
+    # Initiate a var to be returned as the output (of 'str' data type).
+    num_str = f"{num_int}"
+    
+    # Check for valid input
+    # If input is invalid, provide an explanation as to why it is invalid.
+    if type(num_int) != int:
+        return """Please enter a whole number for the 'num_int' input instead of a floating point number"""
+    
+    # If input is valid, return the converted input (of 'str' data type).
+    else:
+        print(f"type(num_str) = {type(num_str)}")
+        return num_str
+
+def str_to_int(num_str):
+    # Check for valid input
+    # If input is valid, return the converted input (of 'int' data type).
+    try:
+        num_int = int(num_str)
+        print(f"type(num_int) = {type(num_int)}")
+        return num_int
+    
+    # If input is invalid, provide an explanation as to why it is invalid.
+    except:
+        num_float = float(num_str)
+        print(f"type(num_float) = {type(num_float)}")
+        return """Please enter a string with a whole number value for the 'num_str' input instead of a string witha floating point number value inside of it"""
+
+print(int_to_str(4)) # -> '4'
+print(int_to_str(-4)) # -> '-4'
+print(int_to_str(0.4)) # -> 'Please enter a valid input'
+
+print(str_to_int('4')) # -> 4
+print(str_to_int('-4')) # -> -4
+print(str_to_int('0.4')) # -> 'Please enter a valid input'
+
+"""
+PHASE IV (REFLECT ON & REFACTOR THE PLAN + THE IMPLEMENTATION)
 """
