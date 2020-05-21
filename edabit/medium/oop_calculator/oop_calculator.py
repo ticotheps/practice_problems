6a7ed0c1639a036512a49443d09a14c51cade0eb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-21 10:47:49 -0400

    feat(solution): create a first-pass solution

--- edabit/medium/oop_calculator/oop_calculator.py
+++ edabit/medium/oop_calculator/oop_calculator.py
@@ -14,3 +14,30 @@ Examples:
     - calculator.multiply(10,5) -> 50
     - calculator.divide(10,5) -> 2
 """
+
+class Calculator:
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
+    
+    def add(self, num1, num2):
+        return num1 + num2
+    
+    def subtract(self, num1, num2):
+        return num1 - num2
+    
+    def multiply(self, num1, num2):
+        return num1 * num2
+    
+    def divide(self, num1, num2):
+        return num1 // num2
+    
+calc = Calculator('calc')
+print(calc)
+
+print(calc.add(10, 5))  # should be 3
+print(calc.subtract(10, 5))  # should be 5
+print(calc.multiply(10, 5))  # should be 50
+print(calc.divide(10, 5))  # should be 2
