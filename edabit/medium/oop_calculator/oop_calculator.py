d049b90d0eb7acc58e0517da77d554df3a2980e9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-20 23:30:38 -0400

    feat(edabit): write the prompt for the 'oop_calculator' problem

---
+++ edabit/medium/oop_calculator/oop_calculator.py
@@ -0,0 +1,16 @@
+"""
+SIMPLE OOP CALCULATOR
+
+Create methods for the calculator class that can do the following:
+    - Add two numbers.
+    - Subtract two numbers.
+    - Multiply two numbers.
+    - Divide two numbers. 
+
+Examples:
+    - calculator = Calculator()
+    - calculator.add(10,5) -> 15
+    - calculator.subtract(10,5) -> 5
+    - calculator.multiply(10,5) -> 50
+    - calculator.divide(10,5) -> 2
+"""
