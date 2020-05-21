bf737f69a11dc807b60f710777386428157c477a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-21 10:55:37 -0400

    test: all test cases in 'test_divide()' pass

--- edabit/medium/oop_calculator/test_oop_calculator.py
+++ edabit/medium/oop_calculator/test_oop_calculator.py
@@ -19,15 +19,10 @@ class Test(unittest.TestCase):
         self.assertEqual(calculator.multiply(10, -5), -50)
         self.assertEqual(calculator.multiply(-10, -5), 50)
 
-    # def test_divide(self):
-    #     self.assertEqual(divide(6,3), 2)
-    #     self.assertEqual(divide(6,-3), -2)
-    #     self.assertEqual(divide(-6,-3), 2)
+    def test_divide(self):
+        self.assertEqual(calculator.divide(10, 5), 2)
+        self.assertEqual(calculator.divide(10, -5), -2)
+        self.assertEqual(calculator.divide(-10, -5), 2)
         
 if __name__ == '__main__':
     unittest.main()
-    
-# print(calc.add(10, 5))  # should be 15
-# print(calc.subtract(10, 5))  # should be 5
-# print(calc.multiply(10, 5))  # should be 50
-# print(calc.divide(10, 5))  # should be 2
