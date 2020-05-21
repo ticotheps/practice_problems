081054d05b6e5c5b6bf35a1fdccad3ac9c9d787f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-21 10:52:16 -0400

    test(tests): all test cases in 'test_add()' pass

--- edabit/medium/oop_calculator/test_oop_calculator.py
+++ edabit/medium/oop_calculator/test_oop_calculator.py
@@ -1,26 +1,33 @@
 import unittest
-from oop_calculator import add, subtract, multiply, divide
+from oop_calculator import Calculator
+
+calculator = Calculator('calculator')
 
 class Test(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1,2), 3)
-        self.assertEqual(add(-1,2), 1)
-        self.assertEqual(add(-1,-2), -3)
+        self.assertEqual(calculator.add(10, 5), 15)
+        self.assertEqual(calculator.add(10, -5), 5)
+        self.assertEqual(calculator.add(-10, -5), -15)
 
-    def test_subtract(self):
-        self.assertEqual(subtract(2,1), 1)
-        self.assertEqual(subtract(2,-1), 3)
-        self.assertEqual(subtract(-2,1), -3)
+    # def test_subtract(self):
+    #     self.assertEqual(subtract(2,1), 1)
+    #     self.assertEqual(subtract(2,-1), 3)
+    #     self.assertEqual(subtract(-2,1), -3)
 
-    def test_multiply(self):
-        self.assertEqual(multiply(2,3), 6)
-        self.assertEqual(multiply(2,-3), -6)
-        self.assertEqual(multiply(=2,-3), 6)
+    # def test_multiply(self):
+    #     self.assertEqual(multiply(2,3), 6)
+    #     self.assertEqual(multiply(2,-3), -6)
+    #     self.assertEqual(multiply(=2,-3), 6)
 
-    def test_divide(self):
-        self.assertEqual(divide(6,3), 2)
-        self.assertEqual(divide(6,-3), -2)
-        self.assertEqual(divide(-6,-3), 2)
+    # def test_divide(self):
+    #     self.assertEqual(divide(6,3), 2)
+    #     self.assertEqual(divide(6,-3), -2)
+    #     self.assertEqual(divide(-6,-3), 2)
         
 if __name__ == '__main__':
     unittest.main()
+    
+# print(calc.add(10, 5))  # should be 15
+# print(calc.subtract(10, 5))  # should be 5
+# print(calc.multiply(10, 5))  # should be 50
+# print(calc.divide(10, 5))  # should be 2
