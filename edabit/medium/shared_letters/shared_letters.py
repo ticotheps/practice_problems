ac738b155e80b254be7a1a47d9e1cec37d3f811b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-22 13:54:25 -0400

    feat(U.P.E.R.): complete the UNDERSTAND phase

---
+++ edabit/medium/shared_letters/shared_letters.py
@@ -0,0 +1,36 @@
+"""
+LETTERS SHARED BETWEEN TWO WORDS
+
+Create a function that returns the number of characters shared between two
+words.
+
+Examples:
+    - get_shared_letters('apple', 'meaty') -> 2
+    - get_shared_letters('fan', 'forsook') -> 1
+    - get_shared_letters('spout', 'shout') -> 4
+"""
+
+"""
+PHASE I - UNDERSTAND THE PROBLEM
+
+Objective:
+    - Create an algorithm that takes in two inputs, 'str_1' and 'str_2', and
+    returns a single output, 'num_of_shared_letters'.
+    
+Expected Inputs & Outputs:
+    - Inputs:
+        - Expected Number: 2
+        - Expected Data Types: string, string
+        - Expected Var Names: 'str_1', 'str_2'
+
+    - Outputs:
+        - Expected Number: 1
+        - Expected Data Types: integer
+        - Expected Var Names: 'num_of_shared_letters'
+        
+Constraints:
+    - Can either of the inputs be empty strings?
+        - No.
+    - Can the strings include non-alphanumeric characters?
+        - Yes.
+"""
