3bc1eec705f2501ba5e2b136ff5a0c527673f211
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-23 10:18:01 -0400

    feat(U.P.E.R.): Write out PHASE I

--- edabit/medium/occurrences_in_a_list/occurrences_in_a_list.py
+++ edabit/medium/occurrences_in_a_list/occurrences_in_a_list.py
@@ -16,3 +16,26 @@ Notes:
     - Lists are zero-indexed.
     - Values in the list will be value-types (don't need to worry about nested lists).
 """
+"""
+PHASE I - UNDERSTANDING THE PROBLEM
+    - Objective:
+        - Write an algorithm that will take in two inputs, (1) a list and (2) a
+        string or integer that may or may not occurr in the given list, and then
+        returns a single list of the indices in which the second input occurs.
+        
+    - Expected Inputs:
+        - Number: 2
+        - Data Types: list, string/integer
+        - Var Names: 'input_list', 'input_element'
+        
+    - Expected Outputs:
+        - Number: 1
+        - Data Types: list
+        - Var Name: 'indices_list'
+
+PHASE II - DEVISING A PLAN
+
+PHASE III - EXECUTING THE PLAN
+
+PHASE IV - REFLECTING ON/REFACTORING THE PLAN
+"""
