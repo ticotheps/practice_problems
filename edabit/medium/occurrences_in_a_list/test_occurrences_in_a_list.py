088d29cb9d5cfa69f10583297cd062ae46178f29
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-23 10:06:54 -0400

    test: add test cases for 'occurrences_in_a_list' problem

---
+++ edabit/medium/occurrences_in_a_list/test_occurrences_in_a_list.py
@@ -0,0 +1,13 @@
+import unittest
+from occurrences_in_a_list import get_indices
+
+class Test(unittest.TestCase):
+    def test_get_occurrences(self):
+        self.assertEqual(get_indices(['a', 'a', 'b', 'a', 'b', 'a'], 'a'), [0, 1, 3, 5])
+        self.assertEqual(get_indices([1, 5, 5, 2, 7], 7), [4])
+        self.assertEqual(get_indices([1, 5, 5, 2, 7], 5), [1, 2])
+        self.assertEqual(get_indices([1, 5, 5, 2, 7], 8) ➞ [])
+        
+if __name__ == '__main__':
+    unittest.main()
+        
