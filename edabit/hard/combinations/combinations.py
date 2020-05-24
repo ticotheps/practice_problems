2356e689e65ac131cd8e79d3b1e87a1c9e629172
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-24 12:30:33 -0400

    feat(edabit): add prompt for 'combinations' problem

---
+++ edabit/hard/combinations/combinations.py
@@ -0,0 +1,7 @@
+"""
+COMBINATIONS
+
+Create a function that variable number of groups of items and returns the number
+of ways that the items can be arranged, with one item from each group. Order
+does not matter.
+"""
