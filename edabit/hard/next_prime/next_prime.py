4a963ce05e75214ca15f4ead17a5fffe71243add
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-25 10:38:30 -0400

    feat(U.P.E.R.): complete phase I of U.P.E.R.

---
+++ edabit/hard/next_prime/next_prime.py
@@ -0,0 +1,47 @@
+"""
+NEXT PRIME
+
+Given an integer, create a function that returns the next prime number. If the
+number is prime, return the number itself.
+
+Examples:
+    - next_prime(12) -> 13
+    - next_prime(24) -> 29
+    - next_prime(11) -> 11
+"""
+
+"""
+PHASE I: UNDERSTANDING THE PROBLEM
+
+- Objective:
+    - Write an algorithm that takes in a single input (an integer) and returns
+      the next prime number (an integer).
+
+- Expected Input:
+    - Number of: 1
+    - Data Type: integer
+    - Var Name: 'input_num'
+    
+- Expected Output:
+    - Number of: 1
+    - Data Type: integer
+    - Var Name: 'next_prime_num'
+    
+- Constraints:
+    - Can the 'input_num' be negative?
+        - No.
+    - Can the 'input_num' be a floating point number?
+        - No.
+    - Can the 'input_num' be 0?
+        - No.
+    - Can the 'input_num' be 1?
+        - No.
+        - The lowest possible input is 2.
+
+PHASE II: DEVISING A PLAN
+    - Brute Force Solution
+        - 
+PHASE III: EXECUTING THE PLAN
+PHASE IV: REFLECTING ON/REFACTORING THE PLAN
+"""
+
