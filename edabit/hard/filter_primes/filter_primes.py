1c1e969bbcde174ca76c3cd64a5e5cbca4b24d5e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-28 09:22:43 -0400

    feat(edabit): add prompt for code challenge

---
+++ edabit/hard/filter_primes/filter_primes.py
@@ -0,0 +1,20 @@
+"""
+FILTER PRIMES FROM A LIST
+
+Create a function that takes in a list and returns a new list containing only
+prime numbers.
+
+Examples:
+    - filter_primes([7, 9, 3, 9, 10, 11, 27]) -> [7, 3, 11]
+    - filter_primes([10007, 1009, 1007, 27, 147, 77, 1001, 70]) -> [10007, 1009]
+    - filter_primes([1009, 10, 10, 10, 3, 33, 9, 4, 1, 61, 63, 69, 1087, 1091,
+    1093, 1097]) -> [1009, 3, 61, 1087, 1091, 1093, 1097]
+"""
+
+"""
+PHASE I: UNDERSTANDING THE PROBLEM
+PHASE II: DEVISING A PLAN
+PHASE III: EXECUTING THE PLAN
+PHASE IV: REFLECTING ON/REFACTORING THE SOLUTION
+"""
+
