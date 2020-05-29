ed17bd7e2475613d7041e8805cefffa81339fd07
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-29 10:41:49 -0400

    feat(U.P.E.R.): complete phase I of U.P.E.R.

--- edabit/hard/find_the_primorial/find_the_primorial.py
+++ edabit/hard/find_the_primorial/find_the_primorial.py
@@ -12,3 +12,41 @@ Examples:
     - get_primorial(2) -> 6
     - get_primorial(8) -> 9699690
 """
+
+"""
+U.P.E.R. Problem-Solving Framework
+
+PHASE I - UNDERSTAND
+
+Defining Terms:
+    - 'Primorial': the product of 'n' number of consecutive prime numbers,
+    beginning with 2 as the first prime number.
+
+Objective:
+    - Write an algorithm that takes in a single input, 'num', and returns a
+    single output, 'primorial', for that number.
+    
+Expected Input(s):
+    - Number Of: 1
+    - Data Type(s): integer
+    - Var Name(s): 'num'
+    
+Expected Output(s):
+    - Number Of: 1
+    - Data Type(s): integer
+    - Var Name(s): 'primorial'
+    
+Constraints:
+    - Can the input 'num' be negative?
+        - No, because it refers to a specific number of prime numbers in a sequence
+        and prime numbers must be positive.
+    - Can the input 'num' be a floating point number?
+        - No, because it refers to a specific number of prime numbers in a sequence
+        and prime numbers must be whole.
+    - Can the input 'num' be null?
+        - No.
+    
+PHASE II - PLAN
+PHASE III - EXECUTE
+PHASE IV - REFLECT/REFACTOR
+"""
