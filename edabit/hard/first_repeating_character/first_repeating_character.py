be96ff556cd579407c57ab0fc820787c5e4b885b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-30 11:44:19 -0400

    feat(edabit): add prompt for coding challenge

---
+++ edabit/hard/first_repeating_character/first_repeating_character.py
@@ -0,0 +1,15 @@
+"""
+FIND FIRST CHARACTER THAT REPEATS
+
+Create a function that takes a string and returns the first character that
+repeats. If there is no repeat of a character, then return "-1".
+
+Examples:
+    - first_repeat('legolas') -> 'l'
+    - first_repeat('Gandalf') -> 'a'
+    - first_repeat('Balrog') -> '-1'
+    - first_repeat('Isildur') -> '-1'  // Case-sensitive 'I' != 'i'
+    
+Notes:
+    - Tests are case-sensitive.
+"""
