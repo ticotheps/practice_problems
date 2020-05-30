e9dc2488ffb984fa45c05177f5a07c162f3a3802
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-30 11:49:34 -0400

    feat(U.P.E.R.): complete PHASE I of U.P.E.R.

--- edabit/hard/first_repeating_character/first_repeating_character.py
+++ edabit/hard/first_repeating_character/first_repeating_character.py
@@ -13,3 +13,32 @@ Examples:
 Notes:
     - Tests are case-sensitive.
 """
+"""
+U.P.E.R. Problem-Solving Framework
+
+PHASE I: UNDERSTAND
+
+Objective:
+    - Write an algorithm that takes in a single input (of string data type) and
+    returns a single output (of string or integer data type).
+    
+Expected Input(s):
+    - Number Of: 1
+    - Data Type: string
+    - Var Name: 'input_str'
+    
+Expected Output(s):
+    - Number Of: 1
+    - Data Type: string or integer
+    - Var Name: 'repeating_char'
+    
+Constraints:
+    - Tests are case-sensitive.
+        - i.e. - "I" != "i"
+
+PHASE II: PLAN
+
+PHASE III: EXECUTE
+
+PHASE IV: REFLECT/REFACTOR
+"""
