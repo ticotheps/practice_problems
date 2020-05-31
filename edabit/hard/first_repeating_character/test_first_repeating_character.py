dccd10dde02299839a872ee2c69ff07b3c34b63f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-31 12:29:03 -0400

    feat(tests): add test cases for optimized solution

--- edabit/hard/first_repeating_character/test_first_repeating_character.py
+++ edabit/hard/first_repeating_character/test_first_repeating_character.py
@@ -1,5 +1,5 @@
 import unittest
-from first_repeating_character import first_repeat
+from first_repeating_character import first_repeat, first_repeat_optimized
 
 class Test(unittest.TestCase):
     def test_first_repeat(self):
@@ -7,6 +7,12 @@ class Test(unittest.TestCase):
         self.assertEqual(first_repeat('Gandalf'), 'a')
         self.assertEqual(first_repeat('Balrog'), -1)
         self.assertEqual(first_repeat('Isildur'), -1)
+        
+    def test_first_repeat_optimized(self):
+        self.assertEqual(first_repeat_optimized('legolas'), 'l')
+        self.assertEqual(first_repeat_optimized('Gandalf'), 'a')
+        self.assertEqual(first_repeat_optimized('Balrog'), -1)
+        self.assertEqual(first_repeat_optimized('Isildur'), -1)
         
 if __name__ == '__main__':
     unittest.main()
