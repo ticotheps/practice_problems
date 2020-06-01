95d1f090bee0b799f0426c73f8b435d9537ea85c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-01 10:38:53 -0400

    feat(edabit): add prompt for coding challenge

---
+++ edabit/hard/purge_and_organize/purge_and_organize.py
@@ -0,0 +1,13 @@
+"""
+PURGE AND ORGANIZE
+
+Given a list of numbers, write a function that returns a list that...
+
+1. has all duplicate elements removed.
+2. is sorted from least to greatest value.
+
+Examples:
+    - unique_sort([1, 2, 4, 3]) -> [1, 2, 3, 4]
+    - unique_sort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) -> [1, 2, 3, 4]
+    - unique_sort([6, 7, 3, 2, 1]) -> [1, 2, 3, 6, 7]
+"""
