06e7132d0bfbb2d53c37b9ee93fa084e2e97d3de
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-01 10:46:00 -0400

    feat(tests): write test cases for code challenge

---
+++ edabit/hard/purge_and_organize/test_purge_and_organize.py
@@ -0,0 +1,11 @@
+import unittest
+from purge_and_organize import unique_sort
+
+class Test(unittest.TestCase):
+    def test_unique_sort(self):
+        self.assertEqual(unique_sort([1, 2, 4, 3]), [1, 2, 3, 4])
+        self.assertEqual(unique_sort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]), [1, 2, 3, 4])
+        self.assertEqual(unique_sort([6, 7, 3, 2, 1]), [1, 2, 3, 6, 7])
+        
+if __name__ == '__main__':
+    unittest.main()
