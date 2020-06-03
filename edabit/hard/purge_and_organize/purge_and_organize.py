aee56e3b96e232bf56b21aaa0ebecf568fb9a60c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-03 11:33:22 -0400

    feat(U.P.E.R.): complete phase I of U.P.E.R.

--- edabit/hard/purge_and_organize/purge_and_organize.py
+++ edabit/hard/purge_and_organize/purge_and_organize.py
@@ -11,3 +11,35 @@ Examples:
     - unique_sort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) -> [1, 2, 3, 4]
     - unique_sort([6, 7, 3, 2, 1]) -> [1, 2, 3, 6, 7]
 """
+"""
+U.P.E.R. PROBLEM-SOLVING FRAMEWORK
+
+PHASE I: UNDERSTAND
+
+- Objective:
+    - Write an algorithm that takes in a single input (a list) and returns an
+      output (also a list) that removes any duplicate elements from the list,
+      while also sorting its values from least to greatest.
+      
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): 'lst'
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): 'lst'
+    
+- Constraints:
+    - Can the elements of the input 'lst' be negative?
+        - Yes.
+    - Can the elements of the input 'lst' be floating point numbers?
+        - Yes.
+    - Can the input 'lst' be empty?
+        - Yes. We will return an empty list as the output if this is the case.
+    
+PHASE II: PLAN
+PHASE III: EXECUTE
+PHASE IV: REFLECT/REFACTOR
+"""
