9459937dc9bff84a3caf1980edb5bcb4f08eb5f3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-07 00:01:40 -0400

    feat(tests): add more test cases to test file

--- edabit/hard/index_of_caps/test_index_of_caps.py
+++ edabit/hard/index_of_caps/test_index_of_caps.py
@@ -2,4 +2,12 @@ import unittest
 from index_of_caps import index_of_caps
 
 class Test(unittest.TestCase):
-    pass
+    def test_index_of_caps(self):
+        self.assertEqual(index_of_caps("eDaBiT"), [1, 3, 5])
+        self.assertEqual(index_of_caps("eQuINoX"), [1, 3, 4, 6])
+        self.assertEqual(index_of_caps("determine"), [])
+        self.assertEqual(index_of_caps("STRIKE"), [0, 1, 2, 3, 4, 5])
+        self.assertEqual(index_of_caps("sUn"), [1])
+        
+if __name__ == '__main__':
+    unittest.main()
