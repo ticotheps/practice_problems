0fc823f85716b595594a15623952cdcc47e9a98f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-08 11:20:01 -0400

    feat(test): write test cases for code challenge

---
+++ edabit/hard/correct_inequality_signs/test_correct_inequality_signs.py
@@ -0,0 +1,11 @@
+import unittest
+from correct_inequality_signs import correct_signs
+
+class Test(unittest.TestCase):
+    def test_correct_signs(self):
+        self.assertEqual(correct_signs("3 < 7 < 11"), True)
+        self.assertEqual(correct_signs("13 > 44 > 33 > 1"), False)
+        self.assertEqual(correct_signs("1 < 2 < 6 < 9 > 3"), True)
+    
+if __name__ == "__main__":
+    unittest.main()
