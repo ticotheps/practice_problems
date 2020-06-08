4d03f81280d64728299c9fd46e57db033713d71c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-08 11:27:00 -0400

    chore:

---
+++ edabit/hard/correct_inequality_signs/correct_inequality_signs.py
@@ -0,0 +1,22 @@
+"""
+CORRECT INEQUALITY SIGNS
+
+Create a function that returns True if a given inequality expression is correct
+and False otherwise.
+"""
+
+"""
+U.P.E.R. Problem-Solving Framework
+
+PHASE I: UNDERSTAND the problem
+PHASE II: device a PLAN
+PHASE III: EXECUTE the plan
+PHASE IV: REFLECT on/REFACTOR the plan
+"""
+
+def correct_signs(txt):
+    return txt
+
+print(correct_signs("3 < 7 < 11"))
+print(correct_signs("13 > 44 > 33 > 1"))
+print(correct_signs("1 < 2 < 6 < 9 > 3"))
