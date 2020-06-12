de8cd9f72ca5f955d1aa1eda33985655f0aca7c4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-12 14:43:06 -0400

    feat(edabit): add prompt for code challenge + rename directory/files

---
+++ edabit/hard/factorial/factorial.py
@@ -0,0 +1,15 @@
+"""
+FACTORIAL OF A POSITIVE INTEGER
+
+Write a function that takes a positive integer and return its factorial.
+
+Examples:
+    - factorial(4) -> 24
+    - factorial(0) -> 1
+    - factorial(9) -> 362880
+    
+Notes:
+    - The factorial of 0 is 1.
+    - The factorial of any positive integer, Z, is Z * (Z-1) * (Z-2) * ... * 1.
+        - i.e. - factorial of 3 is (3 * 2 * 1 = 6)
+"""
