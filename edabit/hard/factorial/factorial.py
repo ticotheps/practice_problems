ba73cf7c755418d950be114a8bfb5aacd73e4204
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-12 15:03:24 -0400

    feat(U.P.E.R Phase II): write down all the steps for the PLAN

--- edabit/hard/factorial/factorial.py
+++ edabit/hard/factorial/factorial.py
@@ -21,19 +21,24 @@ PHASE I: UNDERSTAND [the problem]
 
 - Objective:
     - Write an algorithm that takes in a single input (integer data type) and
-      returns a single output (integer data type), which is the factorial of the
-      given input.
-      
+      returns a single output (integer data type), which is the "factorial" of
+      the given input.
+
+- Key Terms:
+    - "factorial":
+        - the product of a positive integer and all the positive integers below
+          it.
+
 - Expected Input(s):
     - Number Of: 1
     - Data Type(s): integer
     - Var Name(s): 'Z'
-    
+
 - Expected Input(s):
     - Number Of: 1
     - Data Type(s): integer
     - Var Name(s): 'factorial'
-    
+
 - Constraints:
     - Can the input be a negative integer?
         - No.
@@ -43,8 +48,30 @@ PHASE I: UNDERSTAND [the problem]
         - No. The input MUST be a positive integer.
 
 PHASE II: [Devise a] PLAN
-PHASE III: EXECUTE [the plan]
-PHASE IV: REFLECT ON/REFACTOR [the plan/solution]
+
+- First-Pass Solution: (1) Define a function, 'factorial()', that takes in a
+    single input (integer data type), 'Z', and returns a single output (integer
+    data type), 'factorial', which will be the factorial of the given input.
+
+    (2) Declare a var, 'factorial', that will be returned as the output.
+    Initialize it with a value of 1
+
+    (3) If 'Z' is LESS THAN 0, return a string that says "Invalid input. Please
+    try a positive integer instead."
+
+    (4) If 'Z' is equal to 0, return the value of 'factorial'.
+
+    (5) Use a 'for' loop to iterate through a range of numbers (1 -> Z)...
+
+        (a) Declare a var, 'int_below_z', and set it equal to the iterator used
+        in the 'for' loop.
+        
+        (b) Multiply the value of 'factorial' by the value of 'int_below_z' and
+        set that value equal to 'factorial'.
+        
+    (6) Return the value of 'factorial'.
+
+PHASE III: EXECUTE [the plan] PHASE IV: REFLECT ON/REFACTOR [the plan/solution]
 """
 
 
