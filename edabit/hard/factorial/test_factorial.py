9bf46d6411bf3475d0034ce9c287a241285dc899
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-13 23:58:01 -0400

    refactor(test): refactor test case to contain negative floating point number

--- edabit/hard/factorial/test_factorial.py
+++ edabit/hard/factorial/test_factorial.py
@@ -3,7 +3,7 @@ from factorial import factorial
 
 class Test(unittest.TestCase):
     def test_factorial(self):
-        self.assertEqual(factorial(-2), "Invalid input. Please try a positive integer instead.")
+        self.assertEqual(factorial(-2.133), "Invalid input. Please try a positive integer instead.")
         self.assertEqual(factorial(2.133), "Invalid input. Please try a positive integer instead.")
         self.assertEqual(factorial(4), 24)
         self.assertEqual(factorial(0), 1)
