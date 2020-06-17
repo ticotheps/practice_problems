a78fd472b8f6c903f891f96c47fa3304c846a8ad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-17 09:51:45 -0400

    feat(edabit): create a new directory and nested files for code challenge

---
+++ edabit/hard/recursive_length_of_string/recursive_length_of_string.py
@@ -0,0 +1,13 @@
+"""
+RECURSION: LENGTH OF A STRING
+
+Instructions:
+    - Write a function that returns the length of a string. Make your function
+recursive.
+
+Examples:
+    - length('apple') -> 5
+    - length('make') -> 4
+    - length('a') -> 1
+    - length('') -> 0
+"""
