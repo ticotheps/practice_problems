4bb5593925510ddb7c46725e821999e5dcaf72d1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-19 03:31:32 -0400

    feat(edabit): understand the given code challenge prompt

---
+++ edabit/hard/triangular_number_sequence/triangular_number_sequence.py
@@ -0,0 +1,19 @@
+"""
+TRIANGULAR NUMBER SEQUENCE
+
+This triangular number sequence is generated from a pattern of dots that form a
+triangle. The first 5 numbers of the sequence, or dots, are:
+
+1, 3, 6, 10, 15
+
+This means that the first triangle has just one dot, the second one has three
+dots, the third one has six dots, and so on.
+
+Write a function that gives the number of dots, 'n', with its corresponding
+triangle number of the sequence.
+
+Examples:
+    - triangle(1) -> 1
+    - triangle(6) -> 21
+    - triangle(215) -> 23220
+"""
