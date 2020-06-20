7640617719612238d6651172049f21970a706a46
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-20 11:18:59 -0400

    feat(U.P.E.R Phase I): complete UNDERSTAND phase of U.P.E.R.

--- edabit/hard/triangular_number_sequence/triangular_number_sequence.py
+++ edabit/hard/triangular_number_sequence/triangular_number_sequence.py
@@ -22,6 +22,25 @@ The U.P.E.R. Problem-Solving Framework
 
 PHASE I: UNDERSTAND
 
+- Objective: 
+    - Write an algorithm that takes in a single input triangle number, 'n', and
+    returns a single output, 'num_of_dots' (an integer).
+      
+- Expected Input:
+    - Data Type: integer
+    - Var Name: 'n'
+    
+- Expected Output:
+    - Data Type: integer
+    - Var Name: 'num_of_dots'
+    
+- Edge Cases & Constraints:
+    - Can the input integer be negative?
+        - No.
+    - Can the input be a floating point number?
+        - No.
+    - How would you handle an input of "None"?
+        - Return a string that says, "Invalid input. Please enter a positive integer."
 
 PHASE II: PLAN
 PHASE III: EXECUTE
