fc5a6c45e431cc44dffa8f599265b075bba79252
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-25 14:56:49 -0400

    feat(edabit): add prompt for code challenge

---
+++ edabit/hard/alphabet_soup/alphabet_soup.py
@@ -0,0 +1,17 @@
+"""
+ALPHABET SOUP
+
+Create a function that takes in a string and returns a string with its letters
+in alphabetical order.
+
+Examples:
+    - alphabet_soup("hello") ➞ "ehllo"
+    - alphabet_soup("edabit") ➞ "abdeit"
+    - alphabet_soup("hacker") ➞ "acehkr"
+    - alphabet_soup("geek") ➞ "eegk"
+    - alphabet_soup("javascript") ➞ "aacijprstv"
+    
+Notes:
+    - You can assume numbers and punctuation symbols won't be included in test
+    cases. You'll only have to deal with single word, alphabetic characters.
+"""
