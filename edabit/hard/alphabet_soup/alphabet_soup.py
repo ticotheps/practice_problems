0c05e7b259e684a5466335cd1e106f3098f2123b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-27 13:11:54 -0400

    feat(U.P.E.R Phase I): complete Phase I of U.P.E.R.

--- edabit/hard/alphabet_soup/alphabet_soup.py
+++ edabit/hard/alphabet_soup/alphabet_soup.py
@@ -15,3 +15,36 @@ Notes:
     - You can assume numbers and punctuation symbols won't be included in test
     cases. You'll only have to deal with single word, alphabetic characters.
 """
+
+"""
+The U.P.E.R. Problem-Solving Framework
+
+PHASE I: UNDERSTAND
+
+- Objective(s): 
+    - Write an algorithm that takes in a string (of alphabetic characters only)
+      and returns a new string with all of the letters from the given input in 
+      alphabetized order.
+      
+- Expected Input:
+    - Number Of: 1
+    - Data Type: string
+    - Var Name: 'str'
+    
+- Expected Output:
+    - Number Of: 1
+    - Data Type: string
+    - Var Name: 'new_str'
+    
+- Edge Cases & Constraints:
+    - Can the given input be an empty string?
+        - No.
+    - Can the given input string include non-alphabetic characters?
+        - No. Assume that the characters in the given input will only be
+          alphabetic characters.
+
+PHASE II: PLAN
+PHASE III: EXECUTE
+PHASE IV: REFLECT/REFACTOR
+
+"""
