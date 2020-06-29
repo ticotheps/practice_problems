c96a15e1f4a9573f315d2678a30a16ff452590f4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-29 10:53:20 -0400

    feat(edabit): add prompt for code challenge

---
+++ edabit/hard/add_two_string_numbers/add_two_string_numbers.py
@@ -0,0 +1,34 @@
+"""
+ADD TWO STRING NUMBERS
+
+Write a function that adds two numbers. The catch, however, is that the numbers
+will be strings.
+
+Examples:
+    - add_str_nums("4", "5") -> "9"
+    - add_str_nums("abcdefg", "3") -> "-1"
+    - add_str_nums("1", "") -> "1"
+    - add_str_nums("1874682736267235927359283579235789257", "32652983572985729")
+   -> 1874682736267235927391936562808774986
+   
+Notes:
+    - If there are any non-numerical characters, return "-1".
+    - If one option is blank, the code should still work.
+    - Python supports the addiion of integers without limit, try this challenge
+    without using this functionality.
+    - Your function doesn't have to add negative numbers.
+"""
+
+"""
+The U.P.E.R. Problem-Solving Framework
+
+PHASE I: UNDERSTAND
+
+- Objective: 
+    - 
+
+
+PHASE II: PLAN
+PHASE III: EXECUTE
+PHASE IV: REFLECT/REFACTOR
+"""
