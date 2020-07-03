c461567eff4bcafa13610ff5724f50e9057ae160
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-03 08:52:07 -0400

    refactor(first pass solution): remove commented code and print() statements

--- edabit/hard/add_two_string_numbers/add_two_string_numbers.py
+++ edabit/hard/add_two_string_numbers/add_two_string_numbers.py
@@ -77,55 +77,24 @@ PHASE III: EXECUTE (Please See Below)
 """
 
 def add_str_nums(num1, num2):
-    # create a var, 'sum', to keep track of a running total sum
     sum = 0
-    
-    # evaluate both input strings to make sure that they are both positive
-    # integers and are also not empty strings
-    if type(num1) == str and type(num2) == str:
-        # print(f"\nnum1 = {num1}")
-        # print(f"num2 = {num2}")
-        
-        if num1 == "":
-            print("num1 is an empty string")
-        
-        else:
+    if type(num1) == str and type(num2) == str:        
+        if num1 != "":
             for i in num1:          
-                if i.isnumeric() == True:
-                    print(f"i = {i}, which is numeric")
-                    
-                else:
-                    print(f"i = {i}, which is NOT numeric")
-                    return "-1"
+                if i.isnumeric() == False:
+                    return "-1"              
             sum += int(num1)
-            # print(f"***UPDATED sum = {sum}")
-                
-        if num2 == "":
-            print("num2 is an empty string")
         
-        else:  
+        if num2 != "":  
             for j in num2:
-                if j.isnumeric() == True:
-                    print(f"i = {j}, which is numeric")
-                else:
-                    print(f"i = {j}, which is NOT numeric")
+                if j.isnumeric() == False:
                     return "-1"
             sum += int(num2)
-            print(f"***UPDATED sum = {sum}")
 
         return str(sum)
     
     else:
         return "The provided inputs are both not of string data type. Please enter a valid input."
-        
-
-print(add_str_nums("", ""))  # "0"
-print(add_str_nums("1", "01"))  # "2"
-print(add_str_nums("1", ""))  # "1"
-print(add_str_nums("198547982570192857109283570192837509218375091283750192835710298357019237509125710925710923759012375901275901285701925712035712983571092562945875310962518235712385971230956127856123571209358712905610923587102395716258125612095710298","510298570192857910827519027510982561875691857120958371029586187585198273501982573091857091875901875809175091659812750918275091875091857918265901265918659816591750981750981759817598175089175891720570129571098758901750917501975"))  #   "198548492868763049967194397711865020200936966975607313794081327943206822707399212908284015616104251803151710460793585524786631258662967654803793576863784154372202562981937878837883388807533802081502644157231966815017027363013212273"
-print(add_str_nums("0000001", "020006"))  # "20007"
-print(add_str_nums("1325123515s238579875987", "38698592523525325"))  # "-1"
-
 
 """
 PHASE IV: REFLECT/REFACTOR
