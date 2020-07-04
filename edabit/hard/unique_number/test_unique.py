3aa3821ce828788c601289f48f41e21e4e295347
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-04 13:50:35 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase of U.P.E.R.

--- edabit/hard/unique_number/test_unique.py
+++ edabit/hard/unique_number/test_unique.py
@@ -1,11 +1,11 @@
 import unittest
-from unique import unique
+from unique import find_unique_num
 
 class Test(unittest.TestCase):
-    def test_unique(self):
-        self.assertEqual(unique([3, 3, 3, 7, 3, 3]), 7)
-        self.assertEqual(unique([0, 0, 0.77, 0, 0]), 0.77)
-        self.assertEqual(unique([0, 1, 1, 1, 1, 1, 1, 1]), 0)
+    def test_find_unique_num(self):
+        self.assertEqual(find_unique_num([3, 3, 3, 7, 3, 3]), 7)
+        self.assertEqual(find_unique_num([0, 0, 0.77, 0, 0]), 0.77)
+        self.assertEqual(find_unique_num([0, 1, 1, 1, 1, 1, 1, 1]), 0)
         
 if __name__ == '__main__':
     unittest.main()
--- edabit/hard/unique_number/unique.py
+++ edabit/hard/unique_number/unique.py
@@ -8,3 +8,48 @@ NOTES:
     - Test cases will always have exactly one unique number while all others are
     the same.
 """
+
+"""
+The U.P.E.R. Problem-Solving Framework
+
+U = UNDERSTAND the problem
+
+- Objective:
+    - Write an algorithm, 'find_unique_num()', that takes in a single input 
+    list, 'lst', and returns the only unique integer in that list as a single 
+    output, 'unique_num'.
+      
+- Expected Input:
+    - Number Of: 1
+    - Data Type: list
+    - Var Name: 'lst'
+    
+- Expected Output:
+    - Number Of: 1
+    - Data Type: integer
+    - Var Name: 'unique_num'
+    
+- Edge Cases & Constraints:
+    - Can the elements in the given input list be negative numbers?
+        - No. The elements must be positive integers.
+    - Can the elements in the given input list be floating point numbers?
+        - No. The elements must be positive, whole integers.
+    - Can the elements in the given input list be empty strings?
+        - No. The elements must be positive, whole integers.
+
+P = devise a PLAN
+E = EXECUTE the plan
+R = REFLECT/REFACTOR the plan
+
+
+"""
+
+def find_unique_num(lst):
+    unique_num = None
+    
+    return unique_num
+
+print(find_unique_num([3, 3, 3, 7, 3, 3]))  # 7
+print(find_unique_num([0, 0, 0.77, 0, 0]))  # 0.77
+print(find_unique_num([0, 1, 1, 1, 1, 1, 1, 1]))  # 0
+
