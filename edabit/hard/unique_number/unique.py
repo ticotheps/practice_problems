3c2629b4ed68fd544a4d3e1616a985ed7de2a0b8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-04 13:37:42 -0400

    feat(edabit): add code challenge prompt

---
+++ edabit/hard/unique_number/unique.py
@@ -0,0 +1,10 @@
+""""
+WHICH NUMBER IS NOT LIKE THE OTHERS?
+
+Create a function that takes in a list of numbers and returns the unique number
+in that list.
+
+NOTES:
+    - Test cases will always have exactly one unique number while all others are
+    the same.
+"""
