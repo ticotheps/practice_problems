49fdf3fabf60a52b9c08db94f700790fdba8a4fa
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-06 10:11:02 -0400

    feat(edabit): add prompt for code challenge

--- edabit/hard/nth_smallest_element/nth_smallest_element.py
+++ edabit/hard/nth_smallest_element/nth_smallest_element.py
@@ -1,3 +1,20 @@
 """
+NTH SMALLEST ELEMENT
 
+Given an unsorted list, create a function that returns the nth smallest element
+(the smallest element is the FIRST smallest, the second smallest element is the
+second smallest, etc.).
+
+Examples:
+    - nth_smallest([1, 3, 5, 7], 1) -> 1
+    - nth_smallest([1, 3, 5, 7], 3) -> 5
+    - nth_smallest([1, 3, 5, 7], 5) -> None
+    - nth_smallest([7, 3, 5, 1], 2) -> 3
+
+Notes:
+    - 'n' will always be >= 1.
+    - Each number in the array will be distinct (there will be a clear
+    ordering).
+    - Given an out of bounds parameter (e.g. a list is of size 'k'), and you are
+    asked to find the m < k smallest element, return None.
 """
