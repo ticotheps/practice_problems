84e0608615dbed36e50d9771d383c23cd40f9bbd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-08 14:19:51 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase of U.P.E.R.

--- edabit/hard/recursion_factorials/recursion_factorials.py
+++ edabit/hard/recursion_factorials/recursion_factorials.py
@@ -9,6 +9,37 @@ Examples:
     - factorial(1) -> 1
     - factorial(0) -> 0
 """
+"""
+The U.P.E.R. Problem-Solving Framework
 
+***** U = UNDERSTAND *****
+- Objective: 
+    - Write a recursive algorithm that can calculate the factorial of a given
+    number.
+    
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type: integer
+    - Var Name: "num"
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type: integer
+    - Var Name: "factorial"
+    
+- Edge Cases & Constraints:
+    - Can the given input number be a negative number?
+        - No.
+    - Can the given input number be a floating point number?
+        - No.
+    - Can the given input value be 0?
+        - Yes.
+        
+***** P = PLAN *****
+***** E = EXECUTE *****
+***** R = REFLECT/REFACTOR *****
+
+
+"""
 def factorial(n):
     pass
