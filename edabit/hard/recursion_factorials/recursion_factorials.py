824bc47608134634c0c2d8c28fc9dd1c5c038af0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-08 14:14:43 -0400

    feat(edabit): add prompt for code challenge

--- edabit/hard/recursion_factorials/recursion_factorials.py
+++ edabit/hard/recursion_factorials/recursion_factorials.py
@@ -1,2 +1,14 @@
+"""
+RECURSION: FACTORIAL
+
+Write a function that calculates the factorial of a number recursively.
+
+Examples:
+    - factorial(5) -> 120
+    - factorial(3) -> 6
+    - factorial(1) -> 1
+    - factorial(0) -> 0
+"""
+
 def factorial(n):
     pass
