c7976f407cdb94d41cf9075daf6c946356ce9537
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-09 09:19:25 -0400

    feat(brute force solution): write a brute force solution that passes all test cases

--- edabit/hard/recursion_factorials/recursion_factorials.py
+++ edabit/hard/recursion_factorials/recursion_factorials.py
@@ -17,10 +17,18 @@ The U.P.E.R. Problem-Solving Framework
     - Write a recursive algorithm that can calculate the factorial of a given
     number.
     
+- Defining Terms:
+    - "Recursion": 
+        - defining something with reference to itself.
+            - i.e. - a function calling itself from within its own function body.
+        - requirements of a recursive function:
+            (a) base case
+            (b) recurrence relation
+
 - Expected Input(s):
     - Number Of: 1
     - Data Type: integer
-    - Var Name: "num"
+    - Var Name: "n"
     
 - Expected Output(s):
     - Number Of: 1
@@ -28,18 +36,46 @@ The U.P.E.R. Problem-Solving Framework
     - Var Name: "factorial"
     
 - Edge Cases & Constraints:
-    - Can the given input number be a negative number?
+    - Can the given input "n" be a negative number?
         - No.
-    - Can the given input number be a floating point number?
+    - Can the given input "n" be a floating point number?
         - No.
     - Can the given input value be 0?
         - Yes.
         
 ***** P = PLAN *****
+- Brute Force Solution
+    (1) Define a function that takes in a single input, "n", and returns a
+    single output, "factorial".
+    
+    (2) Declare a var, "factorial", that will be returned as the output.
+    Initialize it with a value of 1.
+    
+    (3) Use an "if" statement to define a base case. 
+    
+        (a) If "n" equals 0, return "factorial", which should already equal 1.
+    
+        (b) Else, 
+    
 ***** E = EXECUTE *****
 ***** R = REFLECT/REFACTOR *****
 
 
 """
-def factorial(n):
-    pass
+def find_factorial(n):
+    print(f"n = {n}")
+    # the returned output
+    factorial = 1
+    print(f"factorial = {factorial}")
+    
+    # base case
+    if n == 0 or n == 1:
+        return 1
+    
+    return n * find_factorial(n-1)
+
+print(find_factorial(0))     # 1
+print(find_factorial(1))     # 1
+print(find_factorial(3))     # 6
+print(find_factorial(5))     # 120
+    
--- edabit/hard/recursion_factorials/test_recursion_factorials.py
+++ edabit/hard/recursion_factorials/test_recursion_factorials.py
@@ -1,12 +1,12 @@
 import unittest
-from recursion_factorials import factorial
+from recursion_factorials import find_factorial
 
 class Test(unittest.TestCase):
-    def test_factorial(self):
-        self.assertEqual(factorial(5), 120)
-        self.assertEqual(factorial(3), 6)
-        self.assertEqual(factorial(1), 1)
-        self.assertEqual(factorial(0), 1)
+    def test_find_factorial(self):
+        self.assertEqual(find_factorial(5), 120)
+        self.assertEqual(find_factorial(3), 6)
+        self.assertEqual(find_factorial(1), 1)
+        self.assertEqual(find_factorial(0), 1)
         
 if __name__ == "__main__":
     unittest.main()
