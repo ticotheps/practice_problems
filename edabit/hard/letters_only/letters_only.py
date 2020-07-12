f5407cae1c4dd83576f53c3cc8f7f3f482fe5d30
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-12 16:43:52 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

---
+++ edabit/hard/letters_only/letters_only.py
@@ -0,0 +1,51 @@
+"""
+LETTERS ONLY
+
+Write a function that removes any non-letters from a string, returning a
+well-known film title.
+
+Examples:
+    - letters_only("R!=:~0o0./c&}9k`60=y") ➞ "Rocky"
+    - letters_only("^,]%4B|@56a![0{2m>b1&4i4") ➞ "Bambi"
+    - letters_only("^U)6$22>8p).") ➞ "Up"
+
+"""
+"""
+The 4 Phases of the U.P.E.R. Problem-Solving Framework:
+
+U = UNDERSTAND Phase
+P = PLAN Phase
+E = EXECUTE Phase
+R = REFLECT/REFACTOR Phase
+
+***** The UNDERSTAND Phase *****
+- Objective:
+    - Write an algorithm that takes in a single input string, removes any
+      non-alphabetic characters, and returns a single output string (which also
+      happens to be a well-known film title).
+      
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type: string
+    - Var Name: "txt"
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type: string
+    - Var Name: "movie_title"
+    
+- Edge Cases & Constraints:
+    - Can the given input string be empty?
+        - Yes. If so, we will return a string that says, "Invalid input. Please
+          provide a non-empty input string."
+    - Can the input be any other data type besides a string?
+        - No. If so, we will return a string that says, "Invalid input. Please
+          provide an input of string data type.
+
+***** The UNDERSTAND Phase *****
+
+***** The EXECUTE Phase *****
+
+***** The REFLECT/REFACTOR Phase *****
+"""
+
