08d51ee9ea7605c930824785fec87de30869f248
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-12 17:03:44 -0400

    feat(U.P.E.R. Phase III): create a first-pass solution that passes all test cases

--- edabit/hard/letters_only/letters_only.py
+++ edabit/hard/letters_only/letters_only.py
@@ -40,12 +40,46 @@ R = REFLECT/REFACTOR Phase
           provide a non-empty input string."
     - Can the input be any other data type besides a string?
         - No. If so, we will return a string that says, "Invalid input. Please
-          provide an input of string data type.
+          provide an input of string data type."
 
-***** The UNDERSTAND Phase *****
+***** The PLAN & EXECUTE Phases (please see below) *****
+"""
 
-***** The EXECUTE Phase *****
+# (1) Define a function that takes in 1 input, 'txt', and returns 1 output, 
+# 'movie_title'.
+def letters_only(txt):
+    # (2) Declare a var, 'movie_title_list', and initialize it with a pair of 
+    # empty square brackets.
+    movie_title_list = []
+    print(f"**INITIAL** movie_title_list = {movie_title_list}")
+    
+    # (3) Evaluate the given input string, ensuring that it isn't empty.
+    if txt == "":
+        return "Invalid input. Please provide a non-empty input string."
+    
+    # (4) Evaluate the given input string, ensuring that it is of string data 
+    # type.
+    if type(txt) != str:
+        return "Invalid input. Please provide an input of string data type."
+    
+    # (5) Use a 'for' loop to iterate through each char of the given input
+    # string.
+    for char in txt:
+        # (5a) If the char is alphabetic, append it to the 'movie_title' list.
+        if char.isalpha() == True:
+            movie_title_list.append(char)
+            print(f"**UPDATED** movie_title_list = {movie_title_list}")
+        # (5b) Else, print a string saying that the char is not alphabetic.
+        else:
+            print(f"{char} is not an alphabetic character.")
+    # (6) Convert the 'movie_title_list' from a list to a string and return it.
+    return "".join(movie_title_list)
 
+print(letters_only(''))  # 'Invalid input. Please provide a non-empty input string.'
+print(letters_only(23))  # 'Invalid input. Please provide an input of string data type.'
+print(letters_only('R!=:~0o0./c&}9k`60=y'))  # 'Rocky'
+
+"""
 ***** The REFLECT/REFACTOR Phase *****
 """
 
