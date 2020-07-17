0bae95f9b1c64f306d09a502acba43541686cff4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-17 11:54:16 -0400

    feat(edabit): add prompt for code challenge

---
+++ edabit/hard/grade_percentage/grade_percentage.py
@@ -0,0 +1,26 @@
+"""
+YOU FAILEDPASSED THE EXAM
+
+Here is what you see after you take your exam:
+`Your Score: 85%
+
+You FAILEDPASSED the Exam.
+
+Required Score 85%`
+
+The challenge is to fix all of the bugs in this incredible messy code, which the
+code in the image might've actually looked like (probably not)! The code given
+will output the same middle two lines as in the image shown above.
+    - First parameter is the user's score.
+    - Second parameter is the required score.
+    
+Examples:
+    - grade_percentage("85%", "85%") ➞ "You PASSED the Exam"
+    - grade_percentage("99%", "85%") ➞ "You PASSED the Exam"
+    - grade_percentage("65%", "90%") ➞ "You FAILED the Exam"
+    
+Notes:
+    - Note that inputs will be given as a string percentage number.
+    - Maintain all capitalization.
+    - Feel free to declutter and refactor code if it helps!
+"""
