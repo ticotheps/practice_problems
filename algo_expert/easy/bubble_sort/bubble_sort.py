f8d7eb2cd838952df96688a6852e0080205b1b26
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-18 09:47:09 -0400

    chore(prompt): add code challenge prompt

--- algo_expert/easy/bubble_sort/bubble_sort.py
+++ algo_expert/easy/bubble_sort/bubble_sort.py
@@ -1,3 +1,16 @@
+"""
+BUBBLE SORT
+
+Write a function that takes in an array of integers and returns a sorted version
+of that array. Use the Bubble Sort algorithm to sort the array.
+
+Sample Input:
+    - array = [8, 5, 2, 9, 5, 6, 3]
+    
+Sample Output:
+    - [2, 3, 5, 5, 6, 8, 9]
+"""
+
 def bubbleSort(array):
     pass
 
