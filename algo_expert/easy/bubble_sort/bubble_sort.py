aa937fbe1353bbebebbdf84c2a17966442d2f846
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-18 10:04:45 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

--- algo_expert/easy/bubble_sort/bubble_sort.py
+++ algo_expert/easy/bubble_sort/bubble_sort.py
@@ -9,6 +9,40 @@ Sample Input:
     
 Sample Output:
     - [2, 3, 5, 5, 6, 8, 9]
+    
+Notes:
+    - "Bubble Sort" is different from other sorting algorithms because it sorts
+    "in place", meaning that it does not return a new array.
+    - It returns the original, but in a sorted order.
+"""
+"""
+****** UNDERSTAND Phase ******
+- Objective:
+    - Write an algorithm that uses "Bubble Sort" to sort the integers in a given
+    input list and returns the sorted version of that input list.
+    
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): "array"
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): "array"
+    
+- Hints:
+    - Traverse the input array, swapping any two numbers that are out of order.
+    - Keep track of any swaps that you make during an iteration.
+    - Once you arrive at the end of the array, check if you have made any swaps.
+    - If not, the array is sorted and you are done!
+    - Otherwise, repeat the steps laid out in this hint until the array is sorted.
+
+****** PLAN Phase ******
+
+
+****** EXECUTE Phase ******
+
 """
 
 def bubbleSort(array):
