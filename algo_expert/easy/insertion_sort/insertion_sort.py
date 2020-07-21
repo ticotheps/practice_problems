518eb7ec3bd9e179473aba275e0c45c12e70562f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-21 11:05:52 -0400

    chore(prompt): add prompt for insertion sort code challenge

---
+++ algo_expert/easy/insertion_sort/insertion_sort.py
@@ -0,0 +1,23 @@
+"""
+INSERTION SORT
+
+Write a function that takes in an array of integers and returns a sorted version
+of that array. Use the insertion sort algorithm to sort the array.
+
+- Sample Input: [8, 5, 2, 9, 5, 6, 3]
+- Sample Output: [2, 3, 5, 5, 6, 8, 9] 
+
+- Hints:
+    - Divide the input array into two subarrays in place.
+    - The first subarray should be sorted at all times and should start with a
+      length of 1.
+    - The second subarray should be unsorted.
+    - Iterate through the unsorted subarray, inserting all of its elements into
+      the sorted subarray in the correct position by swapping them into place.
+    - Eventually, the entire array will be sorted.
+"""
+
+def insertionSort(array):
+    pass
+
+print(insertionSort([8, 5, 2, 9, 5, 6, 3]))  # [2, 3, 5, 5, 6, 8, 9]
