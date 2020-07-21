22829ebc121d951e11c1d2ee258deb2ffaacd6ea
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-21 10:00:47 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

--- edabit/hard/left_digit/left_digit.py
+++ edabit/hard/left_digit/left_digit.py
@@ -13,9 +13,46 @@ integer in the string.
 - Notes:
     - Each string will have at least two numbers.
 """
+"""
+The 4 Phases of the U.P.E.R. Problem-Solving Framework
+
+****** UNDERSTAND ******
+- Objective:
+    - Write an algorithm that takes in a single input string and returns the
+      left-most integer in that string as the single output.
+      
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type(s): string
+    - Var Name(s): "str"
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type(s): integer
+    - Var Name(s): "left_most_int"
+    
+- Edge Cases & Constraints:
+    - Can the given input string be empty?
+        - Yes. If the given input string is empty, return a string that says,
+          "Invalid input. Please enter a string with a length greater than 0."
+    - Can the numbers in the given input string be negative numbers?
+        - No. They must be positive integers.
+    - Can the numbers in the given input string be floating point numbers?
+        - No. They must be positive integers.
+
+****** PLAN ******
+
+
+****** EXECUTE ****** (Please see below)
+"""
 
 def left_digit(num):
     pass
 
 print(left_digit("TrAdE2W1n95!"))   # 2
 print(left_digit("V3r1ta$"))        # 3
+
+"""
+****** REFLECT/REFACTOR ******
+
+"""
