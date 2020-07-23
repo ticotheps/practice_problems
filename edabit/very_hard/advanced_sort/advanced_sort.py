282f233e97b558fd071819a1d5e0db0ed0197191
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-23 09:08:54 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

--- edabit/very_hard/advanced_sort/advanced_sort.py
+++ edabit/very_hard/advanced_sort/advanced_sort.py
@@ -14,3 +14,36 @@ same value should be in the same sublist.
     - The sublists should be returned in the order of each element's first
       appearance in the given list.
 """
+"""
+The 4 Phases of the U.P.E.R. Problem-Solving Framework
+
+****** UNDERSTAND ******
+- Objective:
+    - Write an algorithm that takes in a single list of either all numbers or all
+      strings and returns a list of multiple sublists where each sublist only
+      contains elements that are duplicates of one another.
+      
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): "lst"
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): "list_of_sublists"
+    
+- Edge Cases & Constraints
+    - Can the given input list include a list of numbers mixed in with strings?
+        - No. They must be all of one type or the other. 
+    - Can the given input list include negative numbers?
+        - Yes.
+    - Can the given input list include floating point numbers?
+        - Yes.
+
+****** PLAN ******
+
+
+****** EXECUTE ******
+
+"""
