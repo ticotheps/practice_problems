de5faa1b2f692ee0904afc356b0f819ddf824261
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-24 08:07:09 -0400

    chore(prompt): add prompt for code challenge

---
+++ edabit/very_hard/setify/setify.py
@@ -0,0 +1,14 @@
+"""
+TRANSFORM INTO A LIST WITH NO DUPLICATES
+
+A set is a collection of unique items. A set can be formed from a list from
+removing all duplicate items.
+
+Create a function that sorts a list and removes all duplicate items from it.
+
+- Examples:
+    - setify([1, 3, 3, 5, 5]) ➞        [1, 3, 5]
+    - setify([4, 4, 4, 4]) ➞           [4]
+    - setify([5, 7, 8, 9, 10, 15]) ➞   [5, 7, 8, 9, 10, 15]
+    - setify([3, 3, 3, 2, 1]) ➞        [1, 2, 3]
+"""
