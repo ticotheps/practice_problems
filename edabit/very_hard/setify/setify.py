20c4daf51409cb161fa990cc1748eff28315c315
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-24 08:14:15 -0400

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

--- edabit/very_hard/setify/setify.py
+++ edabit/very_hard/setify/setify.py
@@ -12,3 +12,39 @@ Create a function that sorts a list and removes all duplicate items from it.
     - setify([5, 7, 8, 9, 10, 15]) ➞   [5, 7, 8, 9, 10, 15]
     - setify([3, 3, 3, 2, 1]) ➞        [1, 2, 3]
 """
+
+"""
+The 4 Phases of the U.P.E.R. Problem-Solving Framework
+
+****** UNDERSTAND Phase ******
+- Objective:
+    - Write an algorithm that takes in a single input list, removes any
+      duplicates, and returns a single output list that contains no duplicates.
+      
+- Expected Input(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): "lst"
+    
+- Expected Output(s):
+    - Number Of: 1
+    - Data Type(s): list
+    - Var Name(s): "new_lst"
+    
+- Edge Cases & Constraints:
+    - Can the given input list include strings?
+        - Yes.
+    - Can the given input list be empty?
+        - No. If this is the case, return a string that says, "Invalid input.
+          Please provide a string with at least 1 element."
+    - Can the elements in the given input list be negative numbers?
+        - Yes.
+    - Can the elements in the given input list be floating point numbers?
+        - Yes.
+    
+****** PLAN Phase ******
+
+****** EXECUTE Phase ****** (Please see below)
+
+
+"""
