af0430f01c7960b36b555a92605cbf3a781841b4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-25 11:08:13 -0400

    chore(prompt): add prompt for "missing_num" code challenge

---
+++ edabit/very_hard/missing_num/missing_num.py
@@ -0,0 +1,15 @@
+"""
+FIND THE MISSING NUMBER
+
+Create a function that takes in a list of numbers between 1 and 10 (excluding
+one number) and returns the missing number.
+
+- Examples:
+    - missing_num([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+    - missing_num([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+    - missing_num([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+    
+- Notes:
+    - The list of numbers will be unsorted (not in order).
+    - Only one number will be missing.
+"""
