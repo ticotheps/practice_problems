814e55822a757763045af65782dcd1b3eefa3b98
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-27 07:24:15 -0400

    feat(U.P.E.R Phase I): complete UNDERSTAND phase

--- code_signal/arcade/edge_of_the_ocean/shape_area/shape_area.py
+++ code_signal/arcade/edge_of_the_ocean/shape_area/shape_area.py
@@ -13,6 +13,24 @@ appending "1-interesting polygons" to its rim, side by side. You can see the
 - Constraints:
     - 1 <= n < 10^4
 """
+"""
+****** UNDERSTAND phase ******
+- Objective: Write an algorithm that takes in a single integer (length of 1 side
+  on the "interesting polygon") for the input and returns a single integer 
+  (area of the "interesting polygon") for the output.
+  
+- Expected input(s): 1, "n", integer data type
+- Expected output(s): 1, "area", integer data type
+
+- Constraints:
+    - "n" will always be between 1 and 10,000.
+
+****** PLAN phase ******
+
+****** EXECUTE phase ******
+
+****** REFLECT/REFACTOR phase ******
+"""
 
 def shapeArea(n):
     pass
