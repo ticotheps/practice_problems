c9c2bf30baf50d5472407f52dba0991859ee5e3c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-27 07:19:46 -0400

    chore(prompt): add the prompt for "shape_area" code challenge

--- code_signal/arcade/edge_of_the_ocean/shape_area/shape_area.py
+++ code_signal/arcade/edge_of_the_ocean/shape_area/shape_area.py
@@ -1,3 +1,19 @@
+"""
+SHAPE AREA
+
+A "1-interesting polygon" is just a square with a side length of 1. An
+"n-interesting" polygon is obtained by taking the "n-1 -interesting polygon and
+appending "1-interesting polygons" to its rim, side by side. You can see the
+"1-", "2-", "3-", and "4-" "-interesting" polygons in the picture provided.
+
+- Example:
+    - For n = 2, the output should be shapeArea(n) = 5. 
+    - For n = 3, the output should be shapeArea(n) = 13
+    
+- Constraints:
+    - 1 <= n < 10^4
+"""
+
 def shapeArea(n):
     pass
 
