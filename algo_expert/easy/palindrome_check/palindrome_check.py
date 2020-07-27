1735964db570caf8d1d50988854af5ded922e930
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-27 08:46:18 -0400

    chore(prompt): add prompt for "palindrome_check" code challenge

--- algo_expert/easy/palindrome_check/palindrome_check.py
+++ algo_expert/easy/palindrome_check/palindrome_check.py
@@ -1,3 +1,25 @@
+"""
+PALINDROME CHECK
+
+Write a function that takes in a non-empty string and that returns a boolean
+representing whether the string is a palindrome. A palindrome is defined as "a
+string that's written the same forward and backward. 
+
+Note: Single character strings ARE palindromes.
+
+Sample Input: "abcdcba"
+Sample Output: "True"
+
+Hints:
+    - Start by building the input string in reverse order and comparing this
+    newly built string to the input string.
+    - Can you optimize your algorithm by using recursion? What are the
+    implications of recursion on an algorithm's space-time complexity analysis?
+    - Go back to an iterative solution and try using pointers to solve this
+    problem: start with a pointer at the first index of the string and a pointer
+    at the final index of the string. What can you do from there?
+"""
+
 def isPalindrome(string):
     pass
 
