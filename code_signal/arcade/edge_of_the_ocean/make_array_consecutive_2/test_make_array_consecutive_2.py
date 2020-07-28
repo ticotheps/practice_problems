ebdfb6d1081aa12d96a7374b65abbb4871915e98
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-28 07:48:32 -0400

    chore(test): add test cases for "make_array_consecutive2" code challenge

--- code_signal/arcade/edge_of_the_ocean/make_array_consecutive_2/test_make_array_consecutive_2.py
+++ code_signal/arcade/edge_of_the_ocean/make_array_consecutive_2/test_make_array_consecutive_2.py
@@ -4,6 +4,10 @@ from make_array_consecutive_2 import makeArrayConsecutive2
 class Test(unittest.TestCase):
     def test_makeArrayConsecutive2(self):
         self.assertEqual(makeArrayConsecutive2([6, 2, 3, 8]), 3)
+        self.assertEqual(makeArrayConsecutive2([0, 3]), 2)
+        self.assertEqual(makeArrayConsecutive2([5, 4, 6]), 0)
+        self.assertEqual(makeArrayConsecutive2([6, 3]), 2)
+        self.assertEqual(makeArrayConsecutive2([1]), 0)
         
 if __name__ == "__main__":
     unittest.main()
