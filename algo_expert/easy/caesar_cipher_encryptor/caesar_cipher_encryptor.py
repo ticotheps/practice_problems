dbb07dcadc9369ca1c002b77cf380c9b98abea6e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-29 15:40:34 -0400

    chore: update HINTS section

--- algo_expert/easy/caesar_cipher_encryptor/caesar_cipher_encryptor.py
+++ algo_expert/easy/caesar_cipher_encryptor/caesar_cipher_encryptor.py
@@ -16,8 +16,21 @@ Sample Output: "zab"
 Hints:
     - Most languages have built-in functions that give you the Unicode value of
     a character as well as the character corresponding to a Unicode value.
+    
     - Consider using such functions to determine which letters the input
     string's letters should be mapped to.
+    
+    - Try creating your own mapping of letters to codes. In other words, try
+    associating each letter in the alphabet with a specific number - its
+    position in the alphabet, for instance - and using that to determine which
+    letters the input string's letters should be mapped to.
+    
+    - How do you handle cases where a letter gets shifted to a position that
+    requires wrapping around the alphabet? What about cases where the key is
+    very large and causes multiple wrappings around the alphabet? The modulo
+    operator should be your friend here.
+    
+    - O(n) time | O(n) space - where n is the length of the input string
 """
 """
 ****** UNDERSTAND Phase ******
