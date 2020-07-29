b88e868204b6f5c14ba8329070784bee338adb4d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-29 15:36:35 -0400

    chore(prompt): add prompt for "caesar_cipher_encryptor" code challenge

--- algo_expert/easy/caesar_cipher_encryptor/caesar_cipher_encryptor.py
+++ algo_expert/easy/caesar_cipher_encryptor/caesar_cipher_encryptor.py
@@ -1,3 +1,26 @@
+"""
+CAESAR CIPHER ENCRYPTOR
+
+Given a non-empty string of lowercase letters and a non-negative integer
+representing a key, write a function that returns a new string obtained by
+shifting every letter in the input string by k positions in the alphabet, where
+k is the key.
+
+NOTE:
+    - Letters should "wrap" around the alphabet; in other words, the letter "z"
+    shifted by one returns the letter "a".
+    
+Sample Inputs: "xyz", 2
+Sample Output: "zab"
+
+Hints:
+    - Most languages have built-in functions that give you the Unicode value of
+    a character as well as the character corresponding to a Unicode value.
+    - Consider using such functions to determine which letters the input
+    string's letters should be mapped to.
+"""
+
+
 def caesarCipherEncryptor(string, key):
     pass
 
