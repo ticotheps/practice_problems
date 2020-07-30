fc354301245160beb0d174ad2713b9e2606c01c3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-07-30 07:53:53 -0400

    feat(U.P.E.R Phase II): complete the PLAN phase

--- code_signal/arcade/edge_of_the_ocean/almost_increasing_sequence/almost_increasing_sequence.py
+++ code_signal/arcade/edge_of_the_ocean/almost_increasing_sequence/almost_increasing_sequence.py
@@ -32,10 +32,9 @@ Constraints:
 
 """
 ****** UNDERSTAND Phase ******
-
 - Objective: Write an algorithm that takes in a single input list, evaluates
   whether or not a single element could be removed from the list to make its order
-  "strictly ascending", and then returns True or False (Boolean value) depending on
+  "strictly increasing", and then returns True or False (Boolean value) depending on
   whether or not it can do that.
   
 - Expected Input(s): 1; list data type; "sequence"
@@ -45,4 +44,37 @@ Constraints:
     - The length of any given input list will be between 2 and 1000000.
     - The value of any element in any given input list will be between -1000000
       and 1000000.
+      
+      
+****** PLAN Phase ******
+- First Pass Solution:
+    (1) Define a helper function, "isStrictlyIncreasing(lst)", that takes in a list
+    and returns True or False based on whether or not the given input list is in
+    'strictly increasing' order.
+    
+    (2) Define a function, "almostIncreasingSequence(sequence)", that takes in a list,
+    determines whether or not a single element can be removed from the list to
+    put the remaining elements in 'strictly increasing" order, and returns True
+    or False based on whether or not it can do this.
+    
+        (a) Define a var, "verified_strictly_increasing", and initialize it with a
+        value of False (Boolean value).
+    
+        (b) Use a 'for' loop to iterate through a range of numbers, beginning with 
+        0 (inclusive) and ending with the length of the given input list...
+
+            (i) Declare a var, "test_list", that will be a replica of the given input
+            list.
+            
+            (ii) Remove the value of "test_list" at the index of iterator "i".
+            
+            (iii) Use the helper function, "isStrictlyIncreasing()", to determine
+            if the remaining elements in "test_list" are in strictly increasing order.
+            
+            (iv) If they are in strictly increasing order, set the value of
+            "verified_strictly_increasing" to True and return that value.
+            
+            (v) If they are NOT in strictly increasing order, do nothing.
+        
+        (c) Return the value of "verified_strictly_increasing".
 """
