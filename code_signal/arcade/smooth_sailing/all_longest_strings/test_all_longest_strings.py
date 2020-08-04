03a92139bf374189bf4977abc7b089b900d02f83
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-04 08:06:48 -0400

    chore(test): added more testcases

--- code_signal/arcade/smooth_sailing/all_longest_strings/test_all_longest_strings.py
+++ code_signal/arcade/smooth_sailing/all_longest_strings/test_all_longest_strings.py
@@ -4,6 +4,21 @@ from all_longest_strings import allLongestStrings
 class Test(unittest.TestCase):
     def test_allLongestStrings(self):
         self.assertEqual(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]), ["aba", "vcd", "aba"])
+        self.assertEqual(allLongestStrings(["aa"]), ["aa"])
+        self.assertEqual(allLongestStrings(["abc", "eeee", "abcd", "dcd"]), ["eeee", "abcd"])
+        self.assertEqual(allLongestStrings(
+            ["a", "abc", "cbd", "zzzzzz", "a", "abcdef", "asasa", "aaaaaa"]), 
+            ["zzzzzz", "abcdef", "aaaaaa"]
+        )
+        self.assertEqual(allLongestStrings(["enyky", "benyky", "yely", "varennyky"]), ["varennyky"])
+        self.assertEqual(allLongestStrings(["abacaba", "abacab", "abac", "xxxxxx"]), ["abacaba"])   
+        self.assertEqual(allLongestStrings(
+            ["young", "yooooooung", "hot", "or", "not", "come", "on", "fire", "water", "watermelon"]), 
+            ["yooooooung", "watermelon"]
+        )
+        self.assertEqual(allLongestStrings(["onsfnib", "aokbcwthc", "jrfcw"]), ["aokbcwthc"])
+        self.assertEqual(allLongestStrings(["lbgwyqkry"]), ["lbgwyqkry"])
+        self.assertEqual(allLongestStrings(["i"]), ["i"])
         
         
 if __name__ == "__main__":
