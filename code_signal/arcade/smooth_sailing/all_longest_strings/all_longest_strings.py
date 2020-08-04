24d8d4f072e539e2e3497b8cba2ea0a2dc7f407d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-04 09:19:38 -0400

    chore(prompt): add prompt for code challenge

---
+++ code_signal/arcade/smooth_sailing/all_longest_strings/all_longest_strings.py
@@ -0,0 +1,10 @@
+"""
+ALL LONGEST STRINGS
+
+Given an array of strings, return another array containing all of its longest
+strings.
+
+Example:
+    - Example Input: ["aba", "aa", "ad", "vcd", "aba"]
+    - Example Output: ["aba", "vcd", "aba"]
+"""
