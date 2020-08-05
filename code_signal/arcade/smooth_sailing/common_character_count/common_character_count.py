32add25ab5be3a0b3ae7c3fdc3aea492cb5994b3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-05 09:14:34 -0400

    chore(prompt): add prompt for code challenge

---
+++ code_signal/arcade/smooth_sailing/common_character_count/common_character_count.py
@@ -0,0 +1,9 @@
+"""
+****** UNDERSTAND Phase ******
+
+Given two strings, find the number of common characters between them.
+
+- Example:
+    - Inputs: "aabcc", "adcaa" (2; string data type; "s1" & "s2")
+    - Outputs: 3 (1; integer data type; "num_common_chars")
+"""
