3acd2cf765d451bed243435a2fd5a5705ae0d953
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-05 09:15:41 -0400

    chore: layout function

--- code_signal/arcade/smooth_sailing/common_character_count/common_character_count.py
+++ code_signal/arcade/smooth_sailing/common_character_count/common_character_count.py
@@ -7,3 +7,8 @@ Given two strings, find the number of common characters between them.
     - Inputs: "aabcc", "adcaa" (2; string data type; "s1" & "s2")
     - Outputs: 3 (1; integer data type; "num_common_chars")
 """
+
+def commonCharacterCount(s1, s2):
+    pass
+
+print(commonCharacterCount("aabcc", "adcaa"))  # 3
