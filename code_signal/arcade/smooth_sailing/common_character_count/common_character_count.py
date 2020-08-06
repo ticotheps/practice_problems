8d41b54dad5cd15f7198b16d071bef3029ea0263
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-06 11:13:35 -0400

    feat(first pass solution): create a first pass solution that passes all test cases

--- code_signal/arcade/smooth_sailing/common_character_count/common_character_count.py
+++ code_signal/arcade/smooth_sailing/common_character_count/common_character_count.py
@@ -7,20 +7,48 @@ Given two strings, find the number of common characters between them.
     - Inputs: "aabcc", "adcaa" (2; string data type; "s1" & "s2")
     - Outputs: 3 (1; integer data type; "num_common_chars")
 """
-
 def commonCharacterCount(s1, s2):
-    # create an empty array to store common strings.
+    s1_dict = {}
+    print(f"\ns1_dict = {s1_dict}")
     
-    # use a 'for' loop to iterate through the first input string.
+    s1_list = [x for x in s1]
+    print(f"s1_list = {s1_list}")
     
-        # use another 'for' loop to iterate through the 2nd input string.
+    for i in s1_list:
+        if i not in s1_dict:
+            s1_dict[i] = 1
+        else:
+            s1_dict[i] += 1
+    print(f"*s1_dict = {s1_dict}")
+    
+    s2_dict = {}
+    print(f"s2_dict = {s2_dict}")
+    
+    s2_list = [y for y in s2]
+    print(f"\ns2_list = {s2_list}")
+    
+    for j in s2_list:
+        if j not in s2_dict:
+            s2_dict[j] = 1
+        else:
+            s2_dict[j] += 1
+    print(f"*s2_dict = {s2_dict}")
+    
+    # declare a "count" var to keep track of the number of common characters
+    count = 0
+    print(f"count = {count}")
+    
+    diff = 0
+    print(f"diff = {diff}")
+    
+    for k in s1_dict:
+        if k in s2_dict:
+            if s1_dict[k] <= s2_dict[k]:
+                count += s1_dict[k]
+            else:
+                count += s2_dict[k]
         
-        # compare the characters using an 'if' statement. If they are 
-        # equal to one another, then append that character into the empty 
-        # array you created.
-        
-        # If they are not equal to one another, do nothing.
-        
-    # return the value of the empty array that you created.
+    return count
 
 print(commonCharacterCount("aabcc", "adcaa"))  # 3
+print(commonCharacterCount("zzzz", "zzzzzzz"))  # 4
--- code_signal/arcade/smooth_sailing/common_character_count/test_common_character_count.py
+++ code_signal/arcade/smooth_sailing/common_character_count/test_common_character_count.py
@@ -4,6 +4,7 @@ from common_character_count import commonCharacterCount
 class Test(unittest.TestCase):
     def test_commonCharacterCount(self):
         self.assertEquals(commonCharacterCount("aabcc", "adcaa"), 3)
+        self.assertEquals(commonCharacterCount("zzzz", "zzzzzzz"), 4)
         
 if __name__ == "__main__":
     unittest.main()
