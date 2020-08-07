b813c07a1d41906d76442610dc38dddeed9b137a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-07 15:53:18 -0400

    chore(test): add more test cases for "is_lucky" code challenge

--- code_signal/arcade/smooth_sailing/is_lucky/test_is_lucky.py
+++ code_signal/arcade/smooth_sailing/is_lucky/test_is_lucky.py
@@ -5,6 +5,15 @@ from is_lucky import isLucky
 class Test(unittest.TestCase):
     def test_isLucky(self):
         self.assertEquals(isLucky(1230), True)
-    
+        self.assertEquals(isLucky(239017), False)
+        self.assertEquals(isLucky(134008), True)
+        self.assertEquals(isLucky(10), False)
+        self.assertEquals(isLucky(11), True)
+        self.assertEquals(isLucky(1010), True)
+        self.assertEquals(isLucky(261534), False)
+        self.assertEquals(isLucky(100000), False)
+        self.assertEquals(isLucky(999999), True)
+        self.assertEquals(isLucky(123321), True)
+        
 if __name__ == "__main__":
     unittest.main()
