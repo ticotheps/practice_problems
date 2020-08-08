05f3df036479c5d0e1626e480d8c0b2649f02e7c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-08-08 13:22:05 -0400

    feat(U.P.E.R Phase II): create a PLAN for the "is_lucky" code challenge

--- code_signal/arcade/smooth_sailing/is_lucky/is_lucky.py
+++ code_signal/arcade/smooth_sailing/is_lucky/is_lucky.py
@@ -24,3 +24,32 @@ Guaranteed constraints:
 
 true if n is a lucky ticket number, false otherwise.
 """
+
+def isLucky(n):
+    # evaluate for even number of digits.
+        # if not even, return a string to ask the user to provide a valid string.
+
+    # declare a new var, "n_str", and convert "n" to a string of integers.
+        
+    # declare a new var, "half_1_str", and use slicing to get the first half of
+    # characters from "n_str".
+    
+    # declare a new var, "half_2_str", and use slicing to get the second half of
+    # characters from "n_str".
+    
+    # declare a new var, "half_1_sum", and set it equal to 0.
+    
+    # declare a new var, "half_2_sum", and set it equal to 0.
+    
+    # use a 'for' loop to iterate through "half_1_str"...
+        # convert the iterated-on element to an integer.
+        # add the integer value of that iterated-on element to "half_1_sum".
+        
+    # use a 'for' loop to iterate through "half_2_str"...
+        # convert the iterated-on element to an integer.
+        # add the integer value of that iterated-on element to "half_2_sum".
+
+    # if "half_1_sum" is equal to "half_2_sum", return True.
+    # else, return False.
+print(isLucky(1230))  # True
+print(isLucky(239017))  # False
