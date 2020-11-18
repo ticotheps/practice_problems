8c869e6e03ead542dd8a6af378e74f4df2d05673
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-17 23:18:55 -0500

    chore(prompt): add code challenge prompt

---
+++ edabit/very_easy/convert_hours_into_secs/convert_hours_into_secs.py
@@ -0,0 +1,14 @@
+"""
+CONVERT HOURS INTO SECONDS
+
+Write a function that converts hours into seconds.
+
+Examples:
+    - how_many_seconds(2) -> 7200
+    - how_many_seconds(10) -> 36000
+    - how_many_seconds(24) -> 86400
+    
+Notes:
+    - 60 seconds in a minute; 60 minutes in a hour.
+    - Don't forget to return your answer.
+"""
