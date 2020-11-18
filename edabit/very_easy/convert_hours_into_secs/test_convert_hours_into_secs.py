4450bc5b39ee6b539086996c3e2ec931e0e57ad9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-18 01:55:44 -0500

    test: add new test cases for exception handling

--- edabit/very_easy/convert_hours_into_secs/test_convert_hours_into_secs.py
+++ edabit/very_easy/convert_hours_into_secs/test_convert_hours_into_secs.py
@@ -9,6 +9,12 @@ class Test(unittest.TestCase):
         self.assertEqual(how_many_seconds(10), 36000)
         self.assertEqual(how_many_seconds(12), 43200)
         self.assertEqual(how_many_seconds(24), 86400)
+        self.assertEqual(how_many_seconds(2.5), 9000)
+    def test_none_value_for_input(self):
+        self.assertRaises(TypeError, how_many_seconds)
+    def test_negative_value_for_input(self):
+        self.assertRaises(TypeError, how_many_seconds)
+
         
 if __name__ == '__main__':
     unittest.main()
