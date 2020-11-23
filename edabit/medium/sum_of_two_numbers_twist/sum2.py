be5feb548078fafcd9e26c07d5d84d4cc25a6068
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-22 23:01:54 -0500

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

--- edabit/medium/sum_of_two_numbers_twist/sum2.py
+++ edabit/medium/sum_of_two_numbers_twist/sum2.py
@@ -13,5 +13,52 @@ Examples:
     
 Notes:
     - Remember how to sum two numbers ON PAPER; it could be useful.
-    - Your function must run in less than 10 seconds because Edabit has a time limit.
+    - Your function must run in less than 10 seconds because Edabit has a time
+    limit.
+    - Each input number can have 1000 digits (characters) in it.
+"""
+
+"""
+U.P.E.R.
+
+- UNDERSTAND
+    - Objective: 
+        - Write an algorithm that takes in two inputs (both integers that
+        are represented using the 'String' data type) and returns a single output
+        (an integer that is also represented using the 'String' data type).
+    
+    - Expected Input(s):
+        - 2 expected inputs
+        - both are integers that are actually of the 'String' data type
+        - "num1", "num2"
+    
+    - Expected Output(s):
+        - 1 expected output
+        - an integer that is represented by a 'String' data type
+        - "sum"
+        
+    - Constraints and Edge Cases:
+        - Constraint #1:
+            - len(num1) <= 1000
+        - Constraint #2:
+            - len(num2) <= 1000
+        - Can either of the given inputs be negative?
+            - Yes, they can be negative because the prompt doesn't explicitly
+              state that they must be positive integers.
+        - Can either of the given inputs be floating point numbers?
+            - Yes, they can be floating point numbers because the prompt doesn't
+              explicitly state that they must be positive integers.
+        - Can either of the given inputs be empty strings?
+            - No, because 'None' is not a number than can be added with another number.
+        
+- PLAN
+    -
+"""
+
+# - EXECUTE
+
+
+"""
+- REFLECT/REFACTOR
+    - 
 """
