9a5a5b6a19d94c52f58a260ac736d91939eee9cc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-22 23:24:19 -0500

    test: create a unit test for 'sum2()'

---
+++ edabit/medium/sum_of_two_numbers_twist/test_sum2.py
@@ -0,0 +1,9 @@
+import unittest
+from sum2 import sum2
+
+class Test(unittest.TestCase):
+    def test_sum2(self):
+        self.assertEqual(sum2("5125515215521515", "125261616261626"), "5250776831783141")
+        
+if '__name__' == __main__:
+    unittest.main()
