98f60ab9171ed9e977abd2e8ef65287c7e14f01a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-22 23:26:32 -0500

    fix(test): fix typo

--- edabit/medium/sum_of_two_numbers_twist/test_sum2.py
+++ edabit/medium/sum_of_two_numbers_twist/test_sum2.py
@@ -5,5 +5,5 @@ class Test(unittest.TestCase):
     def test_sum2(self):
         self.assertEqual(sum2("5125515215521515", "125261616261626"), "5250776831783141")
         
-if '__name__' == __main__:
+if __name__ == '__main__':
     unittest.main()
