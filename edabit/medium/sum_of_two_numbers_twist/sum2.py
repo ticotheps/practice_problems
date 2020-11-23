acd7c59436c6db63c6babc4a2b05d082e9d5f63d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-22 22:50:39 -0500

    chore(prompt): add prompt for code challenge

---
+++ edabit/medium/sum_of_two_numbers_twist/sum2.py
@@ -0,0 +1,17 @@
+"""
+SUM OF TWO NUMBERS (with a twist)!
+
+Find the sum of two numbers.
+
+
+Examples:
+    - sum2("5125515215521515", "125261616261626") -> "5250776831783141"
+    - sum2("6666666666666666666666666666", "99999999999999999999999") ➞
+    "6666766666666666666666666665"
+    - sum2("123456789123456789123456789", "987654321987654321987654329876543") ➞
+    "987654445444443445444443453333332"
+    
+Notes:
+    - Remember how to sum two numbers ON PAPER; it could be useful.
+    - Your function must run in less than 10 seconds because Edabit has a time limit.
+"""
