dc0efb39b118a0623fd3e553252b97dc4c6cdb9b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-26 00:52:31 -0500

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

---
+++ educative/ace_python_interview/data_structures/lists/remove_even_integers/remove_even.py
@@ -0,0 +1,47 @@
+"""
+REMOVE EVEN INTEGERS FROM LIST
+
+Implement a function that removes all the even elements from a given list. Name
+it "remove_even(list)".
+
+Input:
+    - A list with random integers.
+
+Output:
+    - A list with only odd integers.
+    
+Sample Input:
+    - my_list = [1, 2, 4, 5, 10, 6, 3]
+    
+Sample Output:
+    - my_list = [1, 5, 3]
+"""
+
+"""
+U.P.E.R. Problem-Solving Framework
+
+UNDERSTAND:
+    - Objective:
+        - Write an algorithm that takes in a single input list of random integers
+        and returns a single output list of only odd integers.
+        
+    - Expected Inputs:
+        - 1
+        - 'list' data type
+        - "list"
+        
+    - Expected Outputs:
+        - 1
+        - 'list' data type
+        - "odd_list"
+        
+    - Constraints & Edge Cases:
+        - Can the input list contain negative numbers?
+            - Yes. Negative numbers are acceptable.
+        - Can the input list contain floating point numbers?
+            - No.
+        - Can the input list be empty?
+            - No. It must contain at least 1 element.
+        - What happens if we're given a list and that list contains all even numbers?
+            - Return an empty list.
+"""
