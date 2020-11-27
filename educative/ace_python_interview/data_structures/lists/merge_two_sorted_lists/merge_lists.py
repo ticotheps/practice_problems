513b21e35807f68b515a66a09a306c34f8533261
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-26 23:32:59 -0500

    chore(prompt): add prompt for code challenge

---
+++ educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/merge_lists.py
@@ -0,0 +1,20 @@
+"""
+MERGE TWO SORTED LISTS
+
+Objective:
+    - Implement a function that merges two sorted lists, of 'm' and 'n' elements
+    respectively, into another sorted list. Name it 'merge_lists(lst1, lst2)'.
+
+Inputs:
+    - Two sorted lists.
+    
+Outputs:
+    - A merged and sorted list consisting of all elements of both input lists.
+    
+Sample Inputs:
+    - list1 = [1, 3, 4, 5]
+    - list2 = [2, 6, 7, 8]
+    
+Sample Output:
+    - arr = [1, 2, 3, 4, 5, 6, 7, 8]
+"""
