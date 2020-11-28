2400239a96912b229517f02ad9bf697a47b909f6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-28 00:02:15 -0500

    feat(U.P.E.R Phase I): complete the UNDERSTAND phase

--- educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/merge_lists.py
+++ educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/merge_lists.py
@@ -18,3 +18,34 @@ Sample Inputs:
 Sample Output:
     - arr = [1, 2, 3, 4, 5, 6, 7, 8]
 """
+
+"""
+U.P.E.R. Problem-Solving Framework
+
+UNDERSTAND:
+    - Objective:
+        - Write an algorithm that takes in two inputs (two sorted lists of integers)
+        and returns an single output (a merged and sorted list of the two input
+        lists).
+    
+    - Inputs:
+        - Expecting: 2
+        - Data Types: list, list
+        - Variable Names: "lst_a", "lst_b"
+        
+    - Outputs:
+        - Expecting: 1
+        - Data Type: list
+        - Variable Name: "merged_lst"
+        
+    - Constraints & Edge Cases:
+        - Can either of the input lists be empty?
+            - No. You cannot merge an empty list into another list.
+        - Can the input lists have a differnt number of elements in each?
+            - Yes.
+        - Can the input lists contain negative integers as elements?
+            - Yes.
+        - Can the input lists contain floating point numbers as elements?
+            - No. The prompt states that the input lists will contain 'integers', which are whole numbers.
+    
+"""
