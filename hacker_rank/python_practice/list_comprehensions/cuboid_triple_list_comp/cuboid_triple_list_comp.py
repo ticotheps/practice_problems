40c31063cd670cfbe4617f1d86e8802c0697625f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-28 01:41:00 -0500

    feat(U.P.E.R. Phase IV): add alternate solution with triple list comprehensions

--- hacker_rank/python_practice/list_comprehensions/cuboid_triple_list_comp/cuboid_triple_list_comp.py
+++ hacker_rank/python_practice/list_comprehensions/cuboid_triple_list_comp/cuboid_triple_list_comp.py
@@ -1,14 +1,19 @@
-def print_coordinates(x, y, z, n):
-    possible_coordinates_lst = []
+# Brute Force Solution
+# def print_coordinates(x, y, z, n):
+#     possible_coordinates_lst = []
     
-    for i in range(0, x+1):
-        for j in range(0, y+1):
-            for k in range(0, z+1):
-                if i + j + k != n:
-                    coordinates = [i, j, k]
-                    possible_coordinates_lst.append(coordinates)
+#     for i in range(0, x+1):
+#         for j in range(0, y+1):
+#             for k in range(0, z+1):
+#                 if i + j + k != n:
+#                     coordinates = [i, j, k]
+#                     possible_coordinates_lst.append(coordinates)
                 
-    return possible_coordinates_lst
+#     return possible_coordinates_lst
+
+# Alternate Solution with List Comprehensions
+def print_coordinates(x, y, z, n):
+    return [[i, j, k] for i in range(0, x+1) for j in range(0, y+1) for k in range(0, z+1) if i + j + k != 3]
 
 print(print_coordinates(1, 1, 2, 3))  # [[0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 2]]
 
