f5cf3c97def66d3d393037cace62c60751d2e6cc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-28 00:18:24 -0500

    feat(U.P.E.R Phase II): complete the PLAN phase

--- educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/merge_lists.py
+++ educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/merge_lists.py
@@ -48,4 +48,21 @@ UNDERSTAND:
         - Can the input lists contain floating point numbers as elements?
             - No. The prompt states that the input lists will contain 'integers', which are whole numbers.
     
+PLAN:
+    - Brute Force Solution Overview:
+    (1) Write a function that takes in 2 arguments ("lst_a" and "lst_b") which
+    are sorted lists and return a single output list, which is a single, merged
+    & sorted list of the two given input lists.
+    
+    (2) Initialize two variables to represent pointers for indices of each input list as we iterate through them at different paces. The different paces of iteration are due to the dependence upon the outcome of value comparisons between the input lists when merging them together while keeping all of the elements sorted.
+    
+    (3) Iterate through the longer of the two input lists (with the plan of merging elements from "lst_b" INTO "lst_a") AS LONG AS "ind_a" < len(lst_a) and "ind_b" < len(lst_b):
+    
+        (a) Compare the value of "lst_a[ind_a]" to the value of "lst_b[ind_b]". If 'lst_a[ind_a]' < 'lst_b[ind_b]', then there will be no insert occurrence and we will just increment "ind_a" by 1.
+        
+        (b) If 'lst_a[ind_a]' > 'lst_b[ind_b]', we will insert 'lst_b[ind_b]' into 'lst_a' at the 'ind_a' and then we will increment both, 'ind_a' and 'ind_b', by 1.
+        
+    (4) If "ind_a" is equal to len(lst_a), then add the remaining (sorted) elements of 'lst_b' to the end of 'lst_a'.
+    
+    (5) Return 'lst_a'. 
 """
