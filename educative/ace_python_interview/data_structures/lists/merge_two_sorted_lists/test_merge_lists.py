a9936b5ec937913637bffd147503bccdebb0b347
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-28 00:37:52 -0500

    test: add another test case

--- educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/test_merge_lists.py
+++ educative/ace_python_interview/data_structures/lists/merge_two_sorted_lists/test_merge_lists.py
@@ -5,6 +5,7 @@ from merge_lists import merge_lists
 class Test(unittest.TestCase):
     def test_merge_lists(self):
         self.assertEqual(merge_lists([1, 3, 4, 5], [2, 6, 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(merge_lists([4, 5, 6], [-2, -1, 0, 7]), [-2, -1, 0, 4, 5, 6, 7])
         
 if __name__ == '__main__':
     unittest.main()
