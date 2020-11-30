1cf31b7cc61c471e6148b42ef6a1a654d3337aa1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-29 23:15:44 -0500

    chore(prompt): add prompt for coding challenge

---
+++ educative/ace_python_interview/data_structures/lists/list_of_products/find_product.py
@@ -0,0 +1,21 @@
+"""
+LIST OF PRODUCTS OF ALL ELEMENTS
+
+Given a list, modify it so that each index stores the product of all elements in
+the list except the element at the index itself.
+
+Input:
+    - A list of numbers (could be floating point or integers).
+    
+Output:
+    - A list such that each index has a product of all the numbers in the list
+    except the number stored at that index.
+    
+Sample Input/Output #1:
+    - Input arr = [1, 2, 3, 4]
+    - Output arr = [24, 12, 8, 6]
+    
+Sample Input/Output #2:
+    - Input arr = [4, 2, 1, 5, 0]
+    - Output arr = [0, 0, 0, 0, 40]
+"""
