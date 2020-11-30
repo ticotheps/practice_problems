b1cc1c134daabeebe7dca5f7dd3545507443e90e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-11-30 00:08:35 -0500

    feat(chore): add another test case

--- educative/ace_python_interview/data_structures/lists/list_of_products/test_find_product.py
+++ educative/ace_python_interview/data_structures/lists/list_of_products/test_find_product.py
@@ -5,6 +5,7 @@ from find_product import find_product
 class Test(unittest.TestCase):
     def test_find_product(self):
         self.assertEqual(find_product([1, 2, 3, 4]), [24, 12, 8, 6])
+        self.assertEqual(find_product([4, 2, 1, 5, 0]), [0, 0, 0, 0, 40])
         
         
 if __name__ == '__main__':
