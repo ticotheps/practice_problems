d14237d75ca866f08066c64b0e319b2ace27302e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-12-01 20:03:32 -0500

    chore(prompt): provide code prompt

---
+++ hacker_rank/python_practice/basic_data_types/finding_the_percentage/find_average_score.py
@@ -0,0 +1,15 @@
+"""
+FINDING THE PERCENTAGE
+
+The provided code stub will read in a dictionary containing key/value pairs of
+name:[scores] for a list of students. Print the average of the scores array for
+the student name provided, showing the 2 places after the decimal.
+
+Example:
+    - scores = {
+        'alpha': [20, 30, 40],
+        'beta': [30, 50, 70]
+    }
+    - query_name = 'beta'
+    - beta's average score is (30 + 50 + 70)/3 = 50.0
+"""
