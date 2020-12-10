07f6a4ec80af7313e0e1dd343430690d304b0289
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-12-09 23:22:29 -0500

    feat(first pass solution): define a function that will reverse a linked list

--- algo_expert/hard/reverse_linked_list/reverse_linked_list.py
+++ algo_expert/hard/reverse_linked_list/reverse_linked_list.py
@@ -18,3 +18,31 @@ Sample Input:
 Sample Output:
     - 5 -> 4 -> 3 -> 2 -> 1 -> 0     # the new 'head' node with value 5
 """
+
+def reverseLinkedList(head):
+    # Define the first two pointers that will be used to reverse the linked list
+    # at each node.
+    p1, p2 = None, head
+    
+    # Iterate through the linked list so long as the 'p2' pointer does not
+    # reach the end of the list.
+    while p2 is not None:
+        
+        # Create a third pointer, p3, that will keep reference to the value of
+        # p2.next before p2.next is overwritten to reverse the direction of the
+        # linked list AT THIS NODE.
+        p3 = p2.next
+        
+        # Point the 'p2.next' pointer to p2's previous node (i.e. - p1).
+        p2.next = p1
+        
+        # Move the 'p1' pointer forward to the next node to continue iterating
+        # through the linked list.
+        p1 = p2
+        
+        # Move the 'p2' pointer forward to the next node to continue iterating
+        # through the linked list.
+        p2 = p3
+        
+    # Return the value of the new 'head' node.
+    return p1
