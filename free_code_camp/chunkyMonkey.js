3563dd8bb10b139fdf6acfd75188b682eac7f176
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-05-12 12:45:55 -0400

    feat(JS scripting): create a brute force solution for the "Chunky Monkey" challenge

--- free_code_camp/chunkyMonkey.js
+++ free_code_camp/chunkyMonkey.js
@@ -1,5 +1,20 @@
 function chunkArrayInGroups(arr, size) {
-    return arr;
+    // console.log({ arr, size });
+
+    // (1) Use slice() to extract a shallow copy of portions
+    // of the given 'arr' based on the given 'size'.
+
+    const new_2d_arr = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        let new_group = arr.slice(i, i + size)
+        new_2d_arr.push(new_group);
+    };
+    console.log({ new_2d_arr });
+    return new_2d_arr;
 };
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups(["a", "b", "c", "d"], 2);        // [["a", "b"], ["c", "d"]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);          // [[0, 1, 2], [3, 4, 5]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);          // [[0, 1], [2, 3], [4, 5]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);       // [[0, 1, 2], [3, 4, 5], [6]]
